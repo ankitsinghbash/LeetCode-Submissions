269141062b931091ae39eb88d7f0b583bc9e49e0
Ankit Singh <ankitsingh4734414code@gmail.com>
2022-10-17 13:53:50 +0000

    Sync LeetCode submission Runtime - 12 ms (0.09%), Memory - 9.8 MB (100.00%)

--- my-code/0035-search-insert-position/solution.cpp
+++ my-code/0035-search-insert-position/solution.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        
- 
+        bool flag=true;
         for(int i=0;i<nums.size();i++)
         {
             if(nums[i]==target)
             {
+                flag=false;
+                return i;
+            }
+            if(target<nums[i])
+            {
                 return i;
             }
         }
-        
-        //means element is not found:
-        int j=0;
-        int counter=0;
-        while(j<nums.size())
-        {
-            if(nums[j]<target)
-            {
-                counter++;
-            }
-            j++;
+        if(flag==true){
+            return nums.size();
         }
-        return counter;
+        //if not found add to its right position and return is position:
+        
+        return 0;
     }
+   
 };
