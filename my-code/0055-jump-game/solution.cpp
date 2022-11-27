3bdebe02feca3457c2b5eddf5147c35fdd9afbc1
Ankit Singh <ankitsingh4734414code@gmail.com>
2022-11-27 15:00:44 +0000

    Sync LeetCode submission Runtime - 1254 ms (5.00%), Memory - 48.6 MB (99.98%)

---
+++ my-code/0055-jump-game/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:    
+    bool canJump(vector<int>& nums) {
+        //down top dp;
+        int n=nums.size();
+        vector<bool> dp(n+1,false);
+        //let's we reache's the last index:
+        dp[n-1]=true;  
+        for(int i=n-1;i>=0;i--)
+        {
+            //we jum from 1 to nums[i]: and i+j<n;
+           for(int j=1;j<=nums[i]&&i+j<n;j++)
+           {
+               if(dp[i+j]==true)
+               {
+                   dp[i]=true;
+                   break;
+               }
+           }
+        }
+
+       return dp[0];
+    }
+};
