480aa5668d793d19f5f947a82f3ae766fbdc7d92
Ankit Singh <ankitsingh4734414code@gmail.com>
2022-12-05 10:29:06 +0000

    Sync LeetCode submission Runtime - 3 ms (19.83%), Memory - 6.2 MB (100.00%)

---
+++ my-code/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int strStr(string str2, string str1) {
+        
+         int pos=0;
+         int index;
+         while((index=str2.find(str1,pos))!=string::npos)
+         {
+             pos=index+1;
+             return pos-1;
+         }
+       return -1;
+    }
+};
