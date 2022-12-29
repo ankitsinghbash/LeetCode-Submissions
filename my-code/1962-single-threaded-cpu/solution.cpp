4052413c496f8e7d2e70dc7a6b9addf3310ced64
Ankit Singh <ankitsingh4734414code@gmail.com>
2022-12-29 15:56:13 +0000

    Sync LeetCode submission Runtime - 682 ms (5.00%), Memory - 129.3 MB (91.83%)

---
+++ my-code/1962-single-threaded-cpu/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> getOrder(vector<vector<int>>& tasks) {
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        long n = tasks.size();
+        long time = 0, i=0;
+        for(int i=0; i<n; i++)
+            tasks[i].push_back(i);
+        sort(tasks.begin(), tasks.end());
+        vector<int> ret;
+        while(i<n || !pq.empty())
+        {
+            if(pq.empty())
+                time = max(time, (long)tasks[i][0]);
+            while(i<n && time>=tasks[i][0])
+            {
+                pq.push(make_pair(tasks[i][1], tasks[i][2]));
+                i++;
+            }
+            pair<int, int> A = pq.top();
+            pq.pop();
+            time+=A.first;
+            ret.push_back(A.second);
+        }
+        return ret;
+    }
+};
