7e8812b6b28249f13f84b6cc949616186bd24254
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-01-07 11:24:26 +0000

    Sync LeetCode submission Runtime - 49 ms (5.25%), Memory - 12.2 MB (100.00%)

--- my-code/0981-delete-columns-to-make-sorted/solution.cpp
+++ my-code/0981-delete-columns-to-make-sorted/solution.cpp
@@ -1,17 +1,17 @@
-class Solution { 
-    public:
-     int minDeletionSize(vector<string>& strs)
-      { 
-     const int n = strs.size();
-     int m = strs[0].size(); 
-     int ans = 0; 
-     for (int j = 0; j < m; j++) 
-       for (int i = 1; i < n; i++) 
-        if (strs[i][j] < strs[i - 1][j]) 
-        { 
-            ans++;
-            break; 
-         }
-     return ans;
-      }
- };
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int ans=0;
+        for(int j=0;j<strs[0].size();j++)
+        {
+            for(int i=1;i<strs.size();i++)
+            {
+                if(strs[i][j]<strs[i-1][j]){
+                    ans++;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
