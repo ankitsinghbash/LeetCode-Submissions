0a81a5cea80230b8b6b847b582f68b18b1045a56
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-01-10 05:13:47 +0000

    Sync LeetCode submission Runtime - 3 ms (2.79%), Memory - 8.6 MB (100.00%)

--- my-code/0144-binary-tree-preorder-traversal/solution.cpp
+++ my-code/0144-binary-tree-preorder-traversal/solution.cpp
@@ -8,28 +8,49 @@
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
- */class Solution {
-     public:
-         vector<int> preorderTraversal(TreeNode* root) 
-             {
-                     vector<int>ans;
-                             if(root==NULL)
-                                         return ans;
-                                                 stack<TreeNode*>st;
-                                                         st.push(root);
-                                                                 while(!st.empty())
-                                                                         {
-                                                                                     root=st.top();
-                                                                                                 st.pop();
-                                                                                                             ans.push_back(root->val);
-                                                                                                                         if(root->right!=NULL)
-                                                                                                                                         st.push(root->right);
-                                                                                                                                                     if(root->left!=NULL)
-                                                                                                                                                                     st.push(root->left);
-                                                                                                                                                                             }
-                                                                                                                                                                                     return ans;
-                                                                                                                                                                                         }
-                                                                                                                                                                                         };
- 
+ */
+class Solution {
+public:  
+    
+    
+    vector<int> ans;
+    vector<int> preorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st;
+        if(root==NULL){
+           return {};
+       }
+        st.push(root);
+       while(!st.empty())
+       {
+           TreeNode *node = st.top();
+           st.pop();
+           if(node->right!=NULL){
+                   st.push(node->right);
+           }
+           if(node->left!=NULL){
+                 st.push(node->left);
+           }
+           ans.push_back(node->val);
+       }
+       return ans;
+
+    }
+};
 
 
+
+
+
+/*
+ vector<int> ans;
+    void solve(TreeNode *root){
+
+         if(root==NULL){
+             return; 
+         }
+        
+        ans.push_back(root->val);
+        solve(root->left);
+        solve(root->right);
+    }
+*/
