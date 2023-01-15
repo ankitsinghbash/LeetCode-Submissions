de21ed7b1099c7d3123ee9f1e29d13283da6a195
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-01-15 17:57:17 +0000

    Sync LeetCode submission Runtime - 489 ms (5.02%), Memory - 18.1 MB (100.00%)

--- my-code/0045-jump-game-ii/solution.cpp
+++ my-code/0045-jump-game-ii/solution.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
+
+   long long  solve(vector<int> &nums,int n,int idx,vector<int> &dp){
+ 
+      if(idx>=n-1){
+          return 0;
+      }
+   
+      if(dp[idx]!=-1){
+          return dp[idx];
+      }
+     
+      long long mini = INT_MAX;
+      for(long long  i=1;i<=nums[idx];i++){
+          long long ans = solve(nums,n,idx+i,dp)+1;
+          mini=min(mini,ans);
+      }
+      return dp[idx]=mini;
+   }
+
     int jump(vector<int>& nums) {
-        vector<int> dp(nums.size());
-        for(int i=nums.size()-2;i>=0;i--)
-        {
-            int step=nums[i];
-            int mini=INT_MAX;
-            if(step>0)
-            {
-               for(int j=1;j<=step && i+j<nums.size();j++)
-               {
-                   mini=min(mini,dp[i+j]);
-              }
-            }
-            if(mini==INT_MAX)
-            {
-                dp[i]=mini;
-            }
-            else
-            {
-                dp[i]=mini+1;
-            }
-        }
-        return dp[0];
+        long long n=nums.size();
+        vector<int> dp(n+1,-1);
+        return solve(nums,n,0,dp);
     }
 };
-
-
-
