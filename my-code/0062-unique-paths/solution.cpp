e1a2b7f384d38ba158dce216c79c6fd568de94a7
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-01-21 11:16:40 +0000

    Sync LeetCode submission Runtime - 5 ms (0.60%), Memory - 6.4 MB (100.00%)

--- my-code/0062-unique-paths/solution.cpp
+++ my-code/0062-unique-paths/solution.cpp
@@ -1,47 +1,27 @@
 class Solution {
 public:
-    //some time tabulation is most easy then use tabulation;
 
-    int solve(int n,int m,int i,int j,vector<vector<int>> &dp){
+   int solve(int i,int j,int m,int n,vector<vector<int>> &dp){
+
+       if(i==m-1&&j==n-1) return 1;
+
+       if(i>=m||j>=n) return 0;
+
      
-       if(i==n-1&&j==m-1){
-           return 1;
-       }
-       
-       
-       if(i>=n||j>=m){
-           return 0;
-       }
-  
-       if(dp[i][j]!=-1){
-          return dp[i][j];
-         }
+      if(dp[i][j]!=-1){
+           return dp[i][j];
+      }
 
-  
-       int right = solve(n,m,i,j+1,dp);
-       int down = solve(n,m,i+1,j,dp);
-     
-       return dp[i][j] = right+down;
+       int right = solve(i,j+1,m,n,dp);
+       int down = solve(i+1,j,m,n,dp);
 
-    }
+       return dp[i][j]=right+down;  //formula of dp;
 
-    int uniquePaths(int n, int m) {
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return solve(n,m,0,0,dp);
+   }
+
+
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+        return solve(0,0,m,n,dp);
     }
 };
-/*Tabulation solution:-
-    vector<vector<int>> dp(m+1,vector<int>(n+1));
-        for(int i=0;i<m+1;i++){
-            dp[i][0]=1;
-        }
-        for(int j=0;j<n+1;j++){
-            dp[0][j]=1;
-        }
-        for(int i=1;i<m+1;i++){
-            for(int j=1;j<n+1;j++){
-                dp[i][j]=dp[i-1][j]+dp[i][j-1];
-            }
-        }
-        return dp[m-1][n-1];
-*/
