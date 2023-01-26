e9ba1f881f6052ef242dfa133b05465c6d90821f
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-01-26 17:20:41 +0000

    Sync LeetCode submission Runtime - 31 ms (13.97%), Memory - 9.1 MB (100.00%)

--- my-code/0072-edit-distance/solution.cpp
+++ my-code/0072-edit-distance/solution.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        int n=word1.size();
-        int m=word2.size();
-        int dp[n+1][m+1];
+        int n = word1.size();
+        int m =word2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1));
         dp[0][0]=0;
-        for(int i=1;i<n+1;i++)  //means word1 row me hai:
-        {
+
+        for(int i=0;i<n+1;i++){
             dp[i][0]=i;
         }
-        for(int j=1;j<m+1;j++)   //means word2 col me hai:
-        {
+        for(int j=0;j<m+1;j++){
             dp[0][j]=j;
         }
 
-        for(int i=1;i<n+1;i++)
-        {
-            for(int j=1;j<m+1;j++)
-            {
-                if(word1[i-1]==word2[j-1])
-                {
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(word1[i-1]==word2[j-1]){
                     dp[i][j]=dp[i-1][j-1];
                 }
                 else{
-                    dp[i][j] = min(dp[i-1][j],min(dp[i-1][j-1],dp[i][j-1]))+1;
+                    dp[i][j]=min({dp[i-1][j-1],dp[i-1][j],dp[i][j-1]})+1;
                 }
             }
         }
         return dp[n][m];
-
-
-
     }
 };
