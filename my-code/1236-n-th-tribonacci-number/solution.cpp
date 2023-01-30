bb76e525186ba563d9bc67856bd8309805b03a8e
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-01-30 12:29:59 +0000

    Sync LeetCode submission Runtime - 3 ms (0.26%), Memory - 6 MB (100.00%)

--- my-code/1236-n-th-tribonacci-number/solution.cpp
+++ my-code/1236-n-th-tribonacci-number/solution.cpp
@@ -1,45 +1,51 @@
 class Solution {
 public:
 
-   //this is recursive approach:
-   int solve(int n,int *dp)
-   {
-       dp[0]=0;
-       dp[1]=1;
-       dp[2]=1;
-       for(int i=3;i<=n;i++)
-       {
-           dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
-       }
-      return dp[n];
+   int solve(int n,int dp[]){
+ 
+     if(n==0) return 0;
+     if(n==1 || n==2) return 1;
+
+     if(dp[n]!=-1) return dp[n];
+
+     return dp[n]=solve(n-1,dp)+solve(n-2,dp)+solve(n-3,dp);
+     
    }
 
     int tribonacci(int n) {
-        //first solve by recursion:
-        int dp[n+3]; 
-        int ans=solve(n,dp);
-        return ans;
+        //recursive+memo;
+        int dp[38];
+        memset(dp,-1,sizeof(dp));
+        return solve(n,dp);
     }
 };
 
 
-/*
-Now the approch is right do with dynammic prorgramming:
-//this is recursive approach:
-   int solve(int n)
-   {
-       if(n==0)
-       {
-           return 0;
-       }
-       if(n==1||n==2)
-       {
-           return 1;
-       }
-  
-      int answer=solve(n-1)+solve(n-2)+solve(n-3);
-      return answer;
 
-   }
 
-*/
+//Tabulation:
+// class Solution {
+// public:
+
+//    //this is recursive approach:
+//    int solve(int n,int *dp)
+//    {
+//        dp[0]=0;
+//        dp[1]=1;
+//        dp[2]=1;
+//        for(int i=3;i<=n;i++)
+//        {
+//            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+//        }
+//       return dp[n];
+//    }
+
+//     int tribonacci(int n) {
+//         //first solve by recursion:
+//         int dp[n+3]; 
+//         int ans=solve(n,dp);
+//         return ans;
+//     }
+// };
+
+
