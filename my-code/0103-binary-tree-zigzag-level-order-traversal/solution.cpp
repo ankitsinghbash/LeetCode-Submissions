6b3e110515bcd510d7b527668b4799f6036aac01
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-02-11 19:12:34 +0000

    Sync LeetCode submission Runtime - 3 ms (7.96%), Memory - 12.2 MB (99.92%)

--- my-code/0103-binary-tree-zigzag-level-order-traversal/solution.cpp
+++ my-code/0103-binary-tree-zigzag-level-order-traversal/solution.cpp
@@ -9,96 +9,38 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
- /*
 class Solution {
 public:
-    
-    void zigzag(int level,TreeNode *root,vector<vector<int>> &answer ){
-       stack<TreeNode*> parent,baby; 
-       vector<int> ans;
-       parent.push(root);
-       while(!parent.empty()){
-           TreeNode *temp = parent.top();
-           parent.pop();
-           if(temp){
-           ans.push_back(temp->val);
-           }
-           if(level%2==1)
-           {
-               if(!temp->left){
-               baby.push(temp->left);
-               }
-               if(!temp->right){
-               baby.push(temp->right);
-               }
-           }
-           else{
-               if(!temp->right){
-               baby.push(temp->right);
-               }
-               if(!temp->left){
-               baby.push(temp->left);
-               }
-           }
-           
-           if(parent.size()==0) {
-                swap(parent,baby);
-                answer.push_back(ans);
-                ans.clear();
-                level++;
-                
-            }
-       }
-    }
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>> answer;
-        zigzag(1,root,answer);
-        return answer;
+        vector<vector<int>> real;
+        if(root==NULL)
+        return real;
+
+        queue<TreeNode*> q;
+        q.push(root);
+        int level=0;
+        while(!q.empty())
+        {
+            
+            int size=q.size();
+            vector<int> ans;
+            while(size--)
+            {
+                TreeNode* node=q.front();
+                q.pop();
+                ans.push_back(node->val);
+                if(node->left!=NULL)
+                q.push(node->left);
+                if(node->right!=NULL)
+                q.push(node->right);
+                //not right condtion hare if even or odd that given error in case: [1,2,3,4,null,null,5]
+            }
+            if(level%2!=0)
+                reverse(ans.begin(),ans.end());
+            real.push_back(ans);
+            level++;
+            
+        }
+        return real;
     }
 };
-*/
-class Solution { 
-    public:
-     vector<vector<int>> zigzagLevelOrder(TreeNode* root) { vector<vector<int>> zigZagLevels;
-      if(root==NULL)
-       return zigZagLevels;
-        vector<int> level; 
-        stack<TreeNode*> LtoR; 
-         
-         stack<TreeNode*> RtoL; 
-          
-           LtoR.push(root); 
-           int lvl = 1;
-            //level Number
-             while(!LtoR.empty() || !RtoL.empty()){ 
-                 if(lvl%2 != 0){ 
-                     while(!LtoR.empty())
-                     { 
-                 if(LtoR.top()->left != NULL) 
-                     RtoL.push(LtoR.top()->left);
-                 if(LtoR.top()->right !=NULL)
-                  RtoL.push(LtoR.top()->right);
-                   level.push_back(LtoR.top()->val);
-                    LtoR.pop(); 
-                    }
-                     zigZagLevels.push_back(level);
-                      level.clear(); 
-                      }
-                       else
-                        { 
-                      while(!RtoL.empty()){
-                         if(RtoL.top()->right != NULL)
-                          LtoR.push(RtoL.top()->right);
-                  if(RtoL.top()->left != NULL)
-                   LtoR.push(RtoL.top()->left); 
-                   level.push_back(RtoL.top()->val);
-                    RtoL.pop(); 
-                    } 
-                    zigZagLevels.push_back(level);
-                     level.clear(); 
-                     }
-                      ++lvl; 
-                     } return zigZagLevels;
-                      }
-                  };
-
