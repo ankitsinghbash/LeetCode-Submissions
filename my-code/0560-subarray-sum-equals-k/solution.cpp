1d6fd84b3fe39d865757ef68d3a9bb6fdd7eeb4d
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-02-13 16:12:53 +0000

    Sync LeetCode submission Runtime - 99 ms (21.97%), Memory - 41.6 MB (79.15%)

--- my-code/0560-subarray-sum-equals-k/solution.cpp
+++ my-code/0560-subarray-sum-equals-k/solution.cpp
@@ -1,30 +1,51 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-     
-        int count=0;
-      int n=nums.size();
-      unordered_map<int,int> map;
-      int target = k;
-     int sum=0;  //initilize sum=0 and start each value of arr at prefix:
-     for(int i=0;i<n;i++)
-     {
-         sum+=nums[i];
-         if(sum==target)
-         {
-             count++;
-         }
-         
-         if(map.find(sum-target)!=map.end()) //means ager value mil gyi;
-         {
-             //value repeat hui to uski second value ko add karo;
-             count+=map[sum-target];  //it give second value m[];
-         }
-         //if above find me nahi gya means to map me value daal ke one increase kar do:
-         //if repeat value come in any case then map take it only one time and only increase the
-         //second value so it directly count by the count variable;
-         map[sum]++; 
-     }
-        return count;
+        //prefix sum:
+        int sum=0;
+        int cnt=0;
+        int target=k;
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            if(sum==target) cnt++;
+ 
+          if(mp.find(sum-target)!=mp.end()){
+              cnt+=mp[sum-target];
+          }
+ 
+           mp[sum]++;
+        }
+        return cnt;
     }
 };
+// class Solution {
+// public:
+//     int subarraySum(vector<int>& nums, int k) {
+     
+//         int count=0;
+//       int n=nums.size();
+//       unordered_map<int,int> map;
+//       int target = k;
+//      int sum=0;  //initilize sum=0 and start each value of arr at prefix:
+//      for(int i=0;i<n;i++)
+//      {
+//          sum+=nums[i];
+//          if(sum==target)
+//          {
+//              count++;
+//          }
+         
+//          if(map.find(sum-target)!=map.end()) //means ager value mil gyi;
+//          {
+//              //value repeat hui to uski second value ko add karo;
+//              count+=map[sum-target];  //it give second value m[];
+//          }
+//          //if above find me nahi gya means to map me value daal ke one increase kar do:
+//          //if repeat value come in any case then map take it only one time and only increase the
+//          //second value so it directly count by the count variable;
+//          map[sum]++; 
+//      }
+//         return count;
+//     }
+// };
