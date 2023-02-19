a7138e949aef86e5ebb0880575a877a58c6f4db2
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-02-19 15:52:21 +0000

    Sync LeetCode submission Runtime - 3 ms (2.94%), Memory - 9.3 MB (100.00%)

---
+++ my-code/0129-sum-root-to-leaf-numbers/solution.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    void solve(TreeNode *root,string str,int &ans){
+   
+        if(root==NULL) return;
+
+
+        if(root->left==NULL && root->right==NULL){
+            str+=to_string(root->val);
+            ans+=stoi(str);
+            str.pop_back();
+            return;
+        }     
+
+
+        str+=to_string(root->val);
+        solve(root->left,str,ans);
+        solve(root->right,str,ans);
+  
+
+    }
+
+
+    int sumNumbers(TreeNode* root) {
+        int ans=0;
+        string str = "";
+        solve(root,str,ans);
+        return ans;
+    }
+};
