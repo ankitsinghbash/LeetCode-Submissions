b5bb11be22f67f50eb0ca87343b41d1e9b9fa7ca
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-02-19 04:08:40 +0000

    Sync LeetCode submission Runtime - 6 ms (0.28%), Memory - 12.2 MB (99.92%)

--- my-code/0103-binary-tree-zigzag-level-order-traversal/solution.cpp
+++ my-code/0103-binary-tree-zigzag-level-order-traversal/solution.cpp
@@ -12,35 +12,70 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>> real;
-        if(root==NULL)
-        return real;
+        if(root==NULL) return {};
 
-        queue<TreeNode*> q;
-        q.push(root);
+        queue<TreeNode*> qu;
+        qu.push(root);
         int level=0;
-        while(!q.empty())
-        {
-            
-            int size=q.size();
+        vector<vector<int>> real;
+        while(!qu.empty()){
+            int size = qu.size();
             vector<int> ans;
-            while(size--)
-            {
-                TreeNode* node=q.front();
-                q.pop();
+            while(size--){
+                TreeNode *node = qu.front();
+                qu.pop();
                 ans.push_back(node->val);
-                if(node->left!=NULL)
-                q.push(node->left);
-                if(node->right!=NULL)
-                q.push(node->right);
-                //not right condtion hare if even or odd that given error in case: [1,2,3,4,null,null,5]
+                if(node->left!=NULL){
+                    qu.push(node->left);
+                }
+                if(node->right!=NULL){
+                    qu.push(node->right);
+                }
             }
-            if(level%2!=0)
+            if(level%2!=0){
                 reverse(ans.begin(),ans.end());
+            }
             real.push_back(ans);
             level++;
-            
         }
         return real;
     }
+    
 };
+
+
+// class Solution {
+// public:
+//     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+//         vector<vector<int>> real;
+//         if(root==NULL)
+//         return real;
+
+//         queue<TreeNode*> q;
+//         q.push(root);
+//         int level=0;
+//         while(!q.empty())
+//         {
+            
+//             int size=q.size();
+//             vector<int> ans;
+//             while(size--)
+//             {
+//                 TreeNode* node=q.front();
+//                 q.pop();
+//                 ans.push_back(node->val);
+//                 if(node->left!=NULL)
+//                 q.push(node->left);
+//                 if(node->right!=NULL)
+//                 q.push(node->right);
+//                 //not right condtion hare if even or odd that given error in case: [1,2,3,4,null,null,5]
+//             }
+//             if(level%2!=0)
+//                 reverse(ans.begin(),ans.end());
+//             real.push_back(ans);
+//             level++;
+            
+//         }
+//         return real;
+//     }
+// };
