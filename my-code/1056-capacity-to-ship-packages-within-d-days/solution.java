14553c607b4943bbb916184214802c2d9711237a
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-02-22 16:19:34 +0000

    Sync LeetCode submission Runtime - 11 ms (68.30%), Memory - 54.7 MB (6.57%)

---
+++ my-code/1056-capacity-to-ship-packages-within-d-days/solution.java
@@ -0,0 +1,41 @@
+class Solution {
+   
+     public static int solve(int cap,int arr[])
+    {
+        int count=1;
+        int sum=0;
+        for(int a:arr)
+        {
+            if(a>cap)
+            return Integer.MAX_VALUE;
+            
+            if(sum+a<=cap)
+            {
+                sum+=a;
+            }
+            else
+            {
+                count++;
+                sum=a;
+            }
+        }
+        return count;
+    }
+
+
+    public int shipWithinDays(int[] arr, int D) {
+         
+        int start=1, end=(int)1e9;
+        while(start<end)
+        {
+            int mid=start+(end-start)/2;
+            int days=solve(mid,arr);
+            if(days<=D)
+            end=mid;
+            else
+            start=mid+1;
+        }
+        return start;
+
+    }
+}
