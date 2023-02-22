f7b9df28f8c5a9614efa07db1cbf308acb1a09e3
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-02-22 16:41:26 +0000

    Sync LeetCode submission Runtime - 56 ms (6.54%), Memory - 26.2 MB (100.00%)

---
+++ my-code/1056-capacity-to-ship-packages-within-d-days/solution.cpp
@@ -0,0 +1,90 @@
+class Solution {
+public:
+   
+    int solve(vector<int> &arr,int cap){
+        int sum=0;
+        int  cnt=0;
+        for(auto &x : arr){
+            if(x>cap){
+                return INT_MAX;
+            }
+            if(sum<=cap){
+                sum+=x;
+            }
+            if(sum>cap){
+                sum=0;
+                sum=x;
+                cnt++;
+            }
+        }
+        if(sum<=cap){
+            cnt++;
+        }
+        return cnt;
+    }
+  
+
+    int shipWithinDays(vector<int>& arr, int D) {
+        int low=1;
+        int high=INT_MAX;
+        while(low<high){
+            int mid = low+(high-low)/2;
+            int day = solve(arr,mid);
+            if(day<=D){
+                high=mid;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return low;
+    }
+};
+
+
+
+
+
+
+
+// class Solution {
+   
+//      public static int solve(int cap,int arr[])
+//     {
+//         int count=1;
+//         int sum=0;
+//         for(int a:arr)
+//         {
+//             if(a>cap)
+//             return Integer.MAX_VALUE;
+            
+//             if(sum+a<=cap)
+//             {
+//                 sum+=a;
+//             }
+//             else
+//             {
+//                 count++;
+//                 sum=a;
+//             }
+//         }
+//         return count;
+//     }
+
+
+//     public int shipWithinDays(int[] arr, int D) {
+         
+//         int start=1, end=(int)1e9;
+//         while(start<end)
+//         {
+//             int mid=start+(end-start)/2;
+//             int days=solve(mid,arr);
+//             if(days<=D)
+//             end=mid;
+//             else
+//             start=mid+1;
+//         }
+//         return start;
+
+//     }
+// }
