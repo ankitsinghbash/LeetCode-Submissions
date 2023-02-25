9df985ddc37378840878f3c5a78372bfbc66704f
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-02-25 07:47:06 +0000

    Sync LeetCode submission Runtime - 133 ms (6.39%), Memory - 93.4 MB (99.98%)

--- my-code/0121-best-time-to-buy-and-sell-stock/solution.cpp
+++ my-code/0121-best-time-to-buy-and-sell-stock/solution.cpp
@@ -1,53 +1,15 @@
 class Solution {
 public:
-  
-    int maxProfit(vector<int>& nums) {
-        int lowestcostbefore=INT_MAX;
-        int profifsoldtoday = 0;
-        int maximumpro = 0;
-        
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]<lowestcostbefore)  //this calculate ki abb takk ka minimum buying cost of stock:
-            {
-                lowestcostbefore=nums[i];
-            }
-
-           profifsoldtoday=nums[i]-lowestcostbefore;
-           if(profifsoldtoday>maximumpro)
-           {
-               maximumpro=profifsoldtoday;
-           }
-
+    int maxProfit(vector<int>& prices) {
+        int mini = INT_MAX;
+        int maxx = 0;
+        for(int i=0;i<prices.size();i++){
+               mini=min(mini,prices[i]);
+               if(prices[i]>mini){
+                   int val = prices[i]-mini;
+                   maxx=max(maxx,val);
+               }
         }
-        return maximumpro;
-    
-
+        return maxx;
     }
 };
-
-
-
-/*   vector<int> ans;
-for(int i=0;i<nums.size()-1;i++)
-       {
-       for(int j = i+1;j<nums.size();j++)
-         {
-          if(nums[i]<nums[j])
-           {
-             int diff = nums[j]-nums[i];
-             ans.push_back(diff);
-           }
-        }
-        }
-
-    if(!ans.empty()){
-    int result = *max_element(ans.begin(),ans.end());
-    return result;
-    }
-    else
-    {
-      return 0;
-    }
-
-*/
