4dd6bc86fcbeaed23dbc3124a45c83fe2335d557
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-02-28 08:00:24 +0000

    Sync LeetCode submission Runtime - 444 ms (8.09%), Memory - 9.8 MB (89.98%)

--- my-code/0279-perfect-squares/solution.cpp
+++ my-code/0279-perfect-squares/solution.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
+ 
+   int solve(int n,int val,vector<int> &dp){
+ 
+    if(n==0){
+        return 0;
+    }
+
+    if(val*val>n){   //but more approcriate is if(val*val>n)
+        return 1e9;
+    }
+    
+     if(dp[n]!=-1){
+         return dp[n];
+     }
+   
+     int pick = 1+solve(n-val*val,val,dp);
+     int notpick = solve(n,val+1,dp);
+
+     return dp[n]=min(pick,notpick);
+
+   }
+   
+ 
     int numSquares(int n) {
-        vector<int> dp(n+1,INT_MAX);
-        dp[0]=0;
-        for(int i=1;i<n+1;i++)
-        {
-            int mini = INT_MAX;
-            for(int j=1;j*j<=i;j++)
-            {
-                int rem = i-j*j;
-                mini=min(mini,dp[rem]);
-            }
-            dp[i]=mini+1;
-        }
-        return dp[n];
+      vector<int> dp(n+1,-1);
+        return solve(n,1,dp);
     }
 };
