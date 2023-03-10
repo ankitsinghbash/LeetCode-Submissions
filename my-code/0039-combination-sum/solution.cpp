9fcba70ae96d87c0241c68722403c8c9aec20ca8
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-03-10 18:06:57 +0000

    Sync LeetCode submission Runtime - 52 ms (11.34%), Memory - 25.7 MB (15.03%)

--- my-code/0039-combination-sum/solution.cpp
+++ my-code/0039-combination-sum/solution.cpp
@@ -2,18 +2,14 @@ class Solution {
 public:
    vector<vector<int>> ans;
 
-   void helper(int i,vector<int> path,vector<int> candidates,int target)
+   void helper(int idx,vector<int> path,vector<int> candidates,int target,int n)
    {
 
-      if(candidates.size()==i)
-      {
-          return;
-      }
+      if(idx>=n)  return;
+
  
-     if(target<0)
-     {
-         return;
-     }
+     if(target<0)  return;
+     
 
       if(target==0)
       {
@@ -21,20 +17,19 @@ public:
           return;
       }
 
-      helper(i+1,path,candidates,target); //for traversal from,2,3,6,7:
-
-     path.push_back(candidates[i]);
-     helper(i,path,candidates,target-candidates[i]);  //from traversal according to i ,in first case i from 2 but in second case i from 3,in third case i from 6 and i last case i from 7;
-     path.pop_back();
-
+    for(int i=idx;i<candidates.size();++i){
+        path.push_back(candidates[i]);
+         helper(i,path,candidates,target-candidates[i],n);  
+         path.pop_back();
+      }
        
    }
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        
-    int i=0;
     vector<int> path;
-    helper(i,path,candidates,target);
+    int n=candidates.size();
+    helper(0,path,candidates,target,n);
     return ans;
+
     }
 };
