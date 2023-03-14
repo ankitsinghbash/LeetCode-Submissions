cffb95b8eaedf7d4bc42798fa43374b103c7029b
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-03-14 01:36:31 +0000

    Sync LeetCode submission Runtime - 278 ms (5.19%), Memory - 77.5 MB (58.52%)

---
+++ my-code/1620-check-if-array-pairs-are-divisible-by-k/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool canArrange(vector<int>& arr, int k) {
+        map<int,int> mp;
+        for(int i=0;i<arr.size();i++){
+            int rem   = arr[i]%k;
+            if(rem<0){
+                rem+=k;
+            }
+            mp[rem]++;
+        }
+  
+      if(mp[0]%2!=0){
+          return false;
+      }
+
+       for(int i=1;i<=k/2;i++){
+           if(mp[i]!=mp[k-i]){
+               return false;
+           }
+       }
+       return true;
+ 
+    }
+};
