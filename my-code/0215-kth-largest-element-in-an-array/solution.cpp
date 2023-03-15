03955eea4e59f200e9444c51bcd0418296ca105d
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-03-15 16:13:02 +0000

    Sync LeetCode submission Runtime - 113 ms (5.86%), Memory - 47.4 MB (100.00%)

--- my-code/0215-kth-largest-element-in-an-array/solution.cpp
+++ my-code/0215-kth-largest-element-in-an-array/solution.cpp
@@ -1,13 +1,28 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end());
-        int j=nums.size()-1;
-        int last;
-        while(k--){
-            last = nums[j];
-            j--;
-        }  
-        return last;
+         priority_queue<int,vector<int>,greater<int> > p1;
+         for(int i=0;i<k;i++){
+             p1.push(nums[i]);
+         } 
+
+         for(int i=k;i<nums.size();i++){
+             int val = p1.top();
+             if(val<=nums[i]){
+                 p1.pop();
+                 p1.push(nums[i]);
+             }
+         }
+         return p1.top();
     }
 };
+
+
+// sort(nums.begin(),nums.end());
+//         int j=nums.size()-1;
+//         int last;
+//         while(k--){
+//             last = nums[j];
+//             j--;
+//         }  
+//         return last;
