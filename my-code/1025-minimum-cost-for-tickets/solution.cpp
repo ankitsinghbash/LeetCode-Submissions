50eb17b9955ddb8ab930944e4b9615fd5871e026
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-04-02 07:38:24 +0000

    Sync LeetCode submission Runtime - 9 ms (5.90%), Memory - 9.6 MB (100.00%)

--- my-code/1025-minimum-cost-for-tickets/solution.cpp
+++ my-code/1025-minimum-cost-for-tickets/solution.cpp
@@ -1,18 +1,47 @@
 class Solution {
 public:
-    int mincostTickets(vector<int>& days, vector<int>& costs) {
-        
-        int n=days.size();
-        vector<int> dp(n+1,INT_MAX);
-        dp[n]=0;
-        for(int i=n-1;i>=0;i--)
-        {
-           // dp[i]=costs[0]+dp[i+1];
-            int d7=i,d30=i;
-            while(d7<n&&days[d7]<days[i]+7) d7++;
-            while(d30<n&&days[d30]<days[i]+30) d30++;
-            dp[i]=min(costs[0]+dp[i+1],min(costs[1]+dp[d7],costs[2]+dp[d30]));
+   //index increment kar do day ke hisaab se:
+  //general approach for these type of question:
+   int solve(int idx,vector<int> &days,vector<int> &costs,int n,vector<int> &dp){
+       if(idx>=n){
+           return 0;
+       }
+
+       if(dp[idx]!=-1){
+           return dp[idx];
+       }
+
+         int maxx_day_1 = days[idx]+1;   //when day=1 hoge jaise iss question me while loop nahi chalega:
+         int j = idx;
+         while(j<days.size() && days[j]<maxx_day_1){
+              j++;
+            }  
+
+        int cost_1_day = costs[0]+solve(j,days,costs,n,dp);   //only one day pass tha to idx --goto--> next index+1:
+     
+        int maxx_day_7 = days[idx]+7;
+        j=idx;
+        while(j<n && days[j]<maxx_day_7){
+            j++;
         }
-        return dp[0];
+     
+        int cost_7_day = costs[1]+solve(j,days,costs,n,dp);
+
+        int maxx_day_30  = days[idx]+30;
+        j=idx;
+        while(j<n && days[j]<maxx_day_30){
+            j++;
+        }
+
+        int cost_30_day = costs[2]+solve(j,days,costs,n,dp);
+
+        return dp[idx]=min({cost_1_day,cost_7_day,cost_30_day});
+   }
+
+
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        int n = days.size();
+        vector<int> dp(365,-1);
+        return solve(0,days,costs,n,dp);
     }
 };
