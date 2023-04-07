5ca1a96e9a5530ba1cf85a9f9e2f8e0c769200eb
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-04-07 13:38:06 +0000

    Sync LeetCode submission Runtime - 40 ms (1.87%), Memory - 25.7 MB (100.00%)

--- my-code/0189-rotate-array/solution.cpp
+++ my-code/0189-rotate-array/solution.cpp
@@ -1,17 +1,10 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        //deque take less time:
-        deque<int> d;
-        d.assign(nums.begin(),nums.end());
-        for(int i=1;i<=k;i++){
-            int val = d.back();
-            d.pop_back();
-            d.push_front(val);
-        }
-        nums.clear();
-        for(auto it=d.begin();it!=d.end();++it){
-            nums.push_back(*it);
-        }
+       vector<int> temp(nums.size());
+       for(int i=0;i<nums.size();i++){
+           temp[(i+k)%nums.size()]=nums[i];
+       }
+       nums = temp;
     }
 };
