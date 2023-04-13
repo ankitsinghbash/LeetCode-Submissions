cf56d2016950f4704ee45963386c7108007caa38
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-04-13 21:06:54 +0000

    Sync LeetCode submission Runtime - 17 ms (0.49%), Memory - 11.2 MB (100.00%)

--- my-code/1677-matrix-diagonal-sum/solution.cpp
+++ my-code/1677-matrix-diagonal-sum/solution.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    int diagonalSum(vector<vector<int>> &mat)
-     { 
-         int sum = 0;
-          int s = mat.size();
-          //size of row/col of matrix , both are equal as it is matrix
-           //for iteration through row 
-           for (int i = 0; i < s; i++) { 
-               //for iteration through col
-                for (int j = 0; j < s; j++)
-                 {
-               //i==j is for left to right 
-               //i+j==s-1 is for right to left 
- // using || operator we do not need to remove mid number 
-                      if (i == j || i + j == s - 1) { sum = sum + mat[i][j];
-                       } 
-                   } 
-              }
-               return sum ; 
-  }
+    int diagonalSum(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        int sum=0;
+        for(int i=0;i<matrix.size();i++){
+             for(int j=0;j<matrix[0].size();j++){
+                 if(i==j || i+j==n-1){
+                     sum+=matrix[i][j];
+                 }
+             }
+        }
+        return sum;
+    }
 };
