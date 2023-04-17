9a6a24534a0b4d90553d3844abeab04e9a1e3580
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-04-17 09:48:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.8 MB (100.00%)

---
+++ my-code/1528-kids-with-the-greatest-number-of-candies/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int maxx = 0;
+        for(int i=0;i<candies.size();i++){
+            maxx=max(maxx,candies[i]);
+        }
+        
+        vector<bool> ans;
+        for(int i=0;i<candies.size();i++){
+            if(candies[i]+extraCandies>=maxx){
+                ans.push_back(true);
+            }
+            else{
+                ans.push_back(false);
+            }
+        }
+        return ans;
+    }
+};
