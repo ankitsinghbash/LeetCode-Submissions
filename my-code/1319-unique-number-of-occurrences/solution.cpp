30f9ff664c51af3504a8d8645e53f0c029cf88d6
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-04-18 14:32:51 +0000

    Sync LeetCode submission Runtime - 3 ms (15.46%), Memory - 8.1 MB (100.00%)

--- my-code/1319-unique-number-of-occurrences/solution.cpp
+++ my-code/1319-unique-number-of-occurrences/solution.cpp
@@ -1,25 +1,34 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        unordered_map<int,int> mp;
-        for(int i=0;i<arr.size();i++)
-        {
-            mp[arr[i]]++;
+        int counter=1;
+        int i=1;
+        sort(arr.begin(),arr.end());
+        vector<int> ans;
+        while(i<arr.size()){
+            if(arr[i]==arr[i-1]){
+               counter++; 
+            }
+            else{
+                ans.push_back(counter);
+                counter=0;
+                counter=1;
+            }
+            i++;
         }
-        unordered_map<int,int> :: iterator it;
+        ans.push_back(counter);
+  
+        sort(ans.begin(),ans.end());
 
-        set<int> stt;
-        for(it=mp.begin();it!=mp.end();it++)
-        {
-            if(stt.find((*it).second)!=stt.end())
-            {
+        int j=1;
+        while(j<ans.size()){
+            if(ans[j]==ans[j-1]){
                 return false;
             }
-            else
-            {
-                stt.insert((*it).second);
-            }
+            j++;
         }
         return true;
+
+      
     }
 };
