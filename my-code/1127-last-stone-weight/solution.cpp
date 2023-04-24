4357ead3f2e6fcf434580c155d281e5f17cc4b6c
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-04-24 06:55:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.6 MB (100.00%)

--- my-code/1127-last-stone-weight/solution.cpp
+++ my-code/1127-last-stone-weight/solution.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    int lastStoneWeight(vector<int>& stone) {
-    priority_queue<int> qu; 
-    for(int i=0;i<stone.size();i++){
-        qu.push(stone[i]);
-    }
-    int first,second;
-    while(!qu.empty()){
-        if(qu.size()==1){
-            return qu.top();
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+        for(int i=0;i<stones.size();i++){
+            pq.push(stones[i]);
         }
-         first = qu.top();
-         qu.pop();
-         second = qu.top();
-         qu.pop();
-         if(first!=second){
-             qu.push(first-second);
-         }
-    }
-    return 0;
-    }
+ 
+        int first,second;
+        while(!pq.empty()){
+            if(pq.size()==1){
+                return pq.top();
+            }
+            int first=pq.top();
+            pq.pop();
+            int second = pq.top();
+            pq.pop();
+            if(first!=second){
+                pq.push(first-second);
+            }
+        }
+        return 0;
 
+    }
 };
