f2e591763ab82804a22a582c88d033520ee1d257
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-04-28 04:58:17 +0000

    Sync LeetCode submission Runtime - 3 ms (7.96%), Memory - 12.2 MB (99.92%)

--- my-code/0103-binary-tree-zigzag-level-order-traversal/solution.cpp
+++ my-code/0103-binary-tree-zigzag-level-order-traversal/solution.cpp
@@ -11,71 +11,37 @@
  */
 class Solution {
 public:
+    vector<vector<int>> ans;
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        if(root==NULL) return {};
+      if(root==NULL){
+          vector<vector<int>> check;
+          return check;
+      }
+  
+      int level=0;
+      queue<TreeNode*> qu;
+      qu.push(root);
+      while(!qu.empty()){
+          int size = qu.size();
+          vector<int> store;
+          while(size--){
+              TreeNode *node = qu.front();
+              qu.pop();
+              store.push_back(node->val);
+              if(node->left!=NULL){
+                  qu.push(node->left);
+              }
+              if(node->right!=NULL){
+                  qu.push(node->right);
+              }
+          }
+          level++;
+          if(level%2==0){
+             reverse(store.begin(),store.end());
+          }
+          ans.push_back(store);
+      }
+      return ans;
 
-        queue<TreeNode*> qu;
-        qu.push(root);
-        int level=0;
-        vector<vector<int>> real;
-        while(!qu.empty()){
-            int size = qu.size();
-            vector<int> ans;
-            while(size--){
-                TreeNode *node = qu.front();
-                qu.pop();
-                ans.push_back(node->val);
-                if(node->left!=NULL){
-                    qu.push(node->left);
-                }
-                if(node->right!=NULL){
-                    qu.push(node->right);
-                }
-            }
-            if(level%2!=0){
-                reverse(ans.begin(),ans.end());
-            }
-            real.push_back(ans);
-            level++;
-        }
-        return real;
     }
-    
 };
-
-
-// class Solution {
-// public:
-//     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-//         vector<vector<int>> real;
-//         if(root==NULL)
-//         return real;
-
-//         queue<TreeNode*> q;
-//         q.push(root);
-//         int level=0;
-//         while(!q.empty())
-//         {
-            
-//             int size=q.size();
-//             vector<int> ans;
-//             while(size--)
-//             {
-//                 TreeNode* node=q.front();
-//                 q.pop();
-//                 ans.push_back(node->val);
-//                 if(node->left!=NULL)
-//                 q.push(node->left);
-//                 if(node->right!=NULL)
-//                 q.push(node->right);
-//                 //not right condtion hare if even or odd that given error in case: [1,2,3,4,null,null,5]
-//             }
-//             if(level%2!=0)
-//                 reverse(ans.begin(),ans.end());
-//             real.push_back(ans);
-//             level++;
-            
-//         }
-//         return real;
-//     }
-// };
