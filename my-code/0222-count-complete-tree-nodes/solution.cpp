65beb9953f88b72efbed0477d06ccd4df886ed10
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-05-01 13:09:09 +0000

    Sync LeetCode submission Runtime - 34 ms (0.60%), Memory - 30.7 MB (99.94%)

--- my-code/0222-count-complete-tree-nodes/solution.cpp
+++ my-code/0222-count-complete-tree-nodes/solution.cpp
@@ -11,13 +11,13 @@
  */
 class Solution {
 public:
-   
-    int solve(TreeNode *root){
-        if(root==NULL) return 0;
+   int solve(TreeNode *root){
+       if(root==NULL) return 0;
 
-        return 1+solve(root->left)+solve(root->right);
+       return 1+solve(root->left)+solve(root->right);
+   }
+  
 
-    }
 
     int countNodes(TreeNode* root) {
         return solve(root);
