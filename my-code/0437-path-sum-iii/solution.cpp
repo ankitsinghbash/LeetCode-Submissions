bb8f56c4863937bb97cae9df6f01d3c182b8b9be
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-05-01 22:40:15 +0000

    Sync LeetCode submission Runtime - 39 ms (8.43%), Memory - 74.5 MB (8.43%)

--- my-code/0437-path-sum-iii/solution.cpp
+++ my-code/0437-path-sum-iii/solution.cpp
@@ -1,81 +1,66 @@
 class Solution {
 public:
-    //approach 1;
-   vector<long long> solve_postorder(TreeNode *root,int target,int &counter){
+   int cnt = 0;
+   void solve(TreeNode *root,int target,vector<int> path){
+      if(root==NULL) return;
+   
+      path.push_back(root->val);
+      solve(root->left,target,path);
+      solve(root->right,target,path);
 
-
-       if(root==NULL){
-           vector<long long> v;
-           return v;   //empty vector;
-       }
-
-
-       vector<long long> left_vector = solve_postorder(root->left,target,counter);
-       vector<long long > right_vector = solve_postorder(root->right,target,counter);
- 
-       vector<long long> curr_vector;
-       for(auto &Left : left_vector){
-           curr_vector.push_back(Left+root->val);
-       }
-
-       for(auto &Right : right_vector){
-           curr_vector.push_back(Right+root->val);
-       }
-       curr_vector.push_back(root->val);
-
-       //now find the target in current;
-       for(auto &x : curr_vector){
-           if(x==target){
-               counter++;
-           }
-       }
-
-       return curr_vector;
-
+      long long sum=0;
+      for(int i=path.size()-1;i>=0;i--){
+          sum+=path[i];
+          if(sum==target){
+              cnt++;
+          }
+      }
+      path.pop_back();
    }
- 
- 
+
+
+
     int pathSum(TreeNode* root, int targetSum) {
-        int counter=0;
         if(root==NULL) return 0;
 
-        solve_postorder(root,targetSum,counter);
-        return counter;
+        vector<int> path;
+        solve(root,targetSum,path);
+        return cnt;
     }
 };
 
-//approch 2 give tle understand how recursion work;
+
+// //method 1:
+// //MOst easy every sub-tree(left,right) -> pe jaao or target find karo every path me:
 // class Solution {
 // public:
+//     void dfs(TreeNode *root,long long target,int &cnt){
+//        if(root==NULL) return;
 
-//     void solve(TreeNode *root,int target,vector<int> &path,int &counter){
-
-//       if(root==NULL){
-//           return;
-//       }
-   
-//       path.push_back(root->val);
-//       solve(root->left,target,path,counter);
-//       solve(root->right,target,path,counter);
-
-//       int sum=0;
-//       for(int i=path.size()-1;i>=0;i--){
-//           sum+=path[i];
-//           cout<<path[i]<<" ";
-//           if(sum==target){
-//               counter++;
-//           }
-//       }
-//       cout<<endl;
-//       path.pop_back();
-
-    
+//        target=target-root->val;
+//        if(target==0){
+//            cnt++;
+//        }
+//        dfs(root->left,target,cnt);
+//        dfs(root->right,target,cnt);
 //     }
 
-//     int pathSum(TreeNode* root, int targetSum) {
-//         vector<int> path;
-//         int counter=0;
-//         solve(root,targetSum,path,counter);
-//         return counter;
+//     void solve(TreeNode *root,long long targetSum,int &cnt){
+//         if(root==NULL){
+//             return;
+//         }
+
+//         dfs(root,targetSum,cnt);    
+//         solve(root->left,targetSum,cnt);
+//         solve(root->right,targetSum,cnt);  
+
+//     }
+
+//     int pathSum(TreeNode* root, int targetSum){
+//         if(root==NULL) return 0;
+
+//         int cnt = 0;
+//         solve(root,targetSum,cnt);
+//         return cnt;
 //     }
 // };
