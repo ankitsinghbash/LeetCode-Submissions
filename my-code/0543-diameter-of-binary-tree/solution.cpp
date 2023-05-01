031e262af9540d3c5d192e549a55aaac6a04c3e6
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-05-01 23:13:07 +0000

    Sync LeetCode submission Runtime - 12 ms (18.31%), Memory - 20.5 MB (99.98%)

--- my-code/0543-diameter-of-binary-tree/solution.cpp
+++ my-code/0543-diameter-of-binary-tree/solution.cpp
@@ -1,36 +1,31 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    int ans;
 
-    int dfs(TreeNode *root){
-       if(root==NULL){
-           return 0;
-       }
+  int  solve(TreeNode *root,int &ans){
+    if(root==NULL) return 0;
 
-       int l  = dfs(root->left);
-       int r = dfs(root->right);
-       int koi_ek = max(l,r)+1;
-       int nicha_ho_jaay = l+r+1;
- 
-       ans=max({ans,koi_ek,nicha_ho_jaay});
-       return koi_ek;
-    }
+   
+    int left = solve(root->left,ans);
+    int right = solve(root->right,ans); 
+   
+
+    int only_one = max(left,right)+1;
+    int all = left+right+1;
+
+    ans = max({ans,only_one,all});
+    
+    return only_one;
+  }
+
+
+
+
 
     int diameterOfBinaryTree(TreeNode* root) {
-        ans=INT_MIN;
-        dfs(root);
-        ans-=1;  // -1 in final ans because output index start form 0 to count the size of path on diameter:
-        return ans;
+        if(root==NULL) return 0;
+        int ans = INT_MIN;
+        solve(root,ans);
+        return ans-1;
+
     }
 };
