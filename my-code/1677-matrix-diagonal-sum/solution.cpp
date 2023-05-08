b31edac4fee672d9823117f1a9d45c4760719c40
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-05-08 03:51:51 +0000

    Sync LeetCode submission Runtime - 13 ms (0.49%), Memory - 11.2 MB (100.00%)

--- my-code/1677-matrix-diagonal-sum/solution.cpp
+++ my-code/1677-matrix-diagonal-sum/solution.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-    int diagonalSum(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        int sum=0;
-        for(int i=0;i<matrix.size();i++){
-             for(int j=0;j<matrix[0].size();j++){
-                 if(i==j || i+j==n-1){
-                     sum+=matrix[i][j];
-                 }
-             }
+    int diagonalSum(vector<vector<int>>& mat) {
+        int sum = 0;
+        int n = mat.size();
+        int m = mat[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==j || i+j==n-1){
+                    sum+=mat[i][j];
+                }
+            }
         }
         return sum;
     }
