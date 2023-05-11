4b1c33c78b52da743c8bd782bef6bbccc7969499
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-05-11 09:04:24 +0000

    Sync LeetCode submission Runtime - 1389 ms (5.05%), Memory - 292.4 MB (31.81%)

--- my-code/0300-longest-increasing-subsequence/solution.cpp
+++ my-code/0300-longest-increasing-subsequence/solution.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
- 
-    int solve(vector<int> &nums,int idx,int n,int prev,vector<vector<int>> &dp){
-
- 
-       if(idx==n){
+   int solve(int idx,int prev,vector<int> &nums,vector<vector<int>> &dp){
+       if(idx>nums.size()){
            return 0;
        }
-       
-       if(dp[idx][prev+1]!=-1){
-           return dp[idx][prev+1];
+
+       if(dp[idx][prev]!=-1){
+           return dp[idx][prev];
        }
 
        int pick=0;
        int notpick=0;
-     //  notpick = 0+solve(nums,idx+1,n,prev,dp);
-       if(prev==-1|| nums[idx]>nums[prev]){
-           pick  = 1+solve(nums,idx+1,n,idx,dp);
-       }
-        notpick = 0+solve(nums,idx+1,n,prev,dp);
-      return dp[idx][prev+1]=max(pick,notpick);
+       notpick = solve(idx+1,prev,nums,dp);
+       if(prev == 0 || nums[idx-1]>nums[prev-1]){
+           pick = 1+solve(idx+1,idx,nums,dp);
+       } 
+       return dp[idx][prev] = max(pick,notpick);
+   }
 
-    }
+
 
 
     int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        vector<vector<int>> dp(n,vector<int>(n+1,-1));
-        return solve(nums,0,n,-1,dp);
+        int n = nums.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        return solve(1,0,nums,dp);
     }
 };
