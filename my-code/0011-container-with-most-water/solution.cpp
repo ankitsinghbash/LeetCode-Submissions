e387cdf60f1171067a7219702f3d5a525583d6d6
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-05-17 19:54:23 +0000

    Sync LeetCode submission Runtime - 116 ms (6.81%), Memory - 59 MB (99.94%)

--- my-code/0011-container-with-most-water/solution.cpp
+++ my-code/0011-container-with-most-water/solution.cpp
@@ -1,73 +1,21 @@
 class Solution {
 public:
-    int maxArea(vector<int>& height) {
-       //area of histogram me next greater element:
-       int i=0;
-       int j=height.size()-1;
-       int ans = INT_MIN;
-       while(i<j)
-       {
-          int maxarea = (j-i)*min(height[i],height[j]);
-          if(maxarea>ans)
-          {
-              ans=maxarea;
-          }
-          if(height[i]>height[j])
-          {
-              //mughe max area chayia hence if height[i]>height[j];
-              j--;
-          }
-          else{
-              i++;
-          }
-       }
-       return ans;
+    int maxArea(vector<int>& arr) {
+        int low = 0;
+        int high = arr.size()-1;
+        int maxx = 0;
+
+        while(low<high){
+           int mini = min(arr[low],arr[high]);
+           int water = (high-low)*mini;
+           maxx = max(maxx,water);
+           if(arr[low]<=arr[high]){
+               low++;
+           }
+           else{
+               high--;
+           }
+        }
+        return maxx;
     }
 };
-
-
-
-/*/
-//brute force:
- int maxstore = INT_MIN;
-        while(height.size()!=1)
-        {
-        
-        int idxzero=0;
-        int idxlast=height.size()-1;
-        int first=height[idxzero]; 
-        int last=height[idxlast];
-        int minimum;
-        if(first>last)
-        {
-            minimum=last;
-        }
-        else{
-            minimum=first;
-        }
-
-        int gaps=idxlast;           //gaps == indexing:
-
-        
-      
-        if(minimum*gaps>maxstore)
-        {
-           maxstore=minimum*gaps;
-        }
-        
-        //delete the minimum element of the idex:
-        if(height[0]==minimum)
-        {
-            //delete this:
-            height.erase(height.begin()+0);
-        }
-        else
-        {
-            //delete the last = height[height.size()-1];
-            height.erase(height.begin()+idxlast);
-        }
-
-        }
-        return maxstore;
-
-*/
