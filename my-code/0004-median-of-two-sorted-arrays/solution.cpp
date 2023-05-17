7b267f5322c26e7753a5304cafd460a101cb9455
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-05-17 20:32:55 +0000

    Sync LeetCode submission Runtime - 44 ms (6.07%), Memory - 90.2 MB (99.98%)

--- my-code/0004-median-of-two-sorted-arrays/solution.cpp
+++ my-code/0004-median-of-two-sorted-arrays/solution.cpp
@@ -1,36 +1,44 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        
-       vector<int> ans;
-        for(int i=0;i<nums1.size();i++)
-        {
-            ans.push_back(nums1[i]);
+        vector<int> store;
+        int i=0;
+        int j=0;
+        while(i<nums1.size() && j<nums2.size()){
+            if(nums1[i]<=nums2[j]){
+                store.push_back(nums1[i]);
+                i++;
+            }
+            else if(nums1[i]>nums2[j]){
+                store.push_back(nums2[j]);
+                j++;
+            }
         }
-        
-        //add second vector value:
-        for(int i=0;i<nums2.size();i++)
-        {
-            ans.push_back(nums2[i]);
+
+        while(i<nums1.size()){
+            store.push_back(nums1[i]);
+            i++;
         }
-        
-        sort(ans.begin(),ans.end());
-        //for even number of median:
-        if(ans.size()%2==0)
-        {
-          //even;
-           int mid1 = (ans.size()/2)-1;
-           int mid2 = ans.size()/2;
-           double result = (((double)ans[mid1]+(double)ans[mid2])/2);
-           return result;
-         }
-        else
-        {
-        //odd;
-        int mid = ans.size()/2;
-        double result = ((double)ans[mid]);
-        return result;
-       }
-     return -1;
+
+        while(j<nums2.size()){
+            store.push_back(nums2[j]);
+            j++;
+        }
+
+        int size = store.size();
+        int low  = 0;
+        int high = store.size()-1;
+        if(size%2==0){
+            int mid = low+(high-low)/2;
+            double total = store[mid]+store[mid+1];
+            double ans = total/2;
+            return ans;
+        }
+        else{
+            int mid = low+(high-low)/2;
+            double total = store[mid];
+            double ans = total/1.0;
+            return ans;
+        }
     }
 };
