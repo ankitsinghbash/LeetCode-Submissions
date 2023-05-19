79fb891d4f65eff41857271181f1df8d675ccffa
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-05-19 23:10:27 +0000

    Sync LeetCode submission Runtime - 36 ms (5.16%), Memory - 16.7 MB (100.00%)

--- my-code/0801-is-graph-bipartite/solution.cpp
+++ my-code/0801-is-graph-bipartite/solution.cpp
@@ -1,37 +1,97 @@
+//Method 1: BFS
 class Solution {
 public:
    
-   bool dfs(vector<vector<int>> &adj,int curr,vector<int> &color,int currcolor){
-        if(color[curr]!=-1) return true;
-  
-        color[curr]=currcolor;
-        for(auto &v : adj[curr]){
-            if(color[v]==color[curr]){
-                return false;
-            }
-            else if(color[v]==-1){
-                int colorv = 1-color[curr];
-                if(dfs(adj,v,color,colorv)==false){
-                    return false;
-                }
-            }
-        }
-        return true;
-
+   bool BFS(int curr_node,int curr_color,unordered_map<int,vector<int>> &adj,vector<int> &color){
+      queue<int> qu;
+      qu.push(curr_node);
+      color[curr_node]=curr_color;
+    
+      while(!qu.empty()){
+         int size = qu.size();
+         while(size--){
+             int u = qu.front();
+             qu.pop();
+             for(auto &v : adj[u]){
+                 if(color[v]==color[u]){
+                     return false;
+                 }
+                 else if(color[v]==-1){
+                     color[v]=1-color[u];
+                     qu.push(v);
+                 }
+             }
+         }
+      }
+      return true;
    }
 
 
 
-    bool isBipartite(vector<vector<int>>& adj) {
-        int V = adj.size();
-        vector<int> color(V,-1);
-        for(int i=0;i<V;++i){
+    bool isBipartite(vector<vector<int>>& graph) {
+        int V=graph.size();
+        unordered_map<int,vector<int>> adj;
+        for(int i=0;i<graph.size();i++){
+            for(int j=0;j<graph[i].size();j++){
+                int v = graph[i][j];
+
+                adj[i].push_back(v);
+            }
+        }
+
+
+        vector<int> color(V,-1);   //also work as a visited:
+        for(int i=0;i<V;i++){
             if(color[i]==-1){
-                if(dfs(adj,i,color,1)==false){
+                if(BFS(i,0,adj,color)==false){
                     return false;
                 }
             }
         }
-        return true;
+       return true;
     }
 };
+
+//Method 2: DFS
+// class Solution {
+// public:
+   
+//    bool dfs(int curr_node,int curr_color,unordered_map<int,vector<int>> &adj,vector<int> &color){
+//       if(color[curr_node]!=-1) return true;
+
+//       color[curr_node] = curr_color;
+//       for(auto &v : adj[curr_node]){
+//           if(color[v]==color[curr_node])return false;
+//           else if(color[v]==-1){
+//               int color_v = 1-color[curr_node];
+//               if(dfs(v,color_v,adj,color)==false){
+//                   return false;
+//               }
+//           }
+//       }
+//       return true; 
+//    }
+
+//     bool isBipartite(vector<vector<int>>& graph) {
+//         int V =  graph.size();
+
+//         unordered_map<int,vector<int>> adj;
+//         for(int i=0;i<graph.size();i++){
+//             for(int j=0;j<graph[i].size();j++){
+//                 int v = graph[i][j];
+
+//                 adj[i].push_back(v);
+//             }
+//         }
+
+//         vector<int> color(V,-1);
+//         for(int i=0;i<V;i++){
+//             if(color[i]==-1){
+//                 if(dfs(i,0,adj,color)==false){
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+// };
