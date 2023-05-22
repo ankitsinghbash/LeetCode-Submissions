6b36a139dc5a10c62ddaa9493626fad983a5eb90
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-05-22 22:24:36 +0000

    Sync LeetCode submission Runtime - 20 ms (8.40%), Memory - 13.5 MB (100.00%)

--- my-code/0347-top-k-frequent-elements/solution.cpp
+++ my-code/0347-top-k-frequent-elements/solution.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
+   typedef pair<int,int> P;
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        priority_queue<pair<int,int>> qu;   //add from high to low:
         unordered_map<int,int> mp;
-        for(auto &x : nums){
-            mp[x]++;
-        }
-
-        for(auto &x : mp){
-            qu.push({x.second,x.first}); //because mai second ke hisaab se sort karna chayiata hu:
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
         }
         
-        vector<int> ans;
-        while(k--){
-            ans.push_back(qu.top().second);
-            qu.pop();
-        }
-        return ans;
+         priority_queue<P> pq;
+         for(auto it=mp.begin();it!=mp.end();it++){
+             pq.push({it->second,it->first});
+         }
+
+         vector<int> ans;
+         while(pq.size()>0 && k--){
+             pair<int,int> Pair = pq.top();
+             pq.pop();
+             ans.push_back(Pair.second);
+         }
+         return ans;
+         
 
     }
 };
