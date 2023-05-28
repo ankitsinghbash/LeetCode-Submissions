9b3386e5902e30c89757ff7063948f9710e6f43d
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-05-28 00:40:30 +0000

    Sync LeetCode submission Runtime - 74 ms (10.30%), Memory - 13.5 MB (71.37%)

--- my-code/1240-stone-game-ii/solution.cpp
+++ my-code/1240-stone-game-ii/solution.cpp
@@ -1,41 +1,39 @@
 class Solution {
 public:
+   int solve(int i,int player1,int M,vector<int> &piles,vector<vector<vector<int>>> &dp){ //player1 indicate alice:
+         if(i>=piles.size()) return 0;
 
-        int dp[101][201];
-
-    int helper(int i,int m,vector<int>& piles){
-
-         if(i >= piles.size())
-         return 0;
-
-         if(dp[i][m] != -1)
-         return dp[i][m];
-
-         int total = 0;
-         int ans = INT_MIN;
-
-         for(int j=0;j<2*m;j++){
-
-             if(i+j < piles.size())
-              total += piles[i+j];
-
-              ans = max(ans,total - helper(i+j+1,max(m,j+1),piles));  
+         if(dp[player1][i][M]!=-1){
+             return dp[player1][i][M];
          }
 
-        return dp[i][m] = ans;
-    }
+         int result = (player1==1) ? INT_MIN : INT_MAX;
+
+         int total = 0;
+         for(int j=0;j<2*M;j++){
+              
+             if(i+j<piles.size()){ 
+             total+=piles[i+j];
+             }
+
+             if(player1==1){
+                 //alice:
+                 result = max(result,total+solve(i+j+1,0,max(M,j+1),piles,dp)); //add total for alice only:
+             }
+             else{
+                 result = min(result,solve(i+j+1,1,max(M,j+1),piles,dp));
+             }
+         }
+        return dp[player1][i][M] =  result;
+
+
+   }
+
+
+
     int stoneGameII(vector<int>& piles) {
-
-
-        memset(dp,-1,sizeof dp);
-        
-        int sum = 0;
-        for(auto x:piles)
-         sum += x;
-
-        
-         int diff = helper(0,1,piles);
-        
-        return (sum+diff)/2;
+        int n = piles.size();
+        vector<vector<vector<int>>> dp(2,vector<vector<int>>(n+1,vector<int>(n+1,-1)));
+        return solve(0,1,1,piles,dp); 
     }
 };
