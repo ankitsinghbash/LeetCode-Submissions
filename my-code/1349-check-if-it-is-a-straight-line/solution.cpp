1fc3889135beb83084f588ee59ee05266a728eed
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-06-05 18:54:35 +0000

    Sync LeetCode submission Runtime - 12 ms (1.95%), Memory - 10.2 MB (100.00%)

---
+++ my-code/1349-check-if-it-is-a-straight-line/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& coordinates) {
+
+        for(int i=2;i<coordinates.size();i++){
+            //new slope:
+            int x = coordinates[i-2][0];
+            int y = coordinates[i-2][1];
+
+            int x1 = coordinates[i-1][0];
+            int y1 = coordinates[i-1][1];
+     
+            int x2 = coordinates[i][0];
+            int y2 = coordinates[i][1];
+
+            if((x1-x)*(y2-y1)!=(x2-x1)*(y1-y)){
+                return false;
+            }
+        }
+        return true;
+
+    }
+};
