efe65daabc049e941307b76a90f8a7fa5c2c4014
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-06-06 00:37:19 +0000

    Sync LeetCode submission Runtime - 29 ms (5.25%), Memory - 14.6 MB (100.00%)

--- my-code/0547-number-of-provinces/solution.cpp
+++ my-code/0547-number-of-provinces/solution.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
-  void dfs(int u,vector<bool> &visited,unordered_map<int,vector<int>> &adjlist){
-       
-       if(visited[u]==true) return;
-       
-       visited[u]=true;
-       for(auto &x : adjlist[u]){
-           if(!visited[x]){
-               dfs(x,visited,adjlist);
-           }
-       }
-       
-       
+   void dfs(int u,unordered_map<int,vector<int>> &adj,vector<bool> &visited){
+      if(visited[u]==true) return;
+
+      visited[u]=true;
+      for(auto &v : adj[u]){
+          if(visited[v]==false){
+              dfs(v,adj,visited);
+          }
+      }
    }
 
 
-    int findCircleNum(vector<vector<int>>& adj) {
-         unordered_map<int,vector<int>> adjlist;
-        for(int i=0;i<adj.size();i++)
-        {
-            for(int j=0;j<adj[0].size();j++)
-            {
-                if(i!=j && adj[i][j]==1)
-                {
-                    adjlist[i].push_back(j);
-                    
+
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        unordered_map<int,vector<int>> adj;
+        for(int i=0;i<isConnected.size();i++){
+            for(int j=0;j<isConnected[0].size();j++){
+                if(i!=j && isConnected[i][j]==1){
+                    adj[i].push_back(j);
                 }
             }
         }
-        
-        vector<bool> visited(adj.size(),false);
-        int counter=0;
-        for(int i=0;i<adj.size();i++){
-            if(!visited[i]){
-            counter++;
-            dfs(i,visited,adjlist);
-            }
+
+        int n = isConnected.size();
+        vector<bool> visited(n,false);
+        int cnt = 0;
+        for(int i=0;i<n;i++){
+             if(visited[i]==false){
+                 cnt++;
+                 dfs(i,adj,visited);
+             }
         }
-        return counter;
+        return cnt;
     }
 };
