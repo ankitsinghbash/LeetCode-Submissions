898d8b7396fb2bce1753c678f2b9c064b5fdbdd6
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-06-21 15:59:55 +0000

    Sync LeetCode submission Runtime - 15 ms (0.67%), Memory - 7.2 MB (100.00%)

--- my-code/0048-rotate-image/solution.cpp
+++ my-code/0048-rotate-image/solution.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        
-        //first we have to find the transpose of the matrix:
-        //then we swaping the col form start of end:
-        //transpose:
-        int n=matrix.size();
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i;j<n;j++) //j=i true / not j=0; important condition of transpose:
-            {
-                swap(matrix[i][j],matrix[j][i]);
-            }
+        vector<vector<int>> store;
+        for(int j=0;j<matrix[0].size();j++){
+             vector<int> sm;
+             for(int i=matrix.size()-1;i>=0;i--){
+                  sm.push_back(matrix[i][j]);
+                  cout<<matrix[i][j]<<" ";
+             }
+            store.push_back(sm);
         }
         
-        //form above we find the transpose:
-        //Now swap the column from start to end:
-        for(int i=0;i<n;i++)  //go to every row:
-        {
-            int mincol=0;
-            int maxcol=n-1;
-            while(mincol<maxcol)
-            {
-                swap(matrix[i][mincol],matrix[i][maxcol]);
-                mincol++;
-                maxcol--;
-            }
-        }
         
+        for(int i=0;i<store.size();i++){
+            for(int j=0;j<store[0].size();j++){
+                matrix[i][j]=store[i][j];
+            }
+            
+        }
     }
 };
