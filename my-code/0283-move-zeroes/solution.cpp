187a6dba930a6eca79e8d5129dd8657c76f349c1
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-06-21 15:27:18 +0000

    Sync LeetCode submission Runtime - 24 ms (7.08%), Memory - 19.2 MB (100.00%)

--- my-code/0283-move-zeroes/solution.cpp
+++ my-code/0283-move-zeroes/solution.cpp
@@ -1,31 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        
-       vector<int> nums1;
-       int counter=0;  //count the number of zeros;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]==0)
-            {
-                counter++;
-            }
-            else
-            {
-                nums1.push_back(nums[i]);   //when nums[i]!=0;
+        int idx=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                swap(nums[i],nums[idx]);
+                idx++;
             }
         }
-        
-        while(counter!=0)
-        {
-            nums1.push_back(0);
-            counter--;
-        }
-        
-        for(int i=0;i<nums1.size();i++)
-        {
-            nums[i]=nums1[i];
-        }
-        
     }
 };
