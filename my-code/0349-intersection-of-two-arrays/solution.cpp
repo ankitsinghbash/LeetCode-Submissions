0cb9003c1780423d0bd6a5fdaf849acebff502ba
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-06-21 14:21:29 +0000

    Sync LeetCode submission Runtime - 3 ms (37.95%), Memory - 11.1 MB (100.00%)

--- my-code/0349-intersection-of-two-arrays/solution.cpp
+++ my-code/0349-intersection-of-two-arrays/solution.cpp
@@ -1,12 +1,28 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int,int>m;
-        for(int i=0;i<nums1.size();i++) m[nums1[i]]++;
-        set<int>s;
-        for(int i=0;i<nums2.size();i++) s.insert(nums2[i]);
-        vector<int>ans;
-        for(auto x:s) if(m[x]>0) ans.push_back(x);
-        return ans;
+        vector<int> ans;
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums1.size();i++){
+            mp[nums1[i]]++;
+        }
+
+       set<int> s;
+       for(int i=0;i<nums2.size();i++){
+           s.insert(nums2[i]);
+       } 
+
+       for(auto it = s.begin();it!=s.end();it++){
+           int value = *it;
+           if(mp[value]>0){
+               ans.push_back(value);
+               mp[value]--;
+           }
+       }
+     
+      
+      
+
+       return ans;
     }
 };
