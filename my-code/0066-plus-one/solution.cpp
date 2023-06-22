177e91367cb967d38e0feae6f1332a6046e86ce4
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-06-22 03:05:33 +0000

    Sync LeetCode submission Runtime - 7 ms (0.08%), Memory - 8.7 MB (100.00%)

--- my-code/0066-plus-one/solution.cpp
+++ my-code/0066-plus-one/solution.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    vector<int> plusOne(vector<int>& d) {
-        
-        //best solution:
-        int n=d.size();
-        int c=1;  //take c is a number in first time;
-        for(int i=n-1;i>=0;i--)
-        {
-            int last=d[i]+c;  //c is not carry it;s a numbe=1 in fist time;
-            //Now i have two option fisrt->last=10, or last= one digit number;
-            if(last==10) //last me only +1 karne hai to 10 hi hoge nahi to last>10
-            {
-                d[i]=0;
-                c=1;
+    vector<int> ans;
+    vector<int> plusOne(vector<int>& digits) {
+        reverse(digits.begin(),digits.end());
+        int carry =0;
+        int i=0;
+        while(carry>=0 && i<digits.size()){
+            int value;
+            if(i==0){
+              value = digits[i]+1+carry;
             }
-            else
-            {
-                 d[i]=last;
-                 c=0;
+            else{
+             value = digits[i]+carry;
             }
+            ans.push_back(value%10);
+            carry = value/10;
+            i++;
         }
-         if(c==1)    //case for [9]
-             d.insert(d.begin(),1); 
-        return d;
+        if(carry>0){
+            ans.push_back(carry);
+        }
         
-       
         
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
