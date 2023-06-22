bfcb0a42693505fe4387775d4d4f9f76443b8538
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-06-22 05:26:34 +0000

    Sync LeetCode submission Runtime - 27 ms (2.04%), Memory - 23.2 MB (100.00%)

--- my-code/0344-reverse-string/solution.cpp
+++ my-code/0344-reverse-string/solution.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        
-        vector<char> second;
-        for(int i=s.size()-1;i>=0;i--)
-        {
-            second.push_back(s[i]);
-        }
-        
-        for(int i=0;i<second.size();i++)
-        {
-            s[i]=second[i];
-        }
-        
+        int low = 0;
+        int high = s.size()-1;
+         while(low<high){
+             swap(s[low],s[high]);
+             low++;
+             high--;
+         }
     }
 };
