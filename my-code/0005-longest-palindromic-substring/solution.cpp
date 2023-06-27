e13cb7300368f59f6293eb66e7dc3d1f4e702d5d
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-06-27 17:14:44 +0000

    Sync LeetCode submission Runtime - 651 ms (10.64%), Memory - 22.6 MB (32.27%)

--- my-code/0005-longest-palindromic-substring/solution.cpp
+++ my-code/0005-longest-palindromic-substring/solution.cpp
@@ -1,32 +1,41 @@
-class Solution
- { 
-     public: 
-     string longestPalindrome(string s) { 
-    vector<vector<int> > dp(2, vector<int>(s.size() + 10)); 
-    int n = (int)s.size() - 1; 
-    dp[0][n] = 1;
-     int mxi = n, mxj = n; 
-     bool last = 0; 
- for(int i = (int)s.size() - 2; i >= 0 ; --i)
-      { 
-    last = !last;
-   for(int j = i; j < s.size(); ++j) 
-   {
-        if(i == j)
-         dp[last][j] = 1;
-        else if(j - i == 1)
-      dp[last][j] = ((s[i] == s[j]) ? 1 : 0);
-    else
-  dp[last][j] = ((s[i] == s[j] && dp[!last][j - 1] == 1) ? 1 : 0);
-   if(dp[last][j] == 1)
-    {
-        if(j - i > mxj - mxi)
-         {
-            mxj = j; mxi = i;
-         }
-         }
-     } 
-     }
- return s.substr(mxi, mxj - mxi + 1); 
- }
- };
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        vector<vector<bool>> dp(s.size(),vector<bool>(s.size()));
+    
+        int start;
+        int last;
+        for(int g=0;g<s.size();g++){
+            for(int i=0,j=g;j<s.size();i++,j++){
+                if(g==0){
+                   dp[i][j]=true;
+                }
+                else if(g==1){
+                    if(s[i]==s[j]){
+                        dp[i][j]=true;
+                    }
+                    else{
+                        dp[i][j]=false;
+                    }
+                }
+                else{
+                     if(s[i]==s[j] && dp[i+1][j-1]==true){
+                         dp[i][j]=true;
+                     }
+                     else{
+                         dp[i][j]=false;
+                     }
+                }
+
+                if(dp[i][j]==true){
+                    start=i;
+                    last=j;
+                }
+            }
+        }
+
+        string str = s.substr(start,last-start+1);
+        return str;
+
+    }
+};
