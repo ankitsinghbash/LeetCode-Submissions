cb857acd1d3b7a6e7fac3ebc8518eb28caccf8e8
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-06-27 05:11:53 +0000

    Sync LeetCode submission Runtime - 14 ms (23.09%), Memory - 5.9 MB (100.00%)

--- my-code/1013-fibonacci-number/solution.cpp
+++ my-code/1013-fibonacci-number/solution.cpp
@@ -1,43 +1,18 @@
-
 class Solution {
 public:
-    int helper(int n,unordered_map<int,int> &dp)
-    {
-        if(n==0||n==1)
-        {
-            return n;
-        }
+   int solve(int n){
+       if(n==0 || n==1){
+           return n;
+       }
 
-      if(dp.find(n)!=dp.end()){
-          return dp[n];
-      }
-        
-      int ans=helper(n-1,dp)+helper(n-2,dp);
-      return dp[n]=ans;
-    }
+       int value = solve(n-1)+solve(n-2);
+       return value;
+   }
+ 
+
+
 
     int fib(int n) {
-        unordered_map<int,int> dp;
-         int solution = helper(n,dp); 
-         return solution;
+        return solve(n);
     }
 };
-
-
-
-/*
-
-class Solution {
-public:
-    int fib(int n) {
-        int dp[n+2];   //this is helpful when user input the value n=0: so always allign in range:
-         dp[0]=0;
-        dp[1]=1;
-        for(int i=2;i<=n;i++)
-        {
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n];
-    }
-};
-*/
