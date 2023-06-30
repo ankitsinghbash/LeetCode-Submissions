60dbc389551dfb85d236503419057c5e327b4eda
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-06-30 15:26:15 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 5.9 MB (100.00%)

--- my-code/0070-climbing-stairs/solution.cpp
+++ my-code/0070-climbing-stairs/solution.cpp
@@ -1,31 +1,15 @@
 class Solution {
 public:
-
-    int solve(int n,int idx,int dp[]){
-  
-       if(idx==n){
-           return 1;
-       }
-
-       if(idx>n){
-           return 0;
-       }
-
-       if(dp[idx]!=-1){
-           return dp[idx];
-       }
-      
-        int step1  = solve(n,idx+1,dp);
-        int step2  = solve(n,idx+2,dp);
-
-        return dp[idx]=step1+step2;
-
-
-    }
-
+    
     int climbStairs(int n) {
-       int dp[46];
-       memset(dp,-1,sizeof(dp));
-       return solve(n,0,dp);
+        //tabulation:
+        int dp[46];  
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<n+1;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
