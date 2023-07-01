0648cbc1725e808f176c6d98bb5734bf353ee74b
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-07-01 10:32:52 +0000

    Sync LeetCode submission Runtime - 4 ms (4.23%), Memory - 7.9 MB (100.00%)

--- my-code/0063-unique-paths-ii/solution.cpp
+++ my-code/0063-unique-paths-ii/solution.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int n=obstacleGrid.size();
+        int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        vector<vector<int>> dp(n+1,vector<int>(m+1));
         for(int i=0;i<n+1;i++){
             dp[i][0]=0;
         }
+   
         for(int j=0;j<m+1;j++){
-            if(j==1){
-                dp[0][j]=1;
-            }
-            else
-            {
-                dp[0][j]=0;
-            }
+             if(j==1){
+                 dp[0][j]=1;    //base case if obstacle come means path not exist:
+             }
+             else{
+                 dp[0][j]=0;
+             }
         }
-        
+
         for(int i=1;i<n+1;i++){
             for(int j=1;j<m+1;j++){
-                if(obstacleGrid[i-1][j-1]!=1){
+                if(obstacleGrid[i-1][j-1]==1){
+                    dp[i][j]=0;
+                }
+                else{
                     dp[i][j]=dp[i-1][j]+dp[i][j-1];
                 }
             }
         }
         return dp[n][m];
-
     }
 };
