642e9d576cc56604c6a37929b4837eccf576abfb
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-07-04 15:01:48 +0000

    Sync LeetCode submission Runtime - 2114 ms (5.07%), Memory - 7 MB (100.00%)

--- my-code/1418-fair-distribution-of-cookies/solution.cpp
+++ my-code/1418-fair-distribution-of-cookies/solution.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
-    void helper(int start, const vector<int>& cookies, vector<int>& temp, int k, int& ans) {
-        int n = cookies.size();
-        if (start == n) {
-            int maxi = INT_MIN;
-            for (int i = 0; i < k; i++) {
-                maxi = max(maxi, temp[i]);
-            }
-            ans = min(ans, maxi);
-            return;
-        }
+   void solve(vector<int> &cookies,int k,int &result,vector<int> &box,int idx){
+       if(idx>=cookies.size()){
+           int max_value = *max_element(box.begin(),box.end());
+           result = min(result,max_value);
+           return;
+       }
 
-        for (int i = 0; i < k; i++) {
-            temp[i] += cookies[start]; // take
-            helper(start + 1, cookies, temp, k, ans);
-            temp[i] -= cookies[start]; // back-track
-        }
-    }
+
+
+       for(int i=0;i<k;i++){
+           box[i]+=cookies[idx];
+           solve(cookies,k,result,box,idx+1);
+           box[i]-=cookies[idx];
+       }
+  
+
+   }
+
+
+
 
     int distributeCookies(vector<int>& cookies, int k) {
-        int ans = INT_MAX;
-        vector<int> temp(k, 0);
-        helper(0, cookies, temp, k, ans);
-        return ans;
+        vector<int> box(k,0);
+        int result = INT_MAX;
+        solve(cookies,k,result,box,0);
+        return result;
     }
 };
