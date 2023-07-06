d24aed8a5156fe414ae0ed9afc39ea6bccbac380
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-07-06 18:17:25 +0000

    Sync LeetCode submission Runtime - 3 ms (1.52%), Memory - 8.2 MB (100.00%)

--- my-code/0206-reverse-linked-list/solution.cpp
+++ my-code/0206-reverse-linked-list/solution.cpp
@@ -10,17 +10,14 @@
  */
 class Solution {
 public:
-   
-   ListNode *solve(ListNode *head,ListNode *prev){
-     if(head==NULL) return prev;
-  
-     ListNode *temp = head->next;
-     head->next=prev;
-     return solve(temp,head);
-   }
-
-
-
+    ListNode *solve(ListNode *head,ListNode *prev){
+        if(head==NULL) return prev;
+        ListNode *temp = head->next;
+        head->next=prev;
+        return solve(temp,head);
+    }
+    
+    
     ListNode* reverseList(ListNode* head) {
         return solve(head,NULL);
     }
