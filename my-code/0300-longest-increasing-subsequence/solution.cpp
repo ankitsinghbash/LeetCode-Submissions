f45c4e4cf2f4656d97b742cd2c507da60711fee7
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-07-06 05:14:51 +0000

    Sync LeetCode submission Runtime - 394 ms (25.27%), Memory - 10.5 MB (100.00%)

--- my-code/0300-longest-increasing-subsequence/solution.cpp
+++ my-code/0300-longest-increasing-subsequence/solution.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
-   int solve(int idx,int prev,vector<int> &nums,vector<vector<int>> &dp){
-       if(idx>nums.size()){
-           return 0;
-       }
-
-       if(dp[idx][prev]!=-1){
-           return dp[idx][prev];
-       }
-
-       int pick=0;
-       int notpick=0;
-       notpick = solve(idx+1,prev,nums,dp);
-       if(prev == 0 || nums[idx-1]>nums[prev-1]){
-           pick = 1+solve(idx+1,idx,nums,dp);
-       } 
-       return dp[idx][prev] = max(pick,notpick);
-   }
-
-
-
-
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        return solve(1,0,nums,dp);
+        vector<int> dp(n,1);
+        for(int i=0;i<n;i++){
+            int maxx=1;
+            for(int j=i-1;j>=0;j--){
+                if(nums[i]>nums[j]){
+                 int value = max(dp[i],dp[j]+1);
+                 maxx = max(maxx,value);
+                }
+            }
+            dp[i]=maxx;
+        }
+         
+        int maxx = INT_MIN;
+        for(int i=0;i<n;i++){
+            maxx = max(maxx,dp[i]);
+        }
+        return maxx;
+
     }
 };
