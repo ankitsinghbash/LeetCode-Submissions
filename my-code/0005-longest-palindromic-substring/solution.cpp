586b5726471db62f6fab1c3e734c44c68b1a4529
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-07-15 07:58:08 +0000

    Sync LeetCode submission Runtime - 742 ms (10.29%), Memory - 22.6 MB (32.27%)

--- my-code/0005-longest-palindromic-substring/solution.cpp
+++ my-code/0005-longest-palindromic-substring/solution.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        vector<vector<bool>> dp(s.size(),vector<bool>(s.size()));
-    
         int start;
         int last;
+        vector<vector<bool>> dp(s.size(),vector<bool>(s.size()));
         for(int g=0;g<s.size();g++){
             for(int i=0,j=g;j<s.size();i++,j++){
                 if(g==0){
-                   dp[i][j]=true;
+                    dp[i][j]=true;
                 }
                 else if(g==1){
                     if(s[i]==s[j]){
@@ -19,23 +18,20 @@ public:
                     }
                 }
                 else{
-                     if(s[i]==s[j] && dp[i+1][j-1]==true){
-                         dp[i][j]=true;
-                     }
-                     else{
-                         dp[i][j]=false;
-                     }
+                    if(dp[i+1][j-1]==true && s[i]==s[j]){
+                        dp[i][j]=true;
+                    }
+                    else{
+                        dp[i][j]=false;
+                    }
                 }
-
                 if(dp[i][j]==true){
-                    start=i;
-                    last=j;
+                    start = i;
+                    last = j;
                 }
             }
         }
-
         string str = s.substr(start,last-start+1);
         return str;
-
     }
 };
