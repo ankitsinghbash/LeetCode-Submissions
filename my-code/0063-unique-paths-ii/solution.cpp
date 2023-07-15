9de260f4fe06af2bafb4f8cc38f74fc26fadb0e5
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-07-15 09:11:08 +0000

    Sync LeetCode submission Runtime - 8 ms (2.55%), Memory - 7.8 MB (100.00%)

--- my-code/0063-unique-paths-ii/solution.cpp
+++ my-code/0063-unique-paths-ii/solution.cpp
@@ -1,32 +1,38 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+   int solve(int i,int j,int n,int m,vector<vector<int>> &obstacleGrid,vector<vector<int>> &dp){
+
+     if(i<0 || j<0 || i>=n || j>=m){
+         return 0;
+     }
+
+     if(dp[i][j]!=-1) return dp[i][j];
+
+     
+
+     if(obstacleGrid[i][j]==1){
+         return 0;
+     }
+
+     if(i==n-1 && j==m-1){
+         return 1;
+     }
+
+     int right = solve(i+1,j,n,m,obstacleGrid,dp);
+     int down =  solve(i,j+1,n,m,obstacleGrid,dp);
+   
+     return  dp[i][j] = right+down;
+   }
+
+
+
+
+
+
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid){
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1));
-        for(int i=0;i<n+1;i++){
-            dp[i][0]=0;
-        }
-   
-        for(int j=0;j<m+1;j++){
-             if(j==1){
-                 dp[0][j]=1;    //base case if obstacle come means path not exist:
-             }
-             else{
-                 dp[0][j]=0;
-             }
-        }
-
-        for(int i=1;i<n+1;i++){
-            for(int j=1;j<m+1;j++){
-                if(obstacleGrid[i-1][j-1]==1){
-                    dp[i][j]=0;
-                }
-                else{
-                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
-                }
-            }
-        }
-        return dp[n][m];
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        return solve(0,0,n,m,obstacleGrid,dp);
     }
 };
