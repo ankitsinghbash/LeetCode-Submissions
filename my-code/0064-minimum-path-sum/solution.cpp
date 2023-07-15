37b9d77298ee8459660ab3611b8ef4627b2f07b7
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-07-15 10:14:25 +0000

    Sync LeetCode submission Runtime - 17 ms (5.52%), Memory - 10 MB (100.00%)

--- my-code/0064-minimum-path-sum/solution.cpp
+++ my-code/0064-minimum-path-sum/solution.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
-   
-   
-    int solve(int i,int j,vector<vector<int>> &grid,int n,int m,vector<vector<int>> &dp){
-
-        if(i==n-1&&j==m-1){
-            return grid[i][j];
+    int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> dp(n,vector<int>(m));
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(i==n-1 && j==m-1){
+                    dp[i][j] = grid[i][j]; 
+                }
+                else if(i==n-1){
+                    dp[i][j] = dp[i][j+1]+grid[i][j];
+                }
+                else if(j==m-1){
+                    dp[i][j] = dp[i+1][j] + grid[i][j];
+                }
+                else{
+                    dp[i][j] = grid[i][j] + min(dp[i+1][j],dp[i][j+1]);
+                }
+            }
         }
+        return dp[0][0];
 
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
 
-        if(i>=n||j>=m){
-            return 1e9;
-        }
-
-     
-
-        int ans =grid[i][j]+min(solve(i+1,j,grid,n,m,dp),solve(i,j+1,grid,n,m,dp));
-       return dp[i][j]=ans;
-    }
-
-     int minPathSum(vector<vector<int>>& grid) {
-         int n=grid.size();
-         int m =grid[0].size();
-         vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-         return solve(0,0,grid,n,m,dp); 
-       
     }
 };
