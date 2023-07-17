6e87c7a0940e529876f389b8c0e13529b4dc9c8c
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-07-17 08:03:00 +0000

    Sync LeetCode submission Runtime - 26 ms (5.02%), Memory - 39 MB (19.18%)

--- my-code/0113-path-sum-ii/solution.cpp
+++ my-code/0113-path-sum-ii/solution.cpp
@@ -1,41 +1,25 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
- 
-   void solve(TreeNode *root,int target,vector<vector<int>> &ans,vector<int> sm){
+    vector<vector<int>> store;
+    void solve(TreeNode *root,int target,vector<int> sm){
+       if(root==NULL) return;
 
-    if(root==NULL)return;
+       target=target-root->val;
+       sm.push_back(root->val);
 
-    target=target-root->val;
-    sm.push_back(root->val);
-
-    if(target==0 && root->left==NULL && root->right==NULL){
-        ans.push_back(sm);
+       if(target==0 && root->left==NULL && root->right==NULL){
+           store.push_back(sm);
+       }
+      
+      solve(root->left,target,sm);
+      solve(root->right,target,sm);
+      sm.pop_back();
     }
-
-    solve(root->left,target,ans,sm);
-    solve(root->right,target,ans,sm);
-    sm.pop_back();
-    
-   }
-
-
 
 
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        vector<vector<int>> ans;
         vector<int> sm;
-        solve(root,targetSum,ans,sm);
-        return ans;
+        solve(root,targetSum,sm);
+        return store;
     }
 };
