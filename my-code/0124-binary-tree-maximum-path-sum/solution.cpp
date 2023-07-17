2e577ea571925d8eb7222ca485ee6c98c29560d1
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-07-17 08:19:21 +0000

    Sync LeetCode submission Runtime - 31 ms (4.31%), Memory - 28.3 MB (13.02%)

--- my-code/0124-binary-tree-maximum-path-sum/solution.cpp
+++ my-code/0124-binary-tree-maximum-path-sum/solution.cpp
@@ -1,41 +1,23 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    int solve(TreeNode *root,int &ans){
-      if(root==NULL){
-        return 0;
-      }
-  
-      int left =  solve(root->left,ans);
-      int right = solve(root->right,ans);
+  int solve(TreeNode *root,int &maxx){
+     if(root==NULL) return 0;
 
+    int left = solve(root->left,maxx);
+    int right = solve(root->right,maxx);
 
-      int first = max(left,right)+root->val;
-      int second = root->val;
-      int third = left+right+root->val;
-   
-      ans = max({ans,first,second,third});
-
-      return max(first,second);   //third isslyia nahi because root->ek path me hona chayia: agar third return means we break the condition of path:
-   
-    }
-
+    int first = root->val+max(left,right);
+    int second = root->val;
+    int third = root->val+left+right;
+    maxx = max({first,second,third,maxx});
+    return max(first,second);
+  }
 
 
 
     int maxPathSum(TreeNode* root) {
-        int ans = INT_MIN;
-        solve(root,ans);
-        return ans;
+       int maxx = INT_MIN;
+       solve(root,maxx);
+       return maxx; 
     }
 };
