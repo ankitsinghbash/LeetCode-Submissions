39a69fa2bb92bf59c6cb8ca67f63cf68f49f7837
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-07-17 04:26:47 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.3 MB (99.92%)

--- my-code/0103-binary-tree-zigzag-level-order-traversal/solution.cpp
+++ my-code/0103-binary-tree-zigzag-level-order-traversal/solution.cpp
@@ -11,37 +11,38 @@
  */
 class Solution {
 public:
-    vector<vector<int>> ans;
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-      if(root==NULL){
-          vector<vector<int>> check;
-          return check;
-      }
-  
-      int level=0;
-      queue<TreeNode*> qu;
-      qu.push(root);
-      while(!qu.empty()){
-          int size = qu.size();
-          vector<int> store;
-          while(size--){
-              TreeNode *node = qu.front();
-              qu.pop();
-              store.push_back(node->val);
-              if(node->left!=NULL){
-                  qu.push(node->left);
-              }
-              if(node->right!=NULL){
-                  qu.push(node->right);
-              }
-          }
-          level++;
-          if(level%2==0){
-             reverse(store.begin(),store.end());
-          }
-          ans.push_back(store);
-      }
-      return ans;
-
+        vector<vector<int>> store;
+        if(root==NULL){
+            return {};
+        }
+        else{
+            int level=-1;
+            queue<TreeNode*> qu;
+            qu.push(root);
+            while(!qu.empty()){
+                int size = qu.size();
+                vector<int> sm;
+                while(size--){
+                   TreeNode *node = qu.front();
+                   qu.pop();
+                   sm.push_back(node->val);
+                   if(node->left!=NULL){
+                       qu.push(node->left);
+                   }
+                   if(node->right!=NULL){
+                       qu.push(node->right);
+                   }
+                }
+                level++;
+                if(level%2!=0){
+                    reverse(sm.begin(),sm.end());
+                }
+                store.push_back(sm);
+                sm.clear();
+            }
+        }
+        return store;
+ 
     }
 };
