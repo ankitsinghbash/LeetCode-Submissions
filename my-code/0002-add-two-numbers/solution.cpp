3fcb934c5ed4f2e41c03ec28934655fda022724a
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-07-17 16:43:18 +0000

    Sync LeetCode submission Runtime - 39 ms (7.20%), Memory - 71.3 MB (99.96%)

--- my-code/0002-add-two-numbers/solution.cpp
+++ my-code/0002-add-two-numbers/solution.cpp
@@ -10,45 +10,48 @@
  */
 class Solution {
 public:
+   ListNode *Reverse(ListNode *head,ListNode *prev){
    
-    ListNode * reverse(ListNode* head){
-    ListNode* curr=head,*prev=NULL,*nex=NULL;
-    while(curr){
-        nex=curr->next;
-        curr->next=prev;
-        prev=curr;
-        curr=nex;
-    }
-    return prev;
-}
+      if(head==NULL) return prev;
+
+      ListNode *temp = head->next;
+      head->next = prev;
+
+      return Reverse(temp,head);
+
+
+   }
+
+
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-    ListNode* rev1=l1;
-    ListNode* rev2=l2;
-    ListNode* dummy=NULL;
-    ListNode* p=dummy;
-    ListNode* m=dummy;
-    int carry=0;
-    while(rev1 || rev2 || carry!=0){
-        int x=carry;
-        if(rev1){
-            x+=rev1->val;
+        ListNode *head1=l1;
+        ListNode *head2=l2;
+        ListNode *dummy = NULL;
+        int carry=0;
+        while(head1!=NULL || head2!=NULL || carry!=0){
+            int x = carry;
+            if(head1!=NULL){
+                x+=head1->val;
+            }
+            if(head2!=NULL){
+                x+=head2->val;
+            }
+
+            ListNode *nn = new ListNode(x%10);
+
+            nn->next = dummy;
+            dummy = nn;
+
+            carry =  x/10;
+            if(head1!=NULL){
+                head1=head1->next;
+            }
+            if(head2!=NULL){
+                head2=head2->next;
+            }
         }
-        if(rev2){
-            x+=rev2->val;
-        }
-        
-        ListNode* nn=new ListNode(x%10);
-        nn->next=dummy;
-        dummy=nn;
-        carry=x/10;
-        if(rev1)
-            rev1=rev1->next;
-        if(rev2)
-            rev2=rev2->next;
-    
-    }
-    
-    return reverse(dummy);
+        ListNode *llist = Reverse(dummy,NULL);
+        return llist;
 
     }
 };
