3ab75ec6161e54de6ef097df4bad2ca8c1a932c9
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-07-17 07:55:56 +0000

    Sync LeetCode submission Runtime - 8 ms (5.17%), Memory - 20.7 MB (99.96%)

--- my-code/0110-balanced-binary-tree/solution.cpp
+++ my-code/0110-balanced-binary-tree/solution.cpp
@@ -1,37 +1,27 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    bool check = true;
-    int solve(TreeNode *root){
-    if(root==NULL){
-        return 0;
-    }
+  int solve(TreeNode *root,bool &start){
+
+    if(root==NULL) return 0;
+
      
-    int left=solve(root->left);
-    int right =solve(root->right);
+    int left = solve(root->left,start);
+    int right = solve(root->right,start);
+
     if(abs(left-right)>1){
-        check=false;
+        start = false;
     }
-   
-    return max(left,right)+1;
-    
-    }
-     
+
+    return 1+max(left,right);
+      
+
+  }
 
 
 
     bool isBalanced(TreeNode* root) {
-        solve(root);
-        return check;
+      bool start = true;
+      solve(root,start);
+      return start; 
     }
 };
