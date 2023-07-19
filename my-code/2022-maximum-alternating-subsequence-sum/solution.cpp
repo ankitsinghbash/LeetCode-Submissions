119204671eed10b16874d8f64d2ffc59d5a4a0b9
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-07-19 12:41:29 +0000

    Sync LeetCode submission Runtime - 468 ms (5.23%), Memory - 162.4 MB (31.23%)

---
+++ my-code/2022-maximum-alternating-subsequence-sum/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+  long long solve(int idx,int x,vector<int> &nums,vector<vector<long long>> &dp){
+   if(idx==nums.size()) return 0;
+
+   if(dp[idx][x]!=-1) return dp[idx][x];
+   
+    long long skip = solve(idx+1,x,nums,dp);
+    long long consider;
+    if(x==0){
+        consider = nums[idx]+solve(idx+1,1,nums,dp);
+    }
+    else{
+        consider = -nums[idx]+solve(idx+1,0,nums,dp);
+    }
+   
+     return  dp[idx][x] = max(skip,consider);
+
+  }
+
+
+    long long maxAlternatingSum(vector<int>& nums) {
+        vector<vector<long long>> dp(nums.size()+1,vector<long long>(2,-1));
+        return solve(0,0,nums,dp);
+    }
+};
