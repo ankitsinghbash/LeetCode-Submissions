32f58e390824511813a2f97beeecc66176c37672
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-07-21 20:06:43 +0000

    Sync LeetCode submission Runtime - 3 ms (13.53%), Memory - 6.2 MB (100.00%)

--- my-code/0096-unique-binary-search-trees/solution.cpp
+++ my-code/0096-unique-binary-search-trees/solution.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-
-   int catalan(int n){
-
-        if(n<=1){
-            return 1;
-        }
-     
-         int res=0;
-         for(int i=0;i<n;i++){
-             res+=catalan(i)*catalan(n-i-1);
-         }
-         return res;
-   }
-
     int numTrees(int n) {
-        //print catalan using recursion:
-        int ans = catalan(n);
-        return ans;
+        vector<int> dp(n+1);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<n+1;i++){
+           for(int j=0;j<i;j++){
+               dp[i]+=dp[j]*dp[i-j-1];
+           }
+        }
+       return dp[n];
     }
 };
+
