60989808b679c411f5785d8697595b8d8207d379
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-07-21 18:43:31 +0000

    Sync LeetCode submission Runtime - 3 ms (16.39%), Memory - 7.5 MB (100.00%)

--- my-code/0091-decode-ways/solution.cpp
+++ my-code/0091-decode-ways/solution.cpp
@@ -1,39 +1,31 @@
 class Solution {
 public:
+   int dp[101];
+   int solve(int idx,string s){
+    if(idx>s.size()) return 0;
 
-  
-    int solve(string s,int idx,int n,vector<int> &dp){
+    if(idx==s.size()) return 1;
+ 
+     if(dp[idx]!=-1) return dp[idx];
 
-      if(idx>=n){   //means when we decoded the all string because our idx getout of box also: means decoding happen:
-          return 1;
-      }
-      
-      if(dp[idx]!=-1){
-          return dp[idx];
-      }
-      
-   
-      int ans=0;  //this does not means ki answer update all time as 0 no : prev ans +=new ans;
-      int op1 = s[idx]-'0';
-      int op2=0;
-      if(idx<n-1){
-          op2=op1*10+s[idx+1]-'0';
-      }
-      if(op1>0){
-         ans+=solve(s,idx+1,n,dp);
-      }
-      if(op1>0 && op2>0 && op2<=26){
-          ans+=solve(s,idx+2,n,dp);
-      }
+     int ans =0;
+     int op1 = s[idx]-'0';  //option1:
+     if(op1>0 && op1<=26){
+          ans+=solve(idx+1,s);
+     }
+     int op2;
+     if(idx+1<s.size()){
+      op2 = 10*op1+s[idx+1]-'0';
+     }
+     if(op1>0 && op2>0 && op2<=26){
+         ans+=solve(idx+2,s);
+     }
+     return dp[idx] = ans;
+   }
 
-     return dp[idx]=ans;
-
-    }
 
     int numDecodings(string s) {
-        int n=s.size();
-        vector<int> dp(n+1,-1);
-      return   solve(s,0,n,dp);
-      
+        memset(dp,-1,sizeof(dp));
+        return solve(0,s);
     }
 };
