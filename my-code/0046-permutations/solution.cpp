b5c4239f8858aae06259fc28a8b1c8be6f8668a9
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-08-03 23:43:45 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.3 MB (100.00%)

--- my-code/0046-permutations/solution.cpp
+++ my-code/0046-permutations/solution.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    void solve(int idx,int n,vector<int> nums){
-
-     if(idx==n){
-         ans.push_back(nums);
-         return;
-     }
-      
-      for(int i=idx;i<n;i++){
-          swap(nums[i],nums[idx]);
-          solve(idx+1,n,nums);
+    void solve(int idx,vector<int> nums){
+  
+      if(idx>=nums.size()){
+          ans.push_back(nums);
+          return;
       }
+
+
+       for(int i=idx;i<nums.size();i++){
+           swap(nums[i],nums[idx]);
+           solve(idx+1,nums);
+       }
+
+
     }
-
-
-
-
     vector<vector<int>> permute(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(),nums.end());
-        solve(0,n,nums);
+        vector<int> sm;
+        solve(0,nums);
         return ans;
     }
 };
