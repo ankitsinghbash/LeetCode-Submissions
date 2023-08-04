dd23968540233f6917201ab46cf25fe85ebc044e
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-08-04 18:28:21 +0000

    Sync LeetCode submission Runtime - 4 ms (62.07%), Memory - 9.4 MB (100.00%)

--- my-code/0139-word-break/solution.cpp
+++ my-code/0139-word-break/solution.cpp
@@ -1,42 +1,31 @@
 class Solution {
 public:
-   int dp[301];
-   int helper(int i,string s,set<string> &wordDic)
-   {
-      if(i==s.size()) return true;
-      string path;
-      if(dp[i]!=-1)
-      {
-          return dp[i];
-      }
+  int dp[301];
+  bool solve(int idx,string s,unordered_set<string> &st){
 
-      for(int j=i;j<s.size();j++)
-      {
-          path+=s[j];
-          //check is path is present in the wordDic or not:
-          if(wordDic.find(path)!=wordDic.end())
-          {
-              //mean present then Ok:
-              //now try to find as same according to next more element use of recursion:
-              if(helper(j+1,s,wordDic))
-              {
-                  return dp[i]=true;
-              }
+     if(idx>=s.size()) return true;
+   
+     if(dp[idx]!=-1) return dp[idx];
+   
+     string path;
+     for(int i=idx;i<s.size();i++){
+          path+=s[i];
+          if(st.find(path)!=st.end()){
+               if(solve(i+1,s,st)==true){
+                   return dp[idx] = true;
+               }
           }
-      }
+     }
+      return dp[idx] = false;
+  }
 
-     //if no word is found and i=0;
-     return dp[i]=false;
-   }
-    
     bool wordBreak(string s, vector<string>& wordDict) {
-    memset(dp,-1,sizeof dp);
-    set<string> st;
-    for(auto x:wordDict)
-    {
-        st.insert(x);
-    }    
-    return helper(0,s,st);
- 
+        unordered_set<string> st;
+        for(auto x : wordDict){
+          st.insert(x);
+        }
+         memset(dp,-1,sizeof(dp));
+        return solve(0,s,st);
+
     }
 };
