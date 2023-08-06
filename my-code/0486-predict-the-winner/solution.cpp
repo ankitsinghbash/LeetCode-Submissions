2d591e2cf513362be3ce595384876e4c4b240e2a
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-08-06 12:24:14 +0000

    Sync LeetCode submission Runtime - 233 ms (5.11%), Memory - 7.4 MB (100.00%)

--- my-code/0486-predict-the-winner/solution.cpp
+++ my-code/0486-predict-the-winner/solution.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
-    bool PredictTheWinner(vector<int>& nums) {
-        vector<vector<int>> dp(nums.size(),vector<int>(nums.size()));
-        int su = 0;
-        for(auto x : nums)
-        {
-            su+=x;
-        }
-
-        for(int g=0;g<nums.size();g++)
-        {
-            for(int i=0,j=g;j<nums.size();i++,j++)
-            {
-                if(g==0)
-                {
-                    dp[i][j]=nums[i];
-                }
-                else if(g==1)
-                {
-                    dp[i][j]=max(nums[i],nums[j]);
-                }
-                else{
-                    int val1  = nums[i]+min(dp[i+2][j],dp[i+1][j-1]);
-                    int val2 =  nums[j]+min(dp[i+1][j-1],dp[i][j-2]);
-                    int val = max(val1,val2);
-                    dp[i][j]=val;
-                }
-            }
-           
-        }
-        return dp[0][nums.size()-1]>=su-dp[0][nums.size()-1];
-    }
+   int solve(int i,int j,vector<int> &nums,bool chance,int myans){
      
+      if(i>j){
+          return 0;
+      }
+    
+      if(chance==true){
+          myans+=max(nums[i]+solve(i+1,j,nums,false,myans),nums[j]+solve(i,j-1,nums,false,myans));     
+      }
+      else{
+           myans+=min(-nums[i]+solve(i+1,j,nums,true,myans),-nums[j]+solve(i,j-1,nums,true,myans)); 
+      }
+      return myans;
+   }
+
+
+
+
+    bool predictTheWinner(vector<int>& nums) {
+        int myans=0;
+        int j = nums.size()-1;
+        return solve(0,j,nums,true,myans)>=0;
+
+      // if(myans>=0) return true; //never use this because there is no & referece in our answer:
+        //if i pass reference then backtrack not happen: check both solution: 
+       // return false;
+    }
+  
 };
