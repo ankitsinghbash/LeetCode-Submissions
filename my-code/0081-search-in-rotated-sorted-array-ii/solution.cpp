ec8bf6b862646bd479bf87a81e5afcd2343e5aef
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-08-12 19:19:52 +0000

    Sync LeetCode submission Runtime - 5 ms (0.73%), Memory - 14 MB (100.00%)

--- my-code/0081-search-in-rotated-sorted-array-ii/solution.cpp
+++ my-code/0081-search-in-rotated-sorted-array-ii/solution.cpp
@@ -1,16 +1,52 @@
 class Solution {
 public:
-    bool search(vector<int>& nums, int target) {
-        //use binary search tree on sorted arr;
-        sort(nums.begin(),nums.end());
-        //now nums is sorted;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target){
-                return true;
-            }
+   
+    int searchPivot(vector<int> &nums){ //search an element in the rotated array:
+      int l=0;
+      int h = nums.size()-1;
+      int result = -1;
+      while(l<h){
+          while(l<h && nums[l]==nums[l+1]){
+              l++;
+          }
+          while(l<h && nums[h]==nums[h-1]){
+              h--;
+          }
+          int mid = l+(h-l)/2;
+          if(nums[mid]>nums[h]){
+              l = mid+1;
+          }
+          else{
+             h = mid;
+             result = h;
+          }
+      }
+     return h;
+   }
+
+bool binarySearch(vector<int>& nums, int target, int left, int right) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target) {
+            return true;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
         }
-        return false;
-
-
     }
+    return false;
+}
+
+bool search(vector<int>& nums, int target) {
+    int n = nums.size();
+    int pivot_search = searchPivot(nums);
+
+    if (binarySearch(nums, target, 0, pivot_search - 1)) {
+        return true;
+    }
+
+    return binarySearch(nums, target, pivot_search, n - 1);
+}
+
 };
