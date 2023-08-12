a78bfab96df7219ea73e623738e145b5f6ab4083
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-08-12 15:02:30 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.1 MB (100.00%)

--- my-code/0033-search-in-rotated-sorted-array/solution.cpp
+++ my-code/0033-search-in-rotated-sorted-array/solution.cpp
@@ -1,17 +1,56 @@
 class Solution {
 public:
+   int solve(vector<int> &nums){
+      int l=0;
+      int h = nums.size()-1;
+      int result = -1;
+      while(l<h){
+          int mid = l+(h-l)/2;
+          if(nums[mid]>nums[h]){
+              l = mid+1;
+          }
+          else{
+              h = mid;
+             result = h;
+          }
+      }
+     return h;
+   }
+
+
+   int binary_search(int l,int h,vector<int> &nums,int target){
+
+        while(l<=h){
+            int mid = l+(h-l)/2;
+            if(target==nums[mid]){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                l = mid+1;
+            }
+            else{
+                h = mid-1;
+            }
+        }
+        return -1;
+
+   }
+    
+
+
+
     int search(vector<int>& nums, int target) {
-        //normally search the target;
-        //i use binary serach; use when element is in sorted from:
-        bool flag=true;
-       for(int i=0;i<nums.size();i++)
-       {
-           if(nums[i]==target)
-           {
-               flag=false;
-               return i;
-           }
-       }
-      return -1;
+        int pivot_idx = solve(nums);
+
+        int idx = binary_search(0,pivot_idx-1,nums,target);
+        if(idx!=-1){
+            return idx;
+        }
+
+        int idx2 = binary_search(pivot_idx,nums.size()-1,nums,target);
+        if(idx2!=-1){
+            return idx2;
+        }
+        return -1;
     }
 };
