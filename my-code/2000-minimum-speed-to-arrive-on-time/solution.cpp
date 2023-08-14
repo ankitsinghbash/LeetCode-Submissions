09cd3051aa54022ffdcc73f943ad2ec0f160f288
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-08-14 06:39:40 +0000

    Sync LeetCode submission Runtime - 244 ms (5.07%), Memory - 101.4 MB (100.00%)

--- my-code/2000-minimum-speed-to-arrive-on-time/solution.cpp
+++ my-code/2000-minimum-speed-to-arrive-on-time/solution.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
-   double OK(int mid,vector<int> &dist){
-       //mid is speed:
-       double time = 0;
-       for(int i=0;i<dist.size();i++){
-         double y = double(dist[i])/mid;
-         if(i==dist.size()-1){
-             time+=y;
-         }
-         else{
-             time+=ceil(y);
-         }
-       }
-      return time;
+   
+   double Ishour(int mid , vector<int> &dist){
+        double total  =0;
+        for(int i=0;i<dist.size();i++){
+            int value = dist[i];
+            int data  = value/mid;
+            if(i!=dist.size()-1){
+            if(value%mid==0){
+                total+=data;
+            }
+            else{
+                total+=data+1;
+            }
+            }else{
+                double data = (double)(value)/(double)(mid);
+                total+=data;
+            }
+        }
+        return total;
    }
 
 
 
     int minSpeedOnTime(vector<int>& dist, double hour) {
-      
-        int low = 0;
-        int high = 1e7;
+        int low=1;
+        int high = 1e9;
         int result = -1;
         while(low<=high){
             int mid = low+(high-low)/2;
-            if(OK(mid,dist)<=hour){
+            if(Ishour(mid,dist)<=hour){
                 result = mid;
-                high=mid-1;
+                high = mid-1;
             }
             else{
                 low = mid+1;
