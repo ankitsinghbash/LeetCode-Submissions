ca677d35d35f8869c87a4e2900b4fe16a23b95d5
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-08-26 06:15:45 +0000

    Sync LeetCode submission Runtime - 28 ms (11.45%), Memory - 34.3 MB (8.34%)

--- my-code/0097-interleaving-string/solution.cpp
+++ my-code/0097-interleaving-string/solution.cpp
@@ -1,30 +1,36 @@
 class Solution {
 public:
+  int dp[101][101];
+  bool solve(int i,int j,int k,string s1,string s2,string s3){
+    
+     if(i==s1.size() && j==s2.size() && k==s3.size()){
+         return true;
+     }
+
+   
+      if(dp[i][j]!=-1){
+          return dp[i][j];
+      }
+     
+   
+      bool left = false;
+      bool right = false;
+      if(i<s1.size() && s1[i]==s3[k]){
+          left = solve(i+1,j,k+1,s1,s2,s3);
+      }
+    
+      if(j<s2.size() && s2[j]==s3[k]){
+          right = solve(i,j+1,k+1,s1,s2,s3);
+      }
+
+      return dp[i][j] = left||right;
+
+  }
+
+
+
     bool isInterleave(string s1, string s2, string s3) {
-        int m = s1.length();
-        int n = s2.length();
-        
-        int l = s3.length();
-		
-        if(m+n != l) return false;
-        
-        bool dp[m+1][n+1];
-        
-        for(int i=0; i<=m;++i){
-            for(int j=0; j<=n; ++j){
-                if(i==0 && j == 0) 
-                    dp[i][j] = true;
-                else if(i>0 && j>0 && s3[i+j-1] == s2[j-1] && s3[i+j-1] == s1[i-1])
-                    dp[i][j] = dp[i][j-1] || dp[i-1][j];
-                else if(i>0 && s3[i+j-1] == s1[i-1])
-                    dp[i][j] = dp[i-1][j];
-                else if(j>0 && s3[i+j-1] == s2[j-1])
-                    dp[i][j] = dp[i][j-1];
-                else 
-                    dp[i][j] = false;
-            }
-        }
-        
-        return dp[m][n];
-    }   
+        memset(dp,-1,sizeof(dp));
+        return solve(0,0,0,s1,s2,s3);
+    }
 };
