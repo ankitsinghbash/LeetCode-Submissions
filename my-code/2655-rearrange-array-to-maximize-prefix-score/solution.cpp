609f04a24bf94c0226a7c3d79f814158fef1a01a
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-09-01 19:04:20 +0000

    Sync LeetCode submission Runtime - 131 ms (6.12%), Memory - 96.1 MB (8.16%)

--- my-code/2655-rearrange-array-to-maximize-prefix-score/solution.cpp
+++ my-code/2655-rearrange-array-to-maximize-prefix-score/solution.cpp
@@ -1,21 +1,23 @@
+typedef long long LL;
 class Solution {
 public:
-    int maxScore(vector<int>& nums) {
-       sort(nums.begin(),nums.end(),greater<int>());
-       vector<long long> prefix(nums.size());
-       prefix[0]=nums[0];
-       
-       int cnt=0;
-       if(prefix[0]>0){
-           cnt++;
-       }
-       for(int i=1;i<nums.size();++i){
-           prefix[i]=prefix[i-1]+nums[i];
-           if(prefix[i]>0){
-               cnt++;
-           }
-       }
+   int maxScore(vector<int>& nums) {
+        sort(nums.begin(),nums.end(),greater<int>());
+        LL prefix_sum=0;
+        vector<LL> store;
+ 
+        for(int i=0;i<nums.size();i++){
+            prefix_sum+=nums[i];
+            store.push_back(prefix_sum);
+        }
+
+     
+        int cnt=0;
+        for(auto x: store){
+            if(x>0){
+                cnt++;
+            }
+        }
         return cnt;
-        
     }
 };
