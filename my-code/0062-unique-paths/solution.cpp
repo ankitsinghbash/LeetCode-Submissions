a04aac3b0a65f42c5bc01beccaa823b6bae198bf
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-09-03 15:46:43 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 6.1 MB (100.00%)

--- my-code/0062-unique-paths/solution.cpp
+++ my-code/0062-unique-paths/solution.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-       //tabulation:
-       vector<vector<int>> dp(m,vector<int>(n));
-       for(int i=0;i<m;i++){
-           dp[i][0]=1;
-       }
-       for(int j=0;j<n;j++){
-           dp[0][j]=1;
-       }
- 
-       for(int i=1;i<m;i++){
-           for(int j=1;j<n;j++){
-               dp[i][j]=dp[i-1][j]+dp[i][j-1];
-           }
-       }
-       return dp[m-1][n-1];
+  int dp[101][101];
+  int solve(int i,int j,int m,int n){
+     if(i>=m || j>=n){
+         return 0;
+     }
 
+      if(i==m-1 && j==n-1){
+          return 1;
+      }
+
+      if(dp[i][j]!=-1){
+          return dp[i][j];
+      }
+  
+      int left = solve(i+1,j,m,n);
+      int down = solve(i,j+1,m,n);
+
+      return dp[i][j] = left+down;
+
+  }
+
+
+    int uniquePaths(int m, int n) {
+        //using recursion:
+        memset(dp,-1,sizeof(dp));
+        return solve(0,0,m,n);
     }
 };
