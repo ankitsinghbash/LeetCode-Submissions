9378130fdfb7bcf1aa518508f9fb1d1612464cdf
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-09-04 20:25:09 +0000

    Sync LeetCode submission Runtime - 9 ms (5.27%), Memory - 7.7 MB (100.00%)

--- my-code/0345-reverse-vowels-of-a-string/solution.cpp
+++ my-code/0345-reverse-vowels-of-a-string/solution.cpp
@@ -1,28 +1,37 @@
 class Solution {
 public:
-   bool vowel(char &ch){
+ bool vowel(char ch){
       return ch=='a' || ch=='e' || ch=='i'  || ch=='o' || ch=='u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U';
    }
 
 
+   void solve(int start,int last,string &s){
+     if(start>=last){
+         return;
+     }
+
+
+      if(vowel(s[start]) && vowel(s[last])){
+          swap(s[start],s[last]);
+          start++;
+          last--;
+      }
+
+      else if(vowel(s[start])){
+          last--;
+      }
+      else{
+          start++;
+      }
+     
+      solve(start,last,s);
+   }
+
+
     string reverseVowels(string s) {
-        int low=0;
-        int high = s.size()-1;
-        while(low<=high){
-            char last = s[high];
-            char start = s[low];
-            if(vowel(last)==true && vowel(start)){
-                swap(s[high],s[low]);
-                 low++;
-                 high--;
-            }
-            else if(vowel(last)==true){
-                low++;
-            }
-            else{
-                high--;
-            }
-        }
+        //by recursion:
+        int n = s.size();
+        solve(0,n-1,s);
         return s;
     }
 };
