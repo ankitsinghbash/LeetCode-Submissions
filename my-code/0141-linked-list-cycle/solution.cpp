33e577de3a86cc6f09fe4733b8dfe96f6b3d5cb3
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-09-04 17:00:26 +0000

    Sync LeetCode submission Runtime - 7 ms (84.00%), Memory - 8.1 MB (100.00%)

--- my-code/0141-linked-list-cycle/solution.cpp
+++ my-code/0141-linked-list-cycle/solution.cpp
@@ -8,26 +8,16 @@
  */
 class Solution {
 public:
-
- 
-   bool solve(ListNode *head){
-       ListNode *slow = head;
-       ListNode *fast = head;
-       while(fast!=NULL && fast->next!=NULL){
-           slow=slow->next;
-           fast=fast->next->next;
-           if(slow==fast){
-               return true;
-           }
-       }
-       return false;
-   }
-
     bool hasCycle(ListNode *head) {
-        if(head==NULL || head->next==NULL){
-            return false;
+        ListNode *slow=head;
+        ListNode *fast = head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast = fast->next->next;
+            if(slow==fast){
+                return true;
+            }
         }
-
-        return solve(head);
+        return false;
     }
 };
