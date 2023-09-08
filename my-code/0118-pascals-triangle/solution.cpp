f0a8f3bef52f49a1405a3b105467d82bb9137517
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-09-08 19:48:01 +0000

    Sync LeetCode submission Runtime - 4 ms (0.61%), Memory - 7.1 MB (100.00%)

--- my-code/0118-pascals-triangle/solution.cpp
+++ my-code/0118-pascals-triangle/solution.cpp
@@ -1,29 +1,31 @@
+class Solution {
+public:
+   vector<vector<int>> ans;
+    vector<vector<int>> generate(int numRows) {
+        int time = numRows;
+        if(time==1){
+            return {{1}};
+        }
+        else if(time==2){
+            return {{1},{1,1}};
+        }
 
- class Solution { public: vector<vector<int>> generate(int numRows) { int n = numRows; vector<vector<int>> ans; if( n == 1) { 		
-     	// edge case just push {1} to ans vec tor
-          ans.push_back({1}); } if(n == 2) { 
-             			// edge case just push {1} and {1,1} ans vec tor 
-                         ans.push_back({1}); 
-                         ans.push_back({1,1}); }
-                          if(n >=3) { 
-                              			// repeat these prior steps 
-                                          ans.push_back({1});
-                                           ans.push_back({1,1}); 	
-                                           		// an variable which will tell us how many element to put on current row 
-                      int level = 2;
-              for(int i = 1; i<n-1; ++i) {
-                   vector<int> v; 
-                   for(int j = 0; j<= level; ++j)
-                    { 				
-                        	// edges case first element of current row has to be first element of prev row.
-                             if(j == 0) v.push_back(ans[i][0]); else if(j == level) {
-                                  							// edges case last element of current row has to be last element of prev row.
-                                                               v.push_back(ans[i][level-1]);
-                                                                } else {
-                                                                     v.push_back(ans[i][j-1] + ans[i][j]); } } ++level;
-                                                                      ans.push_back(v);
-                                                                       }
-                                                                        } 
-                                                                        return ans;
-                                                                         }
-                                                                          };
+        int TIME=time-2;
+        ans.push_back({1}); 
+        ans.push_back({1,1});
+        vector<int> store = {1,1};
+        while(TIME--){
+            vector<int> temp;
+            temp.push_back(store[0]);
+            for(int i=1;i<store.size();i++){
+                int value = store[i]+store[i-1];
+                temp.push_back(value);
+            }
+            temp.push_back(store[store.size()-1]);
+            ans.push_back(temp);
+            store = temp;
+            temp.clear();
+        }
+        return ans;
+    }
+};
