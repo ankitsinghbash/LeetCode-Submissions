82eb6b5b0033f922694b51a397423a0094c49b21
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-09-10 06:09:10 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 6.7 MB (100.00%)

--- my-code/0377-combination-sum-iv/solution.cpp
+++ my-code/0377-combination-sum-iv/solution.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        vector<unsigned int > dp(target+1);
+        vector<unsigned int> dp(1001,0);
         dp[0]=1;
-        for(int i=1;i<=target;i++)
-           for(auto coin:nums)
-                if(coin<=i)
-                    dp[i]+=dp[i-coin];
+        for(int i=1;i<target+1;i++){
+            for(auto coins : nums){
+                if(i>=coins){
+                    dp[i] = dp[i] +dp[i-coins];
+                }
+            }
+        }
         return dp[target];
     }
 };
