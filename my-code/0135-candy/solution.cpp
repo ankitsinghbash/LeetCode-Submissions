e2fec58a59cc18d6c48163e46204841ed5f07cd9
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-09-13 18:32:11 +0000

    Sync LeetCode submission Runtime - 19 ms (5.89%), Memory - 18.6 MB (100.00%)

--- my-code/0135-candy/solution.cpp
+++ my-code/0135-candy/solution.cpp
@@ -1,39 +1,27 @@
 class Solution {
 public:
+    //dry run for [5,3,4,5,6,3,2] at val(6) we have two option left se 3 and right se 2 so i taken max because left ke side asi 3 value rahe hogi jiske lyia candy increase kyia and right me 2 rahi hogi so for optimally we take the left side candy 3 then added 1 for val(6) at 6 it become 4:
+    //so basically we taken two option at each point ki left and right me maximum kon thi:
+    //That why this algorithm works:
     int candy(vector<int>& ratings) {
-        
-        int n=ratings.size();
-        vector<int> left(n,1);   //make a vector of size n+1 because we start add value from i=1 and take all value defalut as 1 for 1 candy minimum to each children;
+        int n = ratings.size();
+        vector<int> left(n,1);
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1]){
+                left[i] = left[i-1]+1;
+            }
+        }
         vector<int> right(n,1);
-        
-        
-        //Now traversal fron i=1;
-       for(int i=1;i<n;i++)
-       {
-            if(ratings[i]>ratings[i-1])
-            {
-            left[i]=left[i-1]+1;
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                right[i]=right[i+1]+1;
             }
         }
-        
-        //index problem nahi karega left right ka because we already given value 1 int the initial case:
-        //we just only increase the value +1 if the condition got true;
-         for(int j=n-2;j>=0;j--){
-            if(ratings[j]>ratings[j+1])
-            {
-                right[j]=right[j+1]+1;
-            }
-        }
-        
-        
-       int sum=0;
+        int sum=0;
         for(int i=0;i<n;i++){
-            sum+=max(left[i],right[i]);
+            int value = max(left[i],right[i]);
+            sum+=value;
         }
         return sum;
-        
-        
-        
-        
     }
 };
