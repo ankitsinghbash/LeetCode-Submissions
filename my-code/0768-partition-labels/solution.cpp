a8a96039f00a25ab5da17a02fe41243e584c2efb
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-09-13 20:04:37 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.3 MB (100.00%)

--- my-code/0768-partition-labels/solution.cpp
+++ my-code/0768-partition-labels/solution.cpp
@@ -1,41 +1,25 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-       //using map:
-       unordered_map<char,int> mp;
-       for(int i=0;i<s.size();++i){
-           mp[s[i]]=i;   //store the lastest value of duplicate value:
-       }
-    
-       vector<int> ans;
-       int maxx=0;
-       int last=0;
-       for(int i=0;i<s.size();i++){
-           maxx=max(maxx,mp[s[i]]);
-           if(i==maxx){
-               ans.push_back(i-last+1);
-               last=i+1;
-           }
-       }
-       return ans;
+        //Now solve this greedy:
+        unordered_map<char,int> last_occur;
+        for(int i=0;i<s.size();i++){
+            last_occur[s[i]]=i;
+        }
+  
+        vector<int> ans;
+        int j=0;
+        while(j<s.size()){
+            int i = j;
+            int skip = last_occur[s[j]];
+            while(i<=skip){
+               skip = max(skip,last_occur[s[i]]);
+               i++;
+            }
+            ans.push_back(i-j);
+            j = i;
+        }
+        return ans;
 
-
-        //using hasmap:
-        // vector<int> arr(26);
-        // for(int i=0;i<s.size();i++){
-        //     arr[s[i]-'a']=i;
-        // }
-
-        // vector<int> ans;
-        // int maxx=0;
-        // int last=0;
-        // for(int i=0;i<s.size();i++){
-        //     maxx=max(maxx,arr[s[i]-'a']);
-        //     if(i==maxx){
-        //         ans.push_back(i-last+1);
-        //         last=i+1;
-        //     }
-        // }
-        // return ans;
     }
 };
