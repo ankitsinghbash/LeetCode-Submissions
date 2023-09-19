0318f60d6cf3eb21c38c78487f00c9cb8624283d
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-09-19 02:23:40 +0000

    Sync LeetCode submission Runtime - 224 ms (5.06%), Memory - 94.8 MB (100.00%)

--- my-code/2294-minimum-time-to-complete-trips/solution.cpp
+++ my-code/2294-minimum-time-to-complete-trips/solution.cpp
@@ -2,25 +2,28 @@
 class Solution {
 public:
     
-    ll solve(ll actual,vector<int> &time){
+    bool solve(ll actual,vector<int> &time,int totalTrips){
         long long sum=0;
         for(int i=0;i<time.size();++i){
             ll value = actual/time[i];
             sum+=value;
         }
-        return sum;
+        return sum>=totalTrips;
     }
 
     long long minimumTime(vector<int>& time, int totalTrips) {
         ll start = 1;
         ll end = (ll)*min_element(time.begin(),time.end())*totalTrips;
-        while(start <= end){
+        while(start <=  end){
+           // ll trip = 0;
             ll mid = start + (end - start)/2;
-            if(solve(mid,time)<totalTrips){
-                start=mid+1;
+           // for(int i=0;i<time.size();i++)
+             //   trip += mid / time[i];
+            if(solve(mid,time,totalTrips)){
+                end=mid-1;
             }
             else 
-                end=mid-1;
+                start = mid + 1;
         }
         return start;
     }
