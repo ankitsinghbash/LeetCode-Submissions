6130e447898a77faf2d30c01512afaea5a4b75b1
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-09-19 02:40:36 +0000

    Sync LeetCode submission Runtime - 230 ms (5.07%), Memory - 101.9 MB (100.00%)

--- my-code/2000-minimum-speed-to-arrive-on-time/solution.cpp
+++ my-code/2000-minimum-speed-to-arrive-on-time/solution.cpp
@@ -1,42 +1,45 @@
 class Solution {
 public:
-   
-   double Ishour(int mid , vector<int> &dist){
-        double total  =0;
-        for(int i=0;i<dist.size();i++){
-            int value = dist[i];
-            int data  = value/mid;
-            if(i!=dist.size()-1){
-            if(value%mid==0){
-                total+=data;
-            }
-            else{
-                total+=data+1;
-            }
-            }else{
-                double data = (double)(value)/(double)(mid);
-                total+=data;
-            }
-        }
-        return total;
+   double OK(int mid,vector<int> &dist){
+       //mid is speed:
+       double time = 0;
+       for(int i=0;i<dist.size();i++){
+         double y = double(dist[i])/mid;
+         if(i==dist.size()-1){
+             time+=y;
+         }
+         else{
+             time+=ceil(y);
+         }
+       }
+      return time;
    }
+
+   bool isPossible(vector<int>& dist, double hour, int speed) {
+        double time = OK(speed,dist);
+        return time <= hour;
+    }
 
 
 
     int minSpeedOnTime(vector<int>& dist, double hour) {
-        int low=1;
-        int high = 1e9;
-        int result = -1;
+        
+        int low = 0;
+        int high = 1e7;
+
+        if (!isPossible(dist, hour, high)) {
+            return -1;
+        }
+
         while(low<=high){
             int mid = low+(high-low)/2;
-            if(Ishour(mid,dist)<=hour){
-                result = mid;
-                high = mid-1;
+            if(OK(mid,dist)<=hour){
+                high=mid-1;
             }
             else{
                 low = mid+1;
             }
         }
-        return result;
+        return low;
     }
 };
