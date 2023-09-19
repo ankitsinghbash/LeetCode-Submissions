970cf299b66869af7e6b8246e5b32c3e9a4d9a7a
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-09-19 02:27:35 +0000

    Sync LeetCode submission Runtime - 73 ms (22.75%), Memory - 54.1 MB (100.00%)

--- my-code/2665-minimum-time-to-repair-cars/solution.cpp
+++ my-code/2665-minimum-time-to-repair-cars/solution.cpp
@@ -9,17 +9,15 @@ public:
    }
 
     long long repairCars(vector<int>& ranks, int cars) {
-        long long  low = 0;
-        long long maxx = *max_element(ranks.begin(),ranks.end());
-        long long  high = (long long )maxx * (long long )cars * (long long )cars;
-        while(low<high){
+        long long  low = 1,high = 1L * ranks[0] * cars * cars;;
+        while(low<=high){
            long long  mid = low+(high-low)/2;
-           if(find(ranks,mid)<cars){
-              low=mid+1;
-           }
-           else{
-               high = mid;
-           }
+          if(find(ranks,mid)>=cars){
+              high = mid-1;
+          }
+          else{
+              low = mid+1;
+          }
         }
         return low;
     }
