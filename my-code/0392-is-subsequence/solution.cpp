6d81e0ab5b0ec00dee1d1e7e62fcf4a640150d5f
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-09-22 15:47:22 +0000

    Sync LeetCode submission Runtime - 34 ms (2.14%), Memory - 281.1 MB (5.17%)

--- my-code/0392-is-subsequence/solution.cpp
+++ my-code/0392-is-subsequence/solution.cpp
@@ -1,34 +1,91 @@
 class Solution {
 public:
+   bool solve(int i,int j,string s,string t,int n,int m){
+        if(i>=n){
+            return true;
+        }
+       
+        if(j>=m){
+            return false;
+        }
+
+        bool first=false;
+        bool second = false;
+        if(s[i]==t[j]){
+            first = solve(i+1,j+1,s,t,n,m);
+        }
+        else{
+            second = solve(i,j+1,s,t,n,m);
+        }
+        return first || second;
+   }
+    
+
+
     bool isSubsequence(string s, string t) {
-        int n=s.size();
-        int m=t.size();
-       vector<vector<bool>> dp(n+1,vector<bool>(m+1));
+        //recursive code:
+        if(s.size()==0){
+            return true;
+        }
+        if(s.size()>t.size()) return false;
 
-        for(int i=0;i<n+1;i++)
-        {
-            dp[i][0]=false;
-        }
-        
-        for(int j=0;j<m+1;j++)
-        {
-            dp[0][j]=true;
-        }
-
-        for(int i=1;i<n+1;i++)
-        {
-            for(int j=1;j<m+1;j++)
-            {
-                if(s[i-1]==t[j-1])
-                {
-                    dp[i][j]=dp[i-1][j-1];
-                }
-                else
-                {
-                    dp[i][j]=dp[i][j-1];
-                }
-            }
-        }
-        return dp[n][m];
+        int n = s.size();
+        int m = t.size();
+        return solve(0,0,s,t,n,m);
     }
 };
+
+
+// class Solution {
+// public:
+//     //dp approach bottom: tabulation:
+//     bool isSubsequence(string s, string t) {
+//          vector<vector<int>> dp(s.size()+1,vector<int>(t.size()+1,-1));
+//          for(int i=0;i<s.size()+1;i++){
+//              dp[i][0]=false;
+//          }
+//          for(int j=0;j<t.size()+1;j++){
+//              dp[0][j]=true;
+//          }
+
+//          for(int i=1;i<s.size()+1;i++){
+//              for(int j=1;j<t.size()+1;j++){
+//                  if(s[i-1]==t[j-1]){
+//                      dp[i][j]=dp[i-1][j-1];
+//                  }
+//                  else{
+//                      dp[i][j]=dp[i][j-1];
+//                  }
+//              }
+//          }
+//          return dp[s.size()][t.size()];
+//     }
+// };
+
+//Two pointer approach:
+// class Solution {
+// public:
+//     bool isSubsequence(string s, string t) {
+//         int s_size = s.size();
+//         int t_size = t.size();
+//         if(s_size>t_size){
+//             return false;
+//         }
+     
+//         int cnt=0;
+//         int i=0;
+//         int j=0;
+//         while(i<s_size && j<t_size){
+//             if(s[i]==t[j]){
+//                 cnt++;
+//                 i++;
+//             }
+//             j++;
+//         }
+//         if(cnt==s_size){
+//             return true;
+//         }
+//         return false;
+
+//     }
+// };
