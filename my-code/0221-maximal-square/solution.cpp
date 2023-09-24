d9e68516745bc5540d63bda9d9ccf6229073a581
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-09-24 01:44:04 +0000

    Sync LeetCode submission Runtime - 70 ms (6.53%), Memory - 21.4 MB (100.00%)

--- my-code/0221-maximal-square/solution.cpp
+++ my-code/0221-maximal-square/solution.cpp
@@ -1,21 +1,44 @@
 class Solution {
 public:
+    
+// _________________________
+// |min(2,2,3)+1        [2] | //so formuala is min({(i+1,j),(i+1,j+1),(i,j+1)})
+// |                        |
+// | [2]                [3] |
+// |________________________|
+
+
     int maximalSquare(vector<vector<char>>& matrix) {
-        if (matrix.empty()) {
-            return 0;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int maxx=0;
+        vector<vector<int>> dp(n,vector<int>(m));
+        for(int i=0;i<n;i++){
+            dp[i][m-1]=matrix[i][m-1]-'0';
+            maxx = max(maxx,dp[i][m-1]);
         }
-        int m = matrix.size(), n = matrix[0].size(), sz = 0;
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!i || !j || matrix[i][j] == '0') {
-                    dp[i][j] = matrix[i][j] - '0';
-                } else {
-                    dp[i][j] = min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1])) + 1;
+        for(int j=0;j<m;j++){
+            dp[n-1][j] = matrix[n-1][j]-'0';
+            maxx = max(maxx,dp[n-1][j]);
+        }
+        
+        for(int j=m-2;j>=0;j--){
+            for(int i=n-2;i>=0;i--){
+                if(matrix[i][j]=='0'){
+                    dp[i][j]=0;
                 }
-                sz = max(dp[i][j], sz);
+                else if(matrix[i+1][j+1]=='0' || matrix[i+1][j]=='0' || matrix[i][j+1]=='0'){
+                    dp[i][j]=matrix[i][j]-'0';
+                }
+                else{
+                    dp[i][j] = min({dp[i+1][j+1],dp[i+1][j],dp[i][j+1]})+1;
+                }
+                maxx = max(maxx,dp[i][j]);
             }
-        }
-        return sz * sz;
+        } 
+        
+         return pow(maxx,2);
     }
 };
+
+
