aed17b9c2c9af94c94d33d5e695e8ba32e2b14c7
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-09-27 08:20:36 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.2 MB (100.00%)

--- my-code/0316-remove-duplicate-letters/solution.cpp
+++ my-code/0316-remove-duplicate-letters/solution.cpp
@@ -2,36 +2,32 @@ class Solution {
 public:
     string removeDuplicateLetters(string s) {
         stack<char> st;
-        vector<int> lastoccur(26, 0);
-        for(int i=0;i<s.length();i++)
-            lastoccur[s[i]-'a'] = i; 
+        vector<int> last_occur(26,0);
+        for(int i=0;i<s.size();i++){
+          last_occur[s[i]-'a']=i;
+        }
 
         vector<bool> visited(26,false);
-        for(int i=0;i<s.size();++i){
-              char ch = s.at(i);
-                if(visited[ch-'a']==true){
-                    continue;
-                }
-                 while(st.size()>0 && st.top()>ch && i<lastoccur[st.top()-'a'] && i<lastoccur[st.top()-'a']){  
-                      visited[st.top()-'a']=false;
-                      cout<<"out element"<<st.top()<<endl;
-                      st.pop();
-                     
-                 }
-                 
-                     st.push(ch);
-                     cout<<"push element"<<ch<<endl;
-                     visited[ch-'a']=true;
-                 
-               
+        for(int i=0;i<s.size();i++){
+          char ch = s[i];
+          if(visited[ch-'a']==true){
+            continue;
+          }
+          while(st.size()>0 && st.top()>ch && i<last_occur[st.top()-'a']){
+            visited[st.top()-'a']=false;
+            st.pop();
+          }
+          st.push(ch);
+          visited[ch-'a']=true;
         }
 
         string ans="";
         while(!st.empty()){
-           ans+=st.top();
-           st.pop();
+          ans+=st.top();
+          st.pop();
         }
         reverse(ans.begin(),ans.end());
         return ans;
+
     }
 };
