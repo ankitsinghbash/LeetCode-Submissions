ff9fed60066a7a5bb6e5d253d6f9db6dba8e7ef6
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-09-28 00:50:05 +0000

    Sync LeetCode submission Runtime - 14 ms (0.38%), Memory - 18.3 MB (100.00%)

--- my-code/2614-maximum-count-of-positive-integer-and-negative-integer/solution.cpp
+++ my-code/2614-maximum-count-of-positive-integer-and-negative-integer/solution.cpp
@@ -1,16 +1,47 @@
 class Solution {
 public:
+   //Now find the last index of positive value;_minimum:
+     int solve(vector<int> &nums){
+         int low=0;;
+         int high=nums.size()-1;
+         while(low<=high){
+             int mid = low+(high-low)/2;
+             if(nums[mid]>0){
+                 high=mid-1;
+             }
+             else{
+                 low=mid+1;
+             }
+         }
+         return low;
+     }
+
+     //maximum idx of negative:
+     int solve1(vector<int> &nums){
+           int low=0;
+           int high=nums.size()-1;
+           while(low<=high){
+               int mid = low+(high-low)/2;
+               if(nums[mid]<0){
+                   low=mid+1;
+               }
+               else{
+                   high = mid-1;
+               }
+           }
+           return high;
+
+     }
+       
+
     int maximumCount(vector<int>& nums) {
-    int pos = 0;
-    int neg = 0;
-    for(int i=0;i<nums.size();i++){
-        if(nums[i]<0){
-            neg++;
-        }
-        else if(nums[i]>0){
-            pos++;
-        }
-    }
-    return max(pos,neg);     
+        int total= nums.size();
+        //first index of positive;
+        int positive_start= solve(nums);
+        int positive = total-positive_start;
+       
+        //last index of negative
+        int negative_idx = solve1(nums);
+        return max(positive,negative_idx+1);
     }
 };
