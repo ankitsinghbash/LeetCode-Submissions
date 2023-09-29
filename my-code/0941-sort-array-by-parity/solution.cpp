9ed8044d8571d68a273de4b706d44a504417b505
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-09-29 09:17:45 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 16.6 MB (100.00%)

--- my-code/0941-sort-array-by-parity/solution.cpp
+++ my-code/0941-sort-array-by-parity/solution.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-        int i=0;
-        int j=nums.size()-1;
-        while(i<=j){
-            if(nums[i]%2==0 && nums[j]%2==0){
-                i++;
-            }
-           else if(nums[i]%2!=0 && nums[j]%2==0){
-                  swap(nums[i],nums[j]);
-                  i++;
-                  j--;
-            }
-            else if(nums[i]%2==0 && nums[j]%2!=0){
-                i++;
-                j--;
-            }
-            else if(nums[i]%2!=0 && nums[j]%2!=0){
-                j--;
-            }
+        
+       int i=0;
+       int j=nums.size()-1;
+       while(i<=j){
+           if(nums[i]%2==0 && nums[j]%2!=0){ //even then odd:
+                   i++;
+                   j--;
+           }
+           else if(nums[i]%2==0 && nums[j]%2==0){ //even then even
+               i++;
+           }
+           else if(nums[i]%2!=0 && nums[j]%2==0){  //odd then even
+               swap(nums[i],nums[j]);
+               i++;
+               j--;
+           }
+           else{     //odd then odd:
+               j--;
+           }
+       }
+ 
 
-        }
+
+
         return nums;
     }
 };
