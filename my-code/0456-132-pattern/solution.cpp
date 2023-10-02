d3cfb1566a497e524f0fb4a527bcad9bf552bf38
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-10-02 17:37:00 +0000

    Sync LeetCode submission Runtime - 70 ms (6.72%), Memory - 48.9 MB (100.00%)

--- my-code/0456-132-pattern/solution.cpp
+++ my-code/0456-132-pattern/solution.cpp
@@ -1,17 +1,36 @@
 class Solution {
 public:
     bool find132pattern(vector<int>& nums) {
-        stack<int> s;
-        int third = INT_MIN;
+        int length = nums.size();
 
-        for (int i = nums.size() - 1; i >= 0; i--) {
-            if (nums[i] < third) return true;
-            while (!s.empty() && s.top() < nums[i]) {
-                third = s.top();
-                s.pop();
+        // Check if the array has less than 3 elements
+        if (length < 3)
+            return false;
+
+        // Stack to keep track of decreasing elements.
+        stack<int> decreasingStack;
+
+        // Maximum value of the third element in the 132 pattern.
+        int maxThirdElement = INT_MIN;
+
+        // Traverse the array from right to left
+        for (int i = length - 1; i >= 0; i--) {
+            int currentNumber = nums[i];
+
+            // Check for 132 pattern
+            if (currentNumber < maxThirdElement)
+                return true;  // Found a 132 pattern.
+
+            // Maintain the stack with decreasing elements
+            while (!decreasingStack.empty() && decreasingStack.top() < currentNumber) {
+                maxThirdElement = decreasingStack.top();
+                decreasingStack.pop();
             }
-            s.push(nums[i]);
+
+            // Push the current element onto the stack.
+            decreasingStack.push(currentNumber);
         }
-        return false;
+
+        return false; // No 132 pattern found
     }
 };
