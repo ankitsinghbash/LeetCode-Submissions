7858fe398b477eb90968fa56b401bc41883aa100
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-10-05 10:25:24 +0000

    Sync LeetCode submission Runtime - 100 ms (8.76%), Memory - 62.5 MB (62.70%)

--- my-code/0817-design-hashmap/solution.cpp
+++ my-code/0817-design-hashmap/solution.cpp
@@ -1,47 +1,46 @@
 class MyHashMap {
-public: 
-    vector<pair<int,int>> v;
+public:
+   typedef pair<int,int> P;
+    vector<list<P>> v;
     MyHashMap() {
-        
+        int M = 10001;
+        v = vector<list<P>>(M,list<P>());
     }
     
     void put(int key, int value) {
-       for(int i=0;i<v.size();i++){
-           if(v[i].first==key){
-               v[i].second = value;
-               return;
-           }
-       }
-       v.push_back({key,value});
+        int idx = key%10001;
+        auto &chain = v[idx];
+        for(auto it=chain.begin();it!=chain.end();it++){
+            pair<int,int> &p = *it;
+            if(p.first==key){
+                p.second=value;
+                return;
+            }
+        }
+        chain.emplace_back(key,value);  //you use direct by emplace_back() function:
+       // chain.push_back({key,value}); //why emplace_back work and push_back not work
     }
     
     int get(int key) {
-         for(int i=0;i<v.size();i++){
-              if(v[i].first==key){
-                  return v[i].second;
-              }
-         }
-         return -1;
+        int idx  = key%10001;
+        auto &chain = v[idx];
+        for(auto it = chain.begin();it!=chain.end();it++){
+            if(it->first==key){
+                return it->second;
+            }
+        }
+        return -1;
     }
     
     void remove(int key) {
-        int idx=-1;
-        for(int i=0;i<v.size();i++){
-            if(v[i].first==key){
-                idx=i;
-                break;
+        int idx = key%10001;
+        auto &chain = v[idx];
+        for(auto it = chain.begin();it!=chain.end();++it){
+            pair<int,int> &p = *it;
+            if(p.first==key){
+                chain.erase(it);
+                return;
             }
-        }
-        if(idx!=-1){
-            v.erase(v.begin()+idx,v.begin()+idx+1);
         }
     }
 };
-
-/**
- * Your MyHashMap object will be instantiated and called as such:
- * MyHashMap* obj = new MyHashMap();
- * obj->put(key,value);
- * int param_2 = obj->get(key);
- * obj->remove(key);
- */
