4801534c4b26ff406f6231651b71df73ea2d6920
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-10-06 10:21:19 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 16.1 MB (100.00%)

--- my-code/0229-majority-element-ii/solution.cpp
+++ my-code/0229-majority-element-ii/solution.cpp
@@ -1,28 +1,53 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        
-        vector<int> ans; //ans wali vector:
-        unordered_map<int,int> mp;
-        for(int i=0;i<nums.size();i++)
-        {
-            mp[nums[i]]++;
-        }
-        
-        int sizes = nums.size();
-        int times = sizes/3;
-        
-        //print element jiska count time se more hai:
-        unordered_map<int,int> :: iterator it;
-        for(it=mp.begin();it!=mp.end();it++)
-        {
-            if((*it).second>times)
-            {
-                ans.push_back((*it).first);
-            }
-        }
-         
-        nums=ans;
-      return nums;
+       //as generarilze formula k-1 element only with [n/k];
+       int major1=0;
+       int major2=0;
+       int cnt2=0;
+       int cnt1=0;
+       for(int i=0;i<nums.size();i++){
+           if(major1==nums[i]){
+               cnt1++;
+           }
+           else if(major2==nums[i]){
+               cnt2++;
+           }
+           else if(cnt1==0){
+               major1=nums[i];
+               cnt1=1;
+           }
+           else if(cnt2==0){
+               major2=nums[i];
+               cnt2=1;
+           }
+           else{
+               cnt1--;
+               cnt2--;
+           }
+       }
+
+
+       cnt1=0;
+       cnt2=0;
+       //verification step means major hoga bhi nahi(Maximum Only 2 element get major):
+       for(int i=0;i<nums.size();i++){
+           if(nums[i]==major1){
+               cnt1++;
+           }
+           else if(nums[i]==major2){
+               cnt2++;
+           }
+       }
+       int n = nums.size();
+       vector<int> result;
+       if(cnt1>floor(n/3)){
+           result.push_back(major1);
+       }
+       if(cnt2>floor(n/3)){
+           result.push_back(major2);
+       }
+       return result;
+
     }
 };
