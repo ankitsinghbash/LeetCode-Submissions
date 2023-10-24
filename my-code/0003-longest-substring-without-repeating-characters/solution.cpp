e2e65b48967777049f163b90e07964892c0e3e3a
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-10-24 07:47:40 +0000

    Sync LeetCode submission Runtime - 20 ms (21.89%), Memory - 11.1 MB (90.89%)

--- my-code/0003-longest-substring-without-repeating-characters/solution.cpp
+++ my-code/0003-longest-substring-without-repeating-characters/solution.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-         
-         int left=0;
-         int right=0;
-         set<char> st;
-         int maxx=0;
-         while(right<s.size())
-         {
-             char ch = s.at(right);
-             if(st.find(ch)==st.end())
-             {
-                 st.insert(ch);
-                 maxx=max(maxx,right-left+1);
-                 
-                 right++;
-             }
-             else
-             {
-                 char ch = s.at(left);
-                 st.erase(ch);
-                 left++;
-             }
+       unordered_set<char> st;
+       int right=0;
+       int left=0;
+       int maxx=0;
+       while(right<s.size()){
+           char ch = s.at(right);
+           if(st.find(ch)==st.end()){
+               st.insert(ch);
+               right++;
+               maxx = max(maxx,right-left);
+           }
+           else{
+               char ch = s.at(left);
+               st.erase(ch);
+               left++;
+           }
+ 
+   
 
-         }
-        return maxx;
 
+
+       }
+       return maxx;
     }
 };
