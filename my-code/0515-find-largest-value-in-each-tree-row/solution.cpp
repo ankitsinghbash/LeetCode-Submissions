19d30b5a1b63b43593ae8ed9d0378bb4f39d4e60
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-10-24 07:54:11 +0000

    Sync LeetCode submission Runtime - 15 ms (2.00%), Memory - 22.4 MB (99.92%)

--- my-code/0515-find-largest-value-in-each-tree-row/solution.cpp
+++ my-code/0515-find-largest-value-in-each-tree-row/solution.cpp
@@ -12,32 +12,29 @@
 class Solution {
 public:
     vector<int> largestValues(TreeNode* root) {
-        vector<int> ans;
         if(root==NULL){
-           return ans;
+            return {};
         }
-
-        //simple the bfs and get the maximum value at each level add to vector ans
-        
-        queue<TreeNode*> qu;
-        qu.push(root);
-        while(!qu.empty()){
+ 
+     vector<int> ans;
+      queue<TreeNode*> qu;
+      qu.push(root);
+    while(!qu.empty()){
             int size = qu.size();
             int maxx = INT_MIN;
             while(size--){
                 TreeNode *node = qu.front();
                 qu.pop();
-                maxx=max(maxx,node->val);
+                maxx = max(maxx,node->val);
                 if(node->left!=NULL){
                     qu.push(node->left);
                 }
                 if(node->right!=NULL){
                     qu.push(node->right);
                 }
-
             }
             ans.push_back(maxx);
-        }
-        return ans;
+    }
+   return ans;
     }
 };
