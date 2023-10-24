7b3c759fc20b9ed5c9681afa82e90c9776b4f91f
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-10-24 01:10:52 +0000

    Sync LeetCode submission Runtime - 292 ms (5.02%), Memory - 125.5 MB (12.87%)

--- my-code/0416-partition-equal-subset-sum/solution.cpp
+++ my-code/0416-partition-equal-subset-sum/solution.cpp
@@ -1,47 +1,33 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        //100% similar to subset sum target problem
-        //https://practice.geeksforgeeks.org/problems/subset-sum-problem-1611555638/1   //subset gfg problem:
-        int n=nums.size();
-        int sum=0;
-        for(int i=0;i<n;i++)
-        {
-            sum+=nums[i];
+        int sum = accumulate(nums.begin(),nums.end(),0);
+        vector<vector<int>> dp(nums.size()+1,vector<int>(sum/2+1));
+        if(sum%2!=0){
+            return false;
         }
-        int dp[n+1][sum/2+1];
-  
-        if(sum%2!=0) return false;  //if sum is odd:
-        dp[0][0]=false;
-        for(int i=0;i<n+1;i++)
-        {
-            dp[i][0]=true;
+        int n = nums.size();
+        for(int i=0;i<n+1;i++){
+            dp[i][0] = true;
         }
-
-        for(int j=0;j<sum/2+1;j++)
-        {
+        for(int j=0;j<sum/2+1;j++){
             dp[0][j]=false;
         }
 
-        for(int i=1;i<n+1;i++)
-        {
-            for(int j=1;j<sum/2+1;j++)
-            {
-                if(j>nums[i-1])   //when j mean sum > arr[i-1] se or equal to then above or negative wale ka or 
-                {
-                    dp[i][j]=dp[i-1][j]||dp[i-1][j-nums[i-1]];
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<sum/2+1;j++){
+                if(j>nums[i-1]){
+                    dp[i][j]=dp[i-1][j] || dp[i-1][j-nums[i-1]];
                 }
-                else if(nums[i-1]==j)  //only extra condition when subset mill gya then direct add true in dp:
-                {
+                else if(j==nums[i-1]){
                     dp[i][j]=true;
                 }
-                else
-                {
-                    //copy above wala:
-                    dp[i][j]=dp[i-1][j];
+                else{
+                    dp[i][j] = dp[i-1][j];
                 }
             }
         }
-       return dp[n][sum/2];
+
+        return dp[n][sum/2];
     }
 };
