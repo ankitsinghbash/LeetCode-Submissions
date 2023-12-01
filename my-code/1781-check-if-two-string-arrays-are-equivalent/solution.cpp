6abcee67e31f0900030075758e78889db4c426cd
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-12-01 22:20:49 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.7 MB (100.00%)

--- my-code/1781-check-if-two-string-arrays-are-equivalent/solution.cpp
+++ my-code/1781-check-if-two-string-arrays-are-equivalent/solution.cpp
@@ -1,13 +1,32 @@
-class Solution 
-{
-     public: 
-     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2)
-      {
-     string s1, s2; 
-     for(auto a:word1)
-      s1+=a; 
-     for(auto a:word2)
-      s2+=a; 
-      return s1==s2;
-       } 
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        int i=0;
+        int j=0;
+        int cnt1=0;
+        int cnt2=0;
+        while(i<word1.size() && j<word2.size()){
+            if(word1[i][cnt1]!=word2[j][cnt2]){
+                return false;
+            }
+            cnt1++;
+            cnt2++;
+            if(cnt1==word1[i].size()){
+                cnt1=0;
+                i++;
+            }
+            if(cnt2==word2[j].size()){
+                cnt2=0;
+                j++;
+            }
+        }
+
+        if(i==word1.size() && j==word2.size()){
+            return true;
+        }
+        return false;
+
+
+
+    }
 };
