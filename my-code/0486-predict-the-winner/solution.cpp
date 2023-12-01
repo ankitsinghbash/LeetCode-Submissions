7d4edb32c2cc5abd51299dc0f4b908cb7dbd90d2
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-12-01 23:35:46 +0000

    Sync LeetCode submission Runtime - 233 ms (5.11%), Memory - 7.8 MB (100.00%)

--- my-code/0486-predict-the-winner/solution.cpp
+++ my-code/0486-predict-the-winner/solution.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-   int solve(int i,int j,vector<int> &nums,bool chance,int myans){
-     
-      if(i>j){
-          return 0;
-      }
     
-      if(chance==true){
-          myans+=max(nums[i]+solve(i+1,j,nums,false,myans),nums[j]+solve(i,j-1,nums,false,myans));     
-      }
-      else{
-           myans+=min(-nums[i]+solve(i+1,j,nums,true,myans),-nums[j]+solve(i,j-1,nums,true,myans)); 
-      }
-      return myans;
-   }
+    int solve(int i,int j,vector<int> &nums,int total,bool flag){
+ 
+        if(j<i){
+            return 0;
+        }
 
+        if(flag==true){
+           total+=max(nums[i]+solve(i+1,j,nums,total,false),nums[j]+solve(i,j-1,nums,total,false));
+        }
+        else{
+           total+=min(-nums[i]+solve(i+1,j,nums,total,true),-nums[j]+solve(i,j-1,nums,total,true));
+        }
 
+        return total;
+
+          
+    }
 
 
     bool predictTheWinner(vector<int>& nums) {
-        int myans=0;
-        int j = nums.size()-1;
-        return solve(0,j,nums,true,myans)>=0;
-
-      // if(myans>=0) return true; //never use this because there is no & referece in our answer:
-        //if i pass reference then backtrack not happen: check both solution: 
-       // return false;
+        int i=0;
+        int j= nums.size()-1;
+        int total=0;
+        return solve(i,j,nums,0,true)>=0;
     }
-  
 };
