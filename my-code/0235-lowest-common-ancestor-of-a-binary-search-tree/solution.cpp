d7027a0550d88b42f112da1525da1693987788c7
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-12-02 02:44:51 +0000

    Sync LeetCode submission Runtime - 28 ms (17.27%), Memory - 23.8 MB (35.58%)

--- my-code/0235-lowest-common-ancestor-of-a-binary-search-tree/solution.cpp
+++ my-code/0235-lowest-common-ancestor-of-a-binary-search-tree/solution.cpp
@@ -1,33 +1,16 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
-
 class Solution {
 public:
-  TreeNode *solve(TreeNode *root,TreeNode *p,TreeNode *q){
-    
-    //dry run for 3->1
-    //note that order of if and else if fix:otherwise dry on 3,1 test case: 
-     if(p->val<root->val && q->val < root->val){
-         return solve(root->left,p,q);
-     }
-     else if(p->val>root->val && q->val>root->val){
-         return solve(root->right,p,q);
-     }
-     else{
-         return root;
-     }
-
-  }
-
-
-
+    TreeNode *solve(TreeNode *root,TreeNode *p,TreeNode *q){
+          //kabhi NULL tak gya hi nahi:
+          if(p->val<root->val && q->val<root->val){
+             return solve(root->left,p,q);
+          }
+          else if(p->val>root->val && q->val>root->val){
+             return solve(root->right,p,q);
+          }
+         
+        return root;
+    }
 
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         return solve(root,p,q);
