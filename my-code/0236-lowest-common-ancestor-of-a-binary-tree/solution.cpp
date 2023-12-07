6a935f9c565b665e976f461dcf8e5ea0c5e4521d
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-12-07 13:18:03 +0000

    Sync LeetCode submission Runtime - 3 ms (99.92%), Memory - 17.6 MB (66.74%)

--- my-code/0236-lowest-common-ancestor-of-a-binary-tree/solution.cpp
+++ my-code/0236-lowest-common-ancestor-of-a-binary-tree/solution.cpp
@@ -1,39 +1,54 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution {
 public:
-
-   TreeNode *solve(TreeNode *root,TreeNode *p,TreeNode *q){
-        if(root==NULL || p==root || q==root){
-            return root;
+    bool solve(TreeNode *root,TreeNode *p,vector<TreeNode*> &path1){
+        if(root==NULL){
+            return false;
         }
 
-       TreeNode *left = solve(root->left,p,q);
-       TreeNode *right = solve(root->right,p,q);
+         if(root->val==p->val){
+             path1.push_back(root);
+             return true;
+         }
 
-       if(left==NULL && right == NULL){
-           return NULL;
-       }
-       if(left==NULL){
-           return right;
-       }
-       if(right==NULL){
-           return left;
-       }
-       else{
-           return root;
-       }
+        bool left = solve(root->left,p,path1);
+        if(left==true){
+            path1.push_back(root);
+            return true;
+        }
 
-   }
+        bool right = solve(root->right,p,path1);
+        if(right==true){
+            path1.push_back(root);
+            return true;
+        }
+
+        return false;
+
+    }
 
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        return solve(root,p,q);
+        //lowest common ancestor using vector<listnode*> method:
+
+       //root to path in a tree:
+        vector<TreeNode*> path1;
+        vector<TreeNode*> path2;
+        solve(root,p,path1);
+        solve(root,q,path2);
+         
+        int len1 = path1.size();
+        int len2 = path2.size();
+       
+
+        int j1 = len1-1;
+        int j2  = len2 -1;
+        while(j1>=0 && j2>=0 && path1[j1]==path2[j2]){
+            j1--;
+            j2--;
+        }
+        return path1[j1+1];
+
+
+        
+
     }
 };
