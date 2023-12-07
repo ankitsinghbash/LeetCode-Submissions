637902484657c3e9b4254fdddf2a3bdeae94eb63
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-12-07 10:40:42 +0000

    Sync LeetCode submission Runtime - 136 ms (8.53%), Memory - 7.7 MB (100.00%)

--- my-code/0486-predict-the-winner/solution.cpp
+++ my-code/0486-predict-the-winner/solution.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
-    
-    int solve(int i,int j,vector<int> &nums,int total,bool flag){
+   int solve(int i,int j,vector<int> &nums){
+       if(j<i){
+           return 0;
+       }
+
+
+       int taken = nums[i] + min(solve(i+2,j,nums),solve(i+1,j-1,nums));
+       int secondtaken = nums[j] + min(solve(i+1,j-1,nums),solve(i,j-2,nums));
+
+       return max(taken,secondtaken);
+
+   }
  
-        if(j<i){
-            return 0;
-        }
-
-        if(flag==true){
-           total+=max(nums[i]+solve(i+1,j,nums,total,false),nums[j]+solve(i,j-1,nums,total,false));
-        }
-        else{
-           total+=min(-nums[i]+solve(i+1,j,nums,total,true),-nums[j]+solve(i,j-1,nums,total,true));
-        }
-
-        return total;
-
-          
-    }
-
-
     bool predictTheWinner(vector<int>& nums) {
-        int i=0;
-        int j= nums.size()-1;
+        //If both play optimal the player it's own turn taken maximum and player2 give
         int total=0;
-        return solve(i,j,nums,0,true)>=0;
+        total = accumulate(nums.begin(),nums.end(),0);
+        int player1 = solve(0,nums.size()-1,nums);
+        int player2 = total-player1;
+        return player1>=player2;
     }
 };
