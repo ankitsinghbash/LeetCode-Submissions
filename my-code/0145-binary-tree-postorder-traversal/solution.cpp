6eb681c820c34503453732b5c838ad54dc053dfe
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-12-09 22:36:06 +0000

    Sync LeetCode submission Runtime - 2 ms (5.75%), Memory - 8.7 MB (100.00%)

--- my-code/0145-binary-tree-postorder-traversal/solution.cpp
+++ my-code/0145-binary-tree-postorder-traversal/solution.cpp
@@ -1,32 +1,30 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
+    vector<int> postorderTraversal(TreeNode* root) {
+               vector<int> store;
+        stack<TreeNode*> st;
+        TreeNode *curr = root;
+        TreeNode *lastVisited = nullptr;
 
-    void helperpost(TreeNode *node,vector<int> &ans)
-    {
-        if(node==NULL)
-        {
-            return;
+        while (curr != nullptr || !st.empty()) {
+            while (curr != nullptr) {
+                st.push(curr);
+                curr = curr->left;
+            }
+
+            TreeNode *node = st.top();
+
+            // Check if we can visit the current node's right subtree
+            if (node->right != nullptr && node->right != lastVisited) {
+                curr = node->right;
+            } else {
+                // Visit the current node since it has no right subtree or the right subtree is already visited
+                store.push_back(node->val);
+                st.pop();
+                lastVisited = node;
+            }
         }
 
-        helperpost(node->left,ans);
-        helperpost (node->right,ans);
-        ans.push_back(node->val);
-    }
-
-    vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        helperpost(root,ans);
-        return ans;
+        return store;
     }
 };
