7a869d36863be825e21ac25b86dbb4462d1fa097
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-12-18 14:52:03 +0000

    Sync LeetCode submission Runtime - 22 ms (49.93%), Memory - 20.6 MB (100.00%)

---
+++ my-code/2042-maximum-product-difference-between-two-pairs/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxProductDifference(vector<int>& nums) {
+        int maxx=INT_MIN;
+        int second_maxx = INT_MIN;
+        int mini = INT_MAX;
+        int second_mini = INT_MAX;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>maxx){
+                second_maxx = maxx;
+                maxx = nums[i];
+            }
+            else{
+                second_maxx = max(second_maxx,nums[i]);
+            }
+            if(nums[i]<mini){
+                second_mini = mini;
+                mini = nums[i];
+            }
+            else{
+                second_mini = min(second_mini,nums[i]);
+            }
+        }
+        int val = maxx*second_maxx-mini*second_mini;
+        return val;
+    }
+};
