f5c9bc3009a5716db994328df32a6999ca6921d0
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-12-26 15:54:29 +0000

    Sync LeetCode submission Runtime - 3 ms (20.62%), Memory - 6.6 MB (100.00%)

--- my-code/1537-maximum-score-after-splitting-a-string/solution.cpp
+++ my-code/1537-maximum-score-after-splitting-a-string/solution.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
-    int data_val1(string &str){
-        int counter=0;
-        for(int i=0;i<str.size();i++){
-            if(str[i]=='1'){
-                counter++;
-            }
-        }
-        return counter;
-    }
-
-     int data_val0(string &str){
-         int counter=0;
-         for(int i=0;i<str.size();i++){
-             if(str[i]=='0'){
-                 counter++;
-             }
-         }
-         return counter;
-     }
-
     int maxScore(string s) {
-        int maxx = INT_MIN;
+        int total_one = 0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='1') total_one++;
+        }
+        int currone=0;
+        int currzero=0;
+        int maxx=INT_MIN;
         for(int i=0;i<s.size()-1;i++){
-            string s1 = s.substr(0,i+1);
-            string s2 = s.substr(i+1);
-            int val1 = data_val0(s1);
-            int val2 = data_val1(s2);
-            maxx = max(maxx,val1+val2);
+                if(s[i]=='0'){
+                    currzero++;
+                }
+                else{
+                    currone++;
+                }
+                int restone = total_one-currone;
+                int ans = currzero+restone;
+                maxx =max(maxx,ans);
         }
         return maxx;
     }
