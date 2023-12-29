c658cf2fa004217bfa8cb96c754b14f841e4dad4
Ankit Singh <ankitsingh4734414code@gmail.com>
2023-12-29 15:39:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.4 MB (100.00%)

--- my-code/0198-house-robber/solution.cpp
+++ my-code/0198-house-robber/solution.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
-
-   int solve(vector<int> &nums,int n,int idx,vector<int> &dp){
-
-        if(idx>=n){
-            return 0;
-        }
-        if(dp[idx]!=-1)
-             return dp[idx];
-      
-         int pick = nums[idx]+solve(nums,n,idx+2,dp);
-         int notpick = solve(nums,n,idx+1,dp);
-          
-         return dp[idx]=max(pick,notpick);
-   }
-
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n+1,-1);
-        return solve(nums,n,0,dp);
-        
+        vector<int> dp(nums.size()+1);
+        dp[0]=0;
+        dp[1]=nums[0];
+
+
+        for(int i=2;i<nums.size()+1;i++){
+            dp[i] = max(dp[i-2]+nums[i-1],dp[i-1]);
+        }
+        return dp[nums.size()];
     }
 };
