022c420931dc646acead001b26093c3ddc319ff2
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-01-02 18:13:30 +0000

    Sync LeetCode submission Runtime - 35 ms (5.12%), Memory - 17.7 MB (100.00%)

--- my-code/0455-assign-cookies/solution.cpp
+++ my-code/0455-assign-cookies/solution.cpp
@@ -1,38 +1,20 @@
 class Solution {
 public:
-    int findContentChildren(vector<int>& boxes, vector<int>& chosize) {
-        int counter=0;
-        for(int i=0;i<chosize.size();i++)
-        {
-            int val = chosize[i];
-            for(int j=0;j<boxes.size();j++)
-            {
-                 if(val==boxes[j])
-                 {
-                     counter++;
-                     boxes.erase(boxes.begin()+j);
-                     chosize.erase(chosize.begin()+i);
-                     break;
-                 }
-            }
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int n = g.size();
+        int m = s.size();
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int i=0;
+        int j=0;
+        int cnt=0;
+        while(i<g.size() && j<s.size()){
+             if(s[j]>=g[i]){
+                 i++;
+                 cnt++;
+             }
+             j++;
         }
-        sort(boxes.begin(),boxes.end());
-        sort(chosize.begin(),chosize.end());
-
-        for(int i=0;i<chosize.size();i++)
-        {
-            int valu = chosize[i];
-            for(int j=0;j<boxes.size();j++)
-            {
-                if(valu>=boxes[j])
-                {
-                    counter++;
-                    boxes.erase(boxes.begin()+j);
-                    break;
-                }
-            }
-        }
-
-        return counter;
+        return i;
     }
 };
