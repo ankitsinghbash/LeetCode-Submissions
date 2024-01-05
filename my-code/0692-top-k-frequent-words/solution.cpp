561ac419bb50b0f42b1fc7e7c85546d715269c54
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-01-05 04:53:13 +0000

    Sync LeetCode submission Runtime - 11 ms (5.79%), Memory - 12.8 MB (100.00%)

--- my-code/0692-top-k-frequent-words/solution.cpp
+++ my-code/0692-top-k-frequent-words/solution.cpp
@@ -1,35 +1,33 @@
-class Cmp{
-    public:
-    bool operator()(pair<string,int> &a,pair<string,int> &b){
-        if(a.second==b.second){
-              return a.first>b.first;
-        }
-        return a.second<b.second;
-            
-    }
-};
-
 class Solution {
 public:
-    typedef pair<string,int> P;
+    struct cmp{
+        bool operator()(pair<string,int> &a,pair<string,int> &b) const{
+            if(a.second == b.second){
+                return a.first<b.first;
+            }
+            return a.second>b.second;
+        }
+    };
     vector<string> topKFrequent(vector<string>& words, int k) {
         unordered_map<string,int> mp;
-        for(auto &v : words){
-            mp[v]++;
+        for(int i=0;i<words.size();i++){
+            string str = words[i];
+            mp[str]++;
         }
 
-        priority_queue<P,vector<P>,Cmp> pq;
-        for(auto it=mp.begin();it!=mp.end();it++){
+        priority_queue<pair<string,int>,vector<pair<string,int>>,cmp> pq; //Image as simple to do what you want as like vector<string,int> vec:  always image that you are in vector and now for visublise priority queue rotate image of vector<int> by 180:
+        for(auto it = mp.begin();it!=mp.end();it++){
             pq.push({it->first,it->second});
+            if(pq.size()>k){
+                pq.pop();
+            }
         }
 
-        vector<string> ans;
-        while(pq.size()>0 && k--){
-            pair<string,int> Pair = pq.top();
+       vector<string> ans(k);
+        for (int i = k - 1; i >= 0; --i) {
+            ans[i] = pq.top().first;
             pq.pop();
-            ans.push_back(Pair.first);
         }
         return ans;
-
     }
 };
