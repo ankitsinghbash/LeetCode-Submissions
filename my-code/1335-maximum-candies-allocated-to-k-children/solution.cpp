43e40029555d7472c5b2d45637590f9fbc35c7fd
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-01-08 19:09:11 +0000

    Sync LeetCode submission Runtime - 157 ms (5.51%), Memory - 84.7 MB (100.00%)

--- my-code/1335-maximum-candies-allocated-to-k-children/solution.cpp
+++ my-code/1335-maximum-candies-allocated-to-k-children/solution.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
- 
-   long long  find_candies(int mid,vector<int> &candies){
+    long long  check(int mid,vector<int> &candies){
        long long  cnt=0;
        for(int i=0;i<candies.size();++i){
             cnt+=candies[i]/mid;
@@ -9,23 +8,21 @@ public:
        return cnt;
    }
 
-
-    int maximumCandies(vector<int>& candies, long long k) {
-        int low=1;
-        int high = *max_element(candies.begin(),candies.end());
-        //int ans=0;
-        while(low<=high){
-             long long  mid = low+(high-low)/2;
-             if(find_candies(mid,candies)<k){
-                      high = mid-1;
+    int maximumCandies(vector<int>& arr, long long k) {
+        int low=0;
+        int high = *max_element(arr.begin(),arr.end());
+        int result = 0;
+        while(low<high){
+             int mid = (low+high+1)/2;
+             if(check(mid,arr)>=k){
+                 low = mid;
+                 result = mid;
              }
-             else
-              {
-                    //  ans=mid;
-                      low=mid+1;
-              }
-
-        }
-        return low-1;
-    }
+            else{
+                high = mid-1;
+            }
+           }
+        return result;
+      }
+    
 };
