df0f6ee0f65ce1b13595113caff5f8cc38a07627
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-01-09 18:21:59 +0000

    Sync LeetCode submission Runtime - 27 ms (56.41%), Memory - 12.7 MB (100.00%)

--- my-code/0200-number-of-islands/solution.cpp
+++ my-code/0200-number-of-islands/solution.cpp
@@ -1,38 +1,23 @@
 class Solution {
 public:
-	int numIslands(vector<vector<char>>& grid) {
-		vector<vector<bool>>v(grid.size(), vector<bool>(grid[0].size(), false));
-		queue<pair<int, int>>q;
-		int cnt=0;
-		for(int i=0; i<grid.size(); i++){
-			for(int j=0; j<grid[0].size(); j++){
-				if(!v[i][j] && grid[i][j]=='1'){
-					v[i][j]=true;
-					cnt++;
-					q.push({i, j});
-					while(!q.empty()){
-						int x=q.front().first, y=q.front().second;
-						q.pop();
-						if(x>0 && grid[x-1][y]=='1' && !v[x-1][y]){
-							v[x-1][y]=true;
-							q.push({x-1, y});
-						}
-						if(y>0 && grid[x][y-1]=='1' && !v[x][y-1]){
-							v[x][y-1]=true;
-							q.push({x, y-1});
-						}
-						if(x<grid.size()-1 && grid[x+1][y]=='1' && !v[x+1][y]){
-							v[x+1][y]=true;
-							q.push({x+1, y});
-						}
-						if(y<grid[0].size()-1 && grid[x][y+1]=='1' && !v[x][y+1]){
-							v[x][y+1]=true;
-							q.push({x, y+1});
-						}
-					}
-				}
-			}
-		}
-		return cnt;
-	}
+    void dfs(int i,int j,vector<vector<char>> &grid){
+        if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size() || grid[i][j]=='0') return; 
+        grid[i][j]='0';
+        dfs(i+1,j,grid);
+        dfs(i-1,j,grid);
+        dfs(i,j+1,grid);
+        dfs(i,j-1,grid);
+}
+    int numIslands(vector<vector<char>>& grid) {
+          int island=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                      if(grid[i][j]=='1'){
+                          island++;
+                          dfs(i,j,grid);
+                      }
+            }
+        }
+        return island;
+    }
 };
