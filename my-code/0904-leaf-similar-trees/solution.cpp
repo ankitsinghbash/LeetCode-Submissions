1c18f11136e47c1cf081ad890af04dbf5a3d9c92
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-01-09 02:54:40 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.2 MB (99.97%)

--- my-code/0904-leaf-similar-trees/solution.cpp
+++ my-code/0904-leaf-similar-trees/solution.cpp
@@ -11,57 +11,27 @@
  */
 class Solution {
 public:
-   vector<int> v1;
-   vector<int> v2;
- 
-    void addleave2(TreeNode *root2){
-            
-      if(root2==NULL){
-          return;
-      }
-
-      if(root2->left==NULL&&root2->right==NULL){
-          v2.push_back(root2->val);
-          cout<<"Data root2 "<<root2->val<<" ";
-          return;
-      }
-          
-          addleave2(root2->left);
-          addleave2(root2->right);
+    vector<int> first;
+    vector<int> second;
+    void solve(TreeNode *root,bool flag){
+        if(root==NULL) return;  
         
-   }
-
-
-
-   void addleave1(TreeNode *root1){
-            
-      if(root1==NULL){
-          return;
-      }
-
-      if(root1->left==NULL&&root1->right==NULL){
-          v1.push_back(root1->val);
-          cout<<"data root1 "<<root1->val<<" ";
-        //   return;
-      }
-         
-          addleave1(root1->left);
-          addleave1(root1->right);
-         
-   }
-
+        if(root->left==NULL && root->right==NULL && flag==true){
+             first.push_back(root->val);
+        }
+        
+        if(root->left==NULL && root->right==NULL && flag==false){
+             second.push_back(root->val);
+        }
+        
+        solve(root->left,flag);
+        solve(root->right,flag);
+    
+    }
+    
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        addleave1(root1);
-        cout<<endl;
-        addleave2(root2);
-        
-        // if(v1.size()!=v2.size()) return false;
-
-        // for(int i=0,j=0;i<v1.size();i++,j++){
-        //     if(v1[i]!=v2[j]) return false;
-        // }
-        // return true;
-        if(v1 == v2) return true;
-        return false;
+        solve(root1,true);
+        solve(root2,false);
+        return first==second ? true : false;
     }
 };
