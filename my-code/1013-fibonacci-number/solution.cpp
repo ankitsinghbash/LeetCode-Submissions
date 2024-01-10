623c5a8a2cf1f83167a70ed9689c7142ede9b09a
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-01-10 19:04:12 +0000

    Sync LeetCode submission Runtime - 2 ms (78.79%), Memory - 6.3 MB (100.00%)

--- my-code/1013-fibonacci-number/solution.cpp
+++ my-code/1013-fibonacci-number/solution.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
-   int solve(int n){
-       if(n==0 || n==1){
-           return n;
-       }
-
-       int value = solve(n-1)+solve(n-2);
-       return value;
-   }
- 
-
-
-
     int fib(int n) {
-        return solve(n);
+        vector<int> dp(31);//[0,1,1,2]
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<n+1;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
