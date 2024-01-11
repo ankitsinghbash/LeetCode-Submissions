409d5e38523379468899685ed67a73608589d93f
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-01-11 17:18:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 6.4 MB (100.00%)

--- my-code/0069-sqrtx/solution.cpp
+++ my-code/0069-sqrtx/solution.cpp
@@ -1,7 +1,25 @@
 class Solution {
 public:
+    typedef long long ll;
     int mySqrt(int x) {
-        int val = sqrt(x);
-        return val;
+        if(x==0 || x==1) return x;
+        ll low  =1;
+        ll high =x;
+        int result = -1;
+        while(low<high){
+            ll mid = low+(high-low)/2;
+            ll val = mid*mid;
+            if(x==val){
+                return mid;
+            }
+            else if(mid*mid<x){
+                result =  mid;
+                low = mid+1;
+            }
+            else{
+                high = mid;
+            }
+        }
+        return result;
     }
 };
