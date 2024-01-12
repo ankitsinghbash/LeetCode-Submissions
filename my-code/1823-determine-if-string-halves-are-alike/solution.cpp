95cc2d26ebdf3642004394add4e067f64464f154
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-01-12 03:56:00 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 6.9 MB (100.00%)

--- my-code/1823-determine-if-string-halves-are-alike/solution.cpp
+++ my-code/1823-determine-if-string-halves-are-alike/solution.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
-    bool halvesAreAlike(string str) {
-        
-    int n=str.size();
-    int counter1=0;
-    int i;
-    for(i=0;i<n/2;i++)
-    {
-      char ch = str.at(i);
-      if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
-        counter1++;
-      }
+    bool check(char ch){
+        if(ch=='a' || ch=='e' || ch=='i' ||ch=='o'||ch=='u'||ch=='A' ||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
+            return true;
+        }
+        return false;
     }
-    
-    int counter2=0;
-    for(int j=i;j<n;j++)
-    {
-      char ch = str.at(j);
-      if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
-        counter2++;
-      }
-      
-    }
-    if(counter1==counter2)
-    {
-       return true;
-    }
-    else
-    {
-       return false;
-    }
-  
-     return 0;
+    bool halvesAreAlike(string s) {
+        int cnt=0;
+        for(int i=0;i<s.size();i++){
+            char ch  = s[i];
+            if(check(ch)){
+                cnt++;
+            }
+        }
+         
+        int cnt2=0;
+        for(int i=0;i<s.size()/2;i++){
+            char ch = s[i];
+            if(check(ch)){
+                cnt2++;
+            }
+        }
+        int val = 2*cnt2-cnt;
+        if(val==0) return true;
+        return false;
+       
     }
 };
