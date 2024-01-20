ebe9408a3222a0cec12013da4c94d863f336faea
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-01-20 03:16:04 +0000

    Sync LeetCode submission Runtime - 2 ms (4.96%), Memory - 7.1 MB (100.00%)

--- my-code/0070-climbing-stairs/solution.cpp
+++ my-code/0070-climbing-stairs/solution.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
-    
     int climbStairs(int n) {
-        //tabulation:
-        int dp[46];  
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=2;
-        for(int i=3;i<n+1;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n];
+       vector<int> dp(46,0);
+       dp[1]=1;
+       dp[2]=2;
+       for(int i=3;i<n+1;i++){
+           dp[i]=dp[i-1]+dp[i-2];
+       } 
+       return dp[n];
     }
 };
