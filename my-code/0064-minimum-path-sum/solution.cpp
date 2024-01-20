dde5d2d31b6d4980828ca98a4f661c868bbe87ed
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-01-20 11:14:35 +0000

    Sync LeetCode submission Runtime - 7 ms (5.52%), Memory - 12.5 MB (100.00%)

--- my-code/0064-minimum-path-sum/solution.cpp
+++ my-code/0064-minimum-path-sum/solution.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
-    int minPathSum(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
+    int minPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
         vector<vector<int>> dp(n,vector<int>(m));
-        for(int i=n-1;i>=0;i--){
-            for(int j=m-1;j>=0;j--){
-                if(i==n-1 && j==m-1){
-                    dp[i][j] = grid[i][j]; 
-                }
-                else if(i==n-1){
-                    dp[i][j] = dp[i][j+1]+grid[i][j];
-                }
-                else if(j==m-1){
-                    dp[i][j] = dp[i+1][j] + grid[i][j];
-                }
-                else{
-                    dp[i][j] = grid[i][j] + min(dp[i+1][j],dp[i][j+1]);
-                }
+        dp[0][0]=matrix[0][0];
+        int prev=matrix[0][0];
+        for(int i=1;i<n;i++){
+            prev = matrix[i][0]+prev;
+            dp[i][0]=prev;
+        }
+
+        int oldprev = matrix[0][0];
+        for(int j=1;j<m;j++){
+            oldprev = matrix[0][j]+oldprev;
+            dp[0][j]=oldprev;
+        }
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                int val = min(dp[i-1][j],dp[i][j-1]);
+                dp[i][j] = matrix[i][j]+val;
             }
         }
-        return dp[0][0];
 
+        return dp[n-1][m-1];
 
     }
 };
+
+
