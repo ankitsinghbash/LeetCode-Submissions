dfb7814d586d6e1146478e7e7695db935fcd9dd6
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-01-25 11:23:39 +0000

    Sync LeetCode submission Runtime - 4 ms (23.19%), Memory - 11 MB (100.00%)

--- my-code/0496-next-greater-element-i/solution.cpp
+++ my-code/0496-next-greater-element-i/solution.cpp
@@ -1,89 +1,33 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-       
-       vector<int> ans;
-
-      map<int,int> mp; 
-      for(int i=0;i<nums2.size();i++)
-      {
-          mp[nums2[i]]=i;
-      }
-
-      for(int i=0;i<nums1.size();i++)
-      {
-          int idx = mp[nums1[i]];
-          bool flag=true;
-          for(int j=idx;j<nums2.size();j++)
-          {
-              if(nums2[j]>nums1[i])
-              {
-                  flag=false;
-                  ans.push_back(nums2[j]);
-                  break;
-              }
-          }
-          if(flag==true)
-          {
-              ans.push_back(-1);
-          }
+        int n = nums2.size();
+        int m = nums1.size();
+        vector<int> arr_nextgreater(nums2.size(),-1);
+        stack<int> st;
+        st.push(nums2[n-1]);
+        for(int i=n-2;i>=0;i--){
+            while(!st.empty() && nums2[i]>=st.top()){
+                st.pop();
+            }
+            if(st.empty()){
+                arr_nextgreater[i]=-1;
+            }
+            else{
+                arr_nextgreater[i]=st.top();
+            }
+            st.push(nums2[i]);
+        }
 
 
-      }
-      return ans;
+    vector<int> ans;
+    for(int i=0;i<nums1.size();i++){
+        int val = nums1[i];
+        //find val in nums2 give index idx:
+        int idx = find(nums2.begin(),nums2.end(),val)-nums2.begin();
+        ans.push_back(arr_nextgreater[idx]);
 
     }
+    return ans;
+    }
 };
-
-
-/*Give TLE:
-
- int binary(int key,vector<int> &nums2,int n)
-   {
-      int low=0;
-	  int high=n-1;
-	  while(low<=high)
-	  {
-		  int mid=(high+low)/2;
-		  if(nums2[mid]==key)
-		  {
-			  return mid;
-		  }
-		  else if(key<nums2[mid])
-		  {
-			  high=mid-1;
-		  }
-		  else
-		  {
-			  low=mid+1;
-		  }
-	  }
-      return -1;
-   }
-
-
-      vector<int> ans;
-        for(int i=0;i<nums1.size();i++)
-        {
-            int val = nums1[i];
-            //idx of val in the nums2;
-            int n=nums2.size();
-            int idx = binary(val,nums2,n);
-            int j = idx+1;
-            bool flag=true;
-            while(j!=n)
-            {
-               if(nums2[j]>val)
-               {
-                   flag=false;
-                   ans.push_back(nums2[j]);
-                   break;
-               }
-            }
-            if(flag==true)
-            {
-                ans.push_back(-1);
-            }
-        }
-        return ans;
-        */
