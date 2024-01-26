2b4cf560770e789f3f2f3a1d494b95dbffb718ad
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-01-26 14:34:51 +0000

    Sync LeetCode submission Runtime - 57 ms (5.03%), Memory - 105.7 MB (5.03%)

--- my-code/0576-out-of-boundary-paths/solution.cpp
+++ my-code/0576-out-of-boundary-paths/solution.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
-    int dp[55][55][55];
-    long long mod = 1e9 + 7;
-    int dfs(int i, int j, int n, int m, int x){
-        
-        if(i < 0 || i >= n || j < 0 || j >= m)
-            return 1;
-        if(x <= 0)
-            return 0;
-        if(dp[i][j][x] != -1)
-            return dp[i][j][x];
-        
-        int up = dfs(i-1,j,n,m,x-1);
-        int down = dfs(i+1,j,n,m,x-1);
-        int left = dfs(i,j-1,n,m,x-1);
-        int right = dfs(i,j+1,n,m,x-1);
-        int total = (up % mod + down % mod + left % mod + right % mod) % mod;
-        dp[i][j][x] = total;
-        return dp[i][j][x];
-    }
-    
-    int findPaths(int n, int m, int maxMove, int Row, int Column) {
-        memset(dp, -1, sizeof(dp));
-        return dfs(Row,Column,n,m,maxMove);
+   
+   const int mod = 1e9+7;
+   int solve(int i,int j,int m,int n,int maxmove,vector<vector<vector<int>>> &dp){
+         
+           if(maxmove<0) return 0;
+
+           if(i<0 || j<0 || i>=m || j>=n){
+                if(maxmove>=0){
+                    return 1;
+                }
+                return 0;
+           }
+           
+           if(dp[i][j][maxmove]!=-1) return dp[i][j][maxmove];
+
+           int total=0;
+           total=(total%mod+solve(i+1,j,m,n,maxmove-1,dp)%mod)%mod;
+           total=(total%mod+solve(i-1,j,m,n,maxmove-1,dp)%mod)%mod;
+           total=(total%mod+solve(i,j+1,m,n,maxmove-1,dp)%mod)%mod;
+           total=(total%mod+solve(i,j-1,m,n,maxmove-1,dp)%mod)%mod;
+
+           dp[i][j][maxmove] =  total%mod;
+           return dp[i][j][maxmove];
+   }
+
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        vector<vector<vector<int>>> dp(51,vector<vector<int>>(51,vector<int>(51,-1)));
+        return solve(startRow,startColumn,m,n,maxMove,dp);
     }
 };
