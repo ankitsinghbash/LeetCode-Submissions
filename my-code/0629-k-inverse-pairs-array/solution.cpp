003a7f675a4460e685ccf5affdf4a6474b26bba2
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-01-27 19:55:55 +0000

    Sync LeetCode submission Runtime - 1572 ms (23.14%), Memory - 11.6 MB (87.00%)

---
+++ my-code/0629-k-inverse-pairs-array/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    //tabulation:
+    int dp[1001][1001], mod = 1e9 + 7;
+    long long dfs(int n, int k){
+        if(n == 0) return 0;
+        if(k == 0) return 1;
+        if(dp[n][k] != -1) return dp[n][k];
+        long long last = min(k, n-1), ans = 0;
+        // f(n,k)=f(n−1,k)+f(n−1,k−1)+f(n−1,k−2)+⋯+f(n−1,k−n+1)
+        for(int i = 0;i <= last;i++)
+            ans = (ans + dfs(n-1, k-i))%mod;
+        return dp[n][k] = ans%mod;
+    }
+public:
+    int kInversePairs(int n, int k) {
+        memset(dp, -1, sizeof(dp));
+        return dfs(n, k) % mod;
+    }
+};
