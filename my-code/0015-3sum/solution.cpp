9d8f60f317d22f6073e49f957683c6a0d5b7c042
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-01-29 00:24:21 +0000

    Sync LeetCode submission Runtime - 102 ms (12.58%), Memory - 38.2 MB (10.75%)

--- my-code/0015-3sum/solution.cpp
+++ my-code/0015-3sum/solution.cpp
@@ -1,52 +1,44 @@
 class Solution {
 public:
-    //https://redquark.org/leetcode/0015-3-sum/          approach:
     vector<vector<int>> threeSum(vector<int>& nums) {
-    vector<vector<int>> mainans;
-    sort(nums.begin(),nums.end());
-    for(int i=0;i<nums.size();i++)
-    {
-        //remove duplicaty:
-        if(i>0&&nums[i]==nums[i-1])   //we remove this condition by usiing set:
-        {
-            continue;
-        }
-        int j=i+1;
-        int k=nums.size()-1;
-        while(j<k)
-        {
-            
-           if(nums[i]+nums[j]+nums[k]==0)
-           {
-              vector<int> ans;
-              ans.push_back(nums[i]);
-              ans.push_back(nums[j]);
-              ans.push_back(nums[k]);
-              mainans.push_back(ans);
-              j++;
-              while(j<k&&nums[j]==nums[j-1])   //we also use set:
-              {
-               j++; //remove j ki duplicaty:
-              }
-           }
-           else if(nums[i]+nums[j]+nums[k]<0)
-           {
-               //array sort the total < 0 required zero hai mean j++;
-               j++;
-           }
-           else
-           {
-               k--;
-           }
-        }
-
-    }
-    return mainans;
+            vector<vector<int>> store;
+            set<vector<int>> st;
+             sort(nums.begin(),nums.end());
+             for(int i=0;i<=nums.size()-3;i++){
+                 int j=i+1;
+                 int k = nums.size()-1;
+                 int val =  -nums[i];
+                 vector<int> ans;
+                 while(j<k){
+                       if(nums[j]+nums[k]>val){
+                           k--;
+                       }
+                       else if(nums[j]+nums[k]<val){
+                               j++;
+                       }
+                       else if(nums[j]+nums[k]==val){
+                            while(j<k && nums[j]==nums[j+1]){
+                               j++;
+                             }
+                            while(j<k && nums[k]==nums[k-1]){
+                                k--;
+                            } 
+                            ans.push_back(nums[i]);
+                            ans.push_back(nums[j]);
+                            ans.push_back(nums[k]);
+                            j++;
+                            k--;
+                            st.insert(ans);
+                            ans.clear();
+                       }
+                 }
+             } 
+             
+             for(auto it = st.begin();it!=st.end();it++){
+                 vector<int> val = *it;
+                 store.push_back(val);
+             }
+             return store;
+        
     }
 };
-
-
-
-
-
-
