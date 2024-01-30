61615328c1eacfc1e891fdcbe578f8f4a781f93e
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-01-30 12:16:10 +0000

    Sync LeetCode submission Runtime - 7 ms (6.72%), Memory - 16.2 MB (91.19%)

--- my-code/0150-evaluate-reverse-polish-notation/solution.cpp
+++ my-code/0150-evaluate-reverse-polish-notation/solution.cpp
@@ -1,45 +1,39 @@
 class Solution {
 public:
-         stack<long long> st; 
-        int evalRPN(vector<string>& tokens) {
-    	
-        string first = tokens[0];
-        int val = stoi(first);
-        st.push(val);
-
-    	for (long long i = 1; i < tokens.size(); i++) {
-            string str = tokens[i];
-    		if (str != "+" && str != "-" && str != "*" && str != "/"){
-                int val  = stoi(str);
-    			st.push(val);    //atoi(tokens[i].c_str()) also use to convert string to integer:
+    int evalRPN(vector<string>& vec) {
+        stack<string> st;
+        for(int i=0;i<vec.size();i++){
+            string ch  = vec[i];
+            if(ch=="+" || ch=="*" || ch=="-" || ch=="/"){
+                   string B = st.top();
+                   st.pop();
+                   string A = st.top();
+                   st.pop();
+                   int val_b = stoi(B);
+                   int val_a = stoi(A);
+                   int result;
+                   if(ch=="+"){
+                       result = val_a+val_b;
+                   }
+                   else if(ch=="-"){
+                       result = val_a-val_b;
+                   }
+                   else if(ch=="*"){
+                       result = val_a*val_b;
+                   }
+                   else{
+                       result = val_a/val_b;
+                   }
+                   string new_str = to_string(result);
+                   st.push(new_str);
             }
-    		else {
-    			long long operand2 = st.top();
-    			st.pop();
-    			long long operand1 = st.top();
-    			st.pop();
-    			if (str == "+"){
-    				long ans = operand1 + operand2;
-                    st.push(ans);
-                }
-    			else if (str == "-"){
-    				 long long  ans = operand1 - operand2;
-                     st.push(ans);
-                }
-    			else if (str == "*"){
-    				long long  ans = operand1 * operand2;
-                    st.push(ans);
-                }
-    			else{
-                    long long ans = operand1 / operand2;
-                    st.push(ans);
-                }
-    				
-    		} 
-    	} 
-    	return st.top();
-     }
+            else{
+                st.push(ch);
+            }
+        }
+
+        string s = st.top();
+        int ans = stoi(s);
+        return ans;
+    }
 };
-
-
-
