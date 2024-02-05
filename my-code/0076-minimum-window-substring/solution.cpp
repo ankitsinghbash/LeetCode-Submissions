c63797e9d099f7df5ae4821dd03421330ff810c2
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-02-05 23:14:54 +0000

    Sync LeetCode submission Runtime - 32 ms (14.26%), Memory - 10.6 MB (99.98%)

--- my-code/0076-minimum-window-substring/solution.cpp
+++ my-code/0076-minimum-window-substring/solution.cpp
@@ -1,52 +1,48 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int n = s.size();
-        int tn= t.size();
-        int left = 0, right;
-        unordered_map<char, int> mp, tmp;
+             unordered_map<char,int> mp1,mp2;  //1 for s and 2 for t:
+             for(int i=0;i<t.size();i++){
+                 mp2[t[i]]++;
+             }
+ 
+             int start=0;
+             int maxx = INT_MAX;
+
+             int right=0;
+             int cnt=0;
+             int left=0;
+             while(right<s.size()){
+                    char ch = s[right];
+
+                    if(mp2.count(ch)==0){
+                        right++;
+                        continue;
+                    }
         
-        // Count the characters in string t
-        for (char c : t)
-            tmp[c]++;
-        
-        int count = 0; // Counter for matching characters
-        
-        int minLength = INT_MAX; // Length of the minimum window
-        int minLeft = 0; // Start index of the minimum window
-        
-        for (right = 0; right < n; right++) {
-            char c = s[right];
-            
-            // Skip characters not present in string t
-            if (tmp.count(c) == 0) continue;
-            
-            // Increment the count of matching characters
-            mp[c]++;
-            
-            // Check if the current character count matches the required count
-            if (mp[c] <= tmp[c]) count++;
-            
-            // If all characters of t are found in the current window
-            if (count == tn) {
-                // Try to minimize the window by moving the left pointer
-                while (tmp.count(s[left])==0|| mp[s[left]]>tmp[s[left]]) {
-                    if (mp.count(s[left])!=0) mp[s[left]]--;
-                    left++;
-                }
-                
-                // Update the minimum window if necessary
-                if (right - left + 1 < minLength) {
-                    minLength = right - left + 1;
-                    minLeft = left;
-                }
-            }
-        }
-        
-        // If no valid window found, return an empty string
-        if (minLength == INT_MAX) return "";
-        
-        // Return the minimum window substring
-        return s.substr(minLeft, minLength);
+                   
+                    if(mp1[ch]<mp2[ch]) cnt++;
+                    //if ch is present in mp2:
+                    mp1[ch]++;
+
+                    if(cnt==t.size()){
+                           //shrink:
+                           while(mp2.count(s[left])==0 || mp1[s[left]]>mp2[s[left]]){
+                               if(mp1[s[left]]>0){
+                                   mp1[s[left]]--;
+                               }
+                               left++;
+                           }
+
+                          
+                          if(maxx>right-left+1){
+                              start = left;
+                               maxx = right-left+1;
+                          }
+                    }
+                  right++;
+             }
+
+             return maxx==INT_MAX ? "" : s.substr(start,maxx);
     }
 };
