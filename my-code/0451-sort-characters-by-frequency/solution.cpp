e47d0bd7fa6a50eb5fde5838fde725fc902086f0
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-02-07 01:25:10 +0000

    Sync LeetCode submission Runtime - 9 ms (8.90%), Memory - 10.9 MB (70.17%)

--- my-code/0451-sort-characters-by-frequency/solution.cpp
+++ my-code/0451-sort-characters-by-frequency/solution.cpp
@@ -1,56 +1,28 @@
 class Solution {
 public:
-
-   static bool cmp(pair<char,int> &a,pair<char,int> &b)
-   {
-       return a.second>b.second;
-   }
-
+    struct cmp{
+         bool operator()(pair<char,int> &a,pair<char,int> &b) const{
+             
+                return a.second>b.second;
+         }
+    };
 
     string frequencySort(string s) {
-        vector<char> v;
-        for(int i=0;i<s.size();i++)
-        {
-            char ch = s.at(i);
-            v.push_back(ch);
+        unordered_map<char,int> mp;
+        for(int i=0;i<s.size();i++){
+            mp[s[i]]++;
         }
-
-        vector<pair<char,int>> v_pair;
-        int n=v.size();
-   vector<bool> visited(n,false);
-    for(int i=0;i<n;i++)
-   {
-     if(visited[i]!=true)
-     {
-       int counter=0;
-       for(int j=0;j<n;j++)
-       {
-         if(v[i]==v[j])
-         {
-           counter++;
-           visited[j]=true;
-         }
-         
-       }
-       v_pair.push_back({v[i],counter});
-     }
-   }
-   
-   sort(v_pair.begin(),v_pair.end(),cmp);
-   
-   
-   
-   string ans;
-   for(int i=0;i<v_pair.size();i++)
-   {
-     int times = v_pair[i].second;
-     while(times!=0)
-     {
-       ans+=v_pair[i].first;
-       times--;
-     }
-   }
-
-      return ans;
+       
+        vector<pair<char,int>> vec(mp.begin(),mp.end());
+        sort(vec.begin(),vec.end(),cmp());
+        string ans="";
+        for(int i=0;i<vec.size();i++){
+            char ch = vec[i].first;
+            int time = vec[i].second;
+            while(time--){
+                ans+=ch;
+            }
+        }
+       return ans;
     }
 };
