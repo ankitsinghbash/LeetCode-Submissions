580d7854d67d21e564d163339f7211eeb864f360
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-02-15 23:39:01 +0000

    Sync LeetCode submission Runtime - 4 ms (52.87%), Memory - 8.5 MB (89.44%)

--- my-code/0097-interleaving-string/solution.cpp
+++ my-code/0097-interleaving-string/solution.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-   bool solve(int i,int j,int k,string s1,string s2,string s3,vector<vector<int>> &dp){
-      if(i==s1.size() && j==s2.size() && k==s3.size()){
-           return true;
-       }
-    
-       if(dp[i][j]!=-1){
-           return dp[i][j]; 
-       }
+    int dp[101][101];
+    bool solve(int i,int j,int k,string &s1,string &s2,string &s3){
+              if(i==s1.size() && j==s2.size() && k==s3.size()){
+                  return true;
+              }
+              
+              if(dp[i][j]!=-1) return dp[i][j];
+  
+              bool flag1=false;
+              bool flag2=false;
+              if(i<s1.size() && s1[i]==s3[k]){
+                  flag1 = solve(i+1,j,k+1,s1,s2,s3);
+              }
+              if(j<s2.size() && s2[j]==s3[k]){
+                  flag2 = solve(i,j+1,k+1,s1,s2,s3);
+              }
 
-        bool left = false;
-        bool right = false;
-        if(i<s1.size() && s1[i]==s3[k]){
-            left = solve(i+1,j,k+1,s1,s2,s3,dp);
-        }
-
-        if(j<s2.size() && s2[j]==s3[k]){
-          right =  solve(i,j+1,k+1,s1,s2,s3,dp);
-        }
-
-
-       return dp[i][j] = left||right;
-
-   }
+             return dp[i][j] = flag1 || flag2;
+    }
 
 
     bool isInterleave(string s1, string s2, string s3) {
-        vector<vector<int>> dp(101,vector<int>(101,-1));
-        return solve(0,0,0,s1,s2,s3,dp);
+        if(s1.size()+s2.size()!=s3.size()) return false;
+        memset(dp,-1,sizeof(dp));
+        return solve(0,0,0,s1,s2,s3);
     }
 };
