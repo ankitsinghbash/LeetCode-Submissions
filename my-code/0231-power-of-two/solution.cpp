d068aa55710ef8069c1a462a67735604b2946891
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-02-19 12:56:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.2 MB (100.00%)

--- my-code/0231-power-of-two/solution.cpp
+++ my-code/0231-power-of-two/solution.cpp
@@ -1,26 +1,11 @@
 class Solution {
 public:
-
-   bool check(long long n,long long val){
-      if(n==1) return true;
-
-      val=2*val;
-      
-      if(val==n){
-          return true;
-      }
-
-      if(val>n){
-          return false;
-      }
-
-      return check(n,val);
-      
-   }
-
-    bool isPowerOfTwo(long long n) {
-        bool ans=check(n,1);
-        if(ans==true) return true;
-        else return false;
+    bool isPowerOfTwo(int n) {
+        if(n==0) return false;
+        if(n<0) return false;
+        if((n&(n-1))==0){
+            return true;
+        }
+        return false;
     }
 };
