e6747ce7843c07ac38c06efa6fe1cec5b6137d4f
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-02-20 13:25:57 +0000

    Sync LeetCode submission Runtime - 69 ms (8.39%), Memory - 57.6 MB (100.00%)

--- my-code/1762-furthest-building-you-can-reach/solution.cpp
+++ my-code/1762-furthest-building-you-can-reach/solution.cpp
@@ -1,35 +1,41 @@
 class Solution {
 public:
-    //heap:
     int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
-        int n = heights.size();
-        int i = 0;
-        priority_queue<int> pq;
-
-        for (i = 0; i < n - 1; i++) {
-            if (heights[i] >= heights[i+1]) {
-                continue;
-            }
-            int diff = heights[i+1] - heights[i];
-            if (diff <= bricks) {
-                bricks -= diff;
-                pq.push(diff);
-            } else if (ladders > 0) {
-                if (pq.size()) {
-                    int x = pq.top();
-                    if (x > diff) {
-                        bricks += x;
-                        pq.pop();
-                        pq.push(diff);
-                        bricks -= diff;
-                    }
-                }
-                ladders--;
-            } else {
-                break;
-            }
-        }
-
-        return i;
+         priority_queue<int,vector<int>> pq;
+         int i=0;
+         for(;i<heights.size()-1;i++){
+              int diff = heights[i+1]-heights[i];
+              if(diff<=0){
+                  continue;
+              }
+              //You have a brick greater then as required:
+              if(bricks>=diff){
+                  bricks = bricks-diff;  //left brick for next jump:
+                  pq.push(diff);    //Jumping possible with these of bricK , store in pq give the maximum old brick pick:
+              }
+              else if(ladders>0){
+                     if(!pq.empty()){
+                         //means in my past i use maximum brick but currently i required less number of bricks:
+                         int maxx_prev = pq.top();
+                         if(maxx_prev<=diff){
+                             ladders--;
+                         }
+                         else{
+                             bricks = bricks+maxx_prev;
+                             pq.pop();
+                             pq.push(diff);
+                             bricks = bricks-diff;
+                             ladders--;
+                         }
+                     }
+                     else{
+                         ladders--;
+                     } 
+              }
+              else{
+                  break;
+              }
+         }
+         return i;
     }
 };
