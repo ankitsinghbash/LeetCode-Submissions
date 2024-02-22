10134329cf5ed84c163af370c3a57bc034a3da75
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-02-22 10:53:06 +0000

    Sync LeetCode submission Runtime - 100 ms (5.06%), Memory - 64.4 MB (100.00%)

--- my-code/1039-find-the-town-judge/solution.cpp
+++ my-code/1039-find-the-town-judge/solution.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int> indegree(n+1);   //a-->b means b is indegree and a is outdegree;
-        vector<int> outdegree(n+1);
-        for(vector<int> &x : trust){
-            int u = x[0];
-            int v  =  x[1];
+        vector<int> indegree(n+1,0);
+        vector<int> outdegree(n+1,0);
+
+        for(int i=0;i<trust.size();i++){
+            int u =  trust[i][0];
+            int v = trust[i][1];
 
             outdegree[u]++;
             indegree[v]++;
         }
-  
-        for(int i=1;i<n+1;i++){
-            if(indegree[i]==n-1&&outdegree[i]==0){
-                return i;
-            }
+
+        int ans=-1;
+        for(int i=1;i<=n;i++){
+              if(indegree[i]==n-1 && outdegree[i]==0){
+                  ans = i;
+                  break;
+              }
         }
-        return -1;
+        return ans;
 
     }
 };
