eb0c16e6acc4427132994079928680ddd2d58bce
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-02-26 14:57:44 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.8 MB (99.95%)

--- my-code/0100-same-tree/solution.cpp
+++ my-code/0100-same-tree/solution.cpp
@@ -1,28 +1,21 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-   bool solve(TreeNode *p,TreeNode *q){
-       if(p==NULL || q==NULL) return p==q;
+    bool solve(TreeNode *p, TreeNode *q){
+        if(p==NULL || q==NULL){
+            return p==q;
+        }
 
-       if(p->val!=q->val) return false;
+        if(p->val!=q->val){
+            return false;
+        }
 
-       bool left = solve(p->left,q->left);
-       bool right = solve(p->right,q->right);
-
-       return left&&right;
-   }
+        bool flag1 = solve(p->left,q->left);
+        bool flag2 = solve(p->right,q->right);
 
 
+        return flag1&&flag2;
+
+    }
     bool isSameTree(TreeNode* p, TreeNode* q) {
         return solve(p,q);
     }
