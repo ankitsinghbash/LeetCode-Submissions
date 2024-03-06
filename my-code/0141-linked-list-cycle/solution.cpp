0c7dcfcc0944c1ca6183c285eb0ef432a2401234
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-03-06 16:23:56 +0000

    Sync LeetCode submission Runtime - 4 ms (97.64%), Memory - 10.6 MB (99.97%)

--- my-code/0141-linked-list-cycle/solution.cpp
+++ my-code/0141-linked-list-cycle/solution.cpp
@@ -1,23 +1,19 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
 public:
+    bool solve(ListNode *&head){
+         ListNode *fast = head;
+         ListNode *slow = head;
+         while(fast!=NULL && fast->next!=NULL){
+               fast = fast->next->next;
+               slow = slow->next;
+               if(fast==slow){
+                   return true;
+               }
+         }
+         return false;
+    }
+
     bool hasCycle(ListNode *head) {
-        ListNode *slow=head;
-        ListNode *fast = head;
-        while(fast!=NULL && fast->next!=NULL){
-            slow=slow->next;
-            fast = fast->next->next;
-            if(slow==fast){
-                return true;
-            }
-        }
-        return false;
+        return solve(head);
     }
 };
