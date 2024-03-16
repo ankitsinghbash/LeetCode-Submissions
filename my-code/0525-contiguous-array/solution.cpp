20cdec1bc4fe6e7868d4ec10bb37bcd1a04e8b3b
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-03-16 16:29:54 +0000

    Sync LeetCode submission Runtime - 99 ms (5.17%), Memory - 87.8 MB (99.72%)

--- my-code/0525-contiguous-array/solution.cpp
+++ my-code/0525-contiguous-array/solution.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
-        unordered_map<int,int> mp;   //sum vs index:
-        mp[0] = -1;  
-        int currsum=0;
-        int maxlen=0;
+        unordered_map<int,int> mp;
+        mp[0]=-1;
+        int curr_sum=0;
+        int maxx=0;
         for(int i=0;i<nums.size();i++){
-              currsum += (nums[i]==1?1:-1);
-              if(mp.find(currsum)!=mp.end()){
-                        maxlen  = max(maxlen,i-mp[currsum]);
-              }
-              else{
-                      mp[currsum]=i;
-              }
-
+            curr_sum+=nums[i]==1?1:-1;
+            if(mp.find(curr_sum)!=mp.end()){
+                maxx = max(i-mp[curr_sum],maxx);
+            }
+            if(mp.find(curr_sum)==mp.end()){
+               mp[curr_sum]=i;
+            }
         }
-        return maxlen;
+        return maxx;
     }
-
 };
