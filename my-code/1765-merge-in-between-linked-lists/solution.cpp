f65a7f0cc168c0dbad1387929d43510598142c2e
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-03-22 09:27:19 +0000

    Sync LeetCode submission Runtime - 194 ms (74.88%), Memory - 98.1 MB (99.85%)

--- my-code/1765-merge-in-between-linked-lists/solution.cpp
+++ my-code/1765-merge-in-between-linked-lists/solution.cpp
@@ -1,22 +1,45 @@
 class Solution {
 public:
     ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
-        ListNode *currNode = list1;
-        int index = 0;
-        while(index < a-1){
-            currNode = currNode->next;
-            index++;
+        //base case:
+        
+
+
+        ListNode *realhead = list1;
+        ListNode *temp = list1;
+        
+        ListNode *temp_start = list1;
+        ListNode *last = NULL;
+        ListNode *list1_last = NULL;
+        int start=0;
+        while(temp_start!=NULL){
+               if(start==b){
+                   list1_last = temp_start->next;
+                 //  temp_start->next = NULL;
+               }
+               start++;
+               temp_start=temp_start->next;
         }
-        ListNode *front = currNode;
-        while(index < b+1){
-           currNode = currNode->next;
-           index++;
+     
+
+        
+        int len=0;
+        ListNode *temp2 = NULL;
+        while(temp!=NULL){
+            if(len==a-1){
+                ListNode *second_start = list2;
+              //  temp->next=NULL;
+                temp->next=second_start;
+                temp2 = second_start;
+                while(temp2->next!=NULL){
+                    temp2=temp2->next;
+                }
+               break;
+            }
+            temp=temp->next;
+            len++;
         }
-        ListNode *rear = currNode;
-        ListNode *secondListTail = list2, *secondListHead = list2;
-        while(secondListTail->next != NULL)secondListTail = secondListTail->next;
-        front->next = secondListHead;
-        secondListTail->next = rear;
-        return list1;
+        temp2->next = list1_last;
+        return realhead;
     }
 };
