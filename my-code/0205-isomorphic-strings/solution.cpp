2cbc202a45b855c0e7b9fab8bd6ab13ea25ee586
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-04-02 16:24:00 +0000

    Sync LeetCode submission Runtime - 4 ms (23.48%), Memory - 9.2 MB (82.23%)

--- my-code/0205-isomorphic-strings/solution.cpp
+++ my-code/0205-isomorphic-strings/solution.cpp
@@ -1,36 +1,19 @@
+
 class Solution {
 public:
-    bool isIsomorphic(string str1, string str2) {
-     int n=str1.size();
-     int m=str2.size();
-     if(n!=m) return false;
-
-    map<char,bool>two;
-    map<char,char>umap; 
-
-    for(int i=0;i<n;i++)
-    {
-     if(!umap[str1[i]])
-      {
-         if(!two[str2[i]])
-          {
-            umap[str1[i]]=str2[i];
-            two[str2[i]]=1;
-           }
-          else
-          {
-            return false;
-          }
-      }
-      else
-      {
-        if(umap[str1[i]]!=str2[i]){
-        return false;
+    bool isIsomorphic(string& s, string& t) {      
+        int n=s.size();
+        unordered_map<char, char> st, ts;
+        for(int i=0; i<n; i++){
+            char cs=s[i], ct=t[i];
+            if (st.count(cs)==0 && ts.count(ct)==0){
+                st[cs]=ct;
+                ts[ct]=cs;
+            } 
+            else{
+                if(st[cs]!=ct || ts[ct]!=cs) return 0;
+            } 
         }
-       }      
-    }
-
-    return true;
-
+        return 1;
     }
 };
