662ed07602a41563e4d871188b64951f4010612d
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-04-03 08:12:07 +0000

    Sync LeetCode submission Runtime - 1284 ms (6.01%), Memory - 11.9 MB (9.04%)

--- my-code/0079-word-search/solution.cpp
+++ my-code/0079-word-search/solution.cpp
@@ -1,39 +1,31 @@
 class Solution {
 public:
+    bool dfs(int i,int j,int idx,vector<vector<char>> &board,string word,vector<vector<bool>> &visited){
+            if(idx==word.size()) return true;
+            
+             if(i<0 || j<0 || i>=board.size() || j>=board[0].size() || visited[i][j]==true || word[idx]!=board[i][j]) return false;
 
-   bool dfs(int i,int j,vector<vector<char>> &board,string word,int counter)
-   {
-    
-     //base:
-     if(counter==word.size())
-     {
-         return true;
-     }
-    
-     if(i<0||i>=board.size()||j<0||j>=board[0].size()||board[i][j]!=word.at(counter))
-     return false;
-      
-     char temp=board[i][j];
-     board[i][j]='@'; //visiting element:
-      bool status = dfs(i+1,j,board,word,counter+1)||dfs(i,j+1,board,word,counter+1)||dfs(i-1,j,board,word,counter+1)||dfs(i,j-1,board,word,counter+1);
-      
-    board[i][j]=temp;
-    return status;
-   }
 
+            visited[i][j]=true;
+            bool f1 = dfs(i+1,j,idx+1,board,word,visited);
+            bool f2 = dfs(i-1,j,idx+1,board,word,visited);
+            bool f3 = dfs(i,j+1,idx+1,board,word,visited);
+            bool f4 = dfs(i,j-1,idx+1,board,word,visited);
+            visited[i][j]=false;
+            return f1 || f2 || f3 || f4;
+    }
 
     bool exist(vector<vector<char>>& board, string word) {
-      for(int i=0;i<board.size();i++)
-      {
-          for(int j=0;j<board[0].size();j++)
-          {
-              int counter=0;
-              if(board[i][j]==word.at(0)&&dfs(i,j,board,word,counter))
-              {
-                  return true;
-              }
-          }
-      }
-      return false;
+       
+        int idx=0;
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+                if(board[i][j]==word[idx]){
+                    vector<vector<bool>> visited(board.size(),vector<bool>(board[0].size()));
+                    if(dfs(i,j,idx,board,word,visited)==true) return true;
+                }
+            }
+        }
+        return false;
     }
 };
