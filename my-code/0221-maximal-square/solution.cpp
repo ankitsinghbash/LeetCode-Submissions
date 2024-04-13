bc2a4ec49590989dd4e1287a492e17b2566ab028
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-04-13 08:05:20 +0000

    Sync LeetCode submission Runtime - 66 ms (6.53%), Memory - 24.3 MB (100.00%)

--- my-code/0221-maximal-square/solution.cpp
+++ my-code/0221-maximal-square/solution.cpp
@@ -1,44 +1,45 @@
 class Solution {
 public:
-    
-// _________________________
-// |min(2,2,3)+1        [2] | //so formuala is min({(i+1,j),(i+1,j+1),(i,j+1)})
-// |                        |
-// | [2]                [3] |
-// |________________________|
-
-
     int maximalSquare(vector<vector<char>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        int maxx=0;
+
+
         vector<vector<int>> dp(n,vector<int>(m));
-        for(int i=0;i<n;i++){
-            dp[i][m-1]=matrix[i][m-1]-'0';
-            maxx = max(maxx,dp[i][m-1]);
-        }
+
+        int maxx=0;
         for(int j=0;j<m;j++){
             dp[n-1][j] = matrix[n-1][j]-'0';
-            maxx = max(maxx,dp[n-1][j]);
+            if(dp[n-1][j]>maxx){
+                maxx  = dp[n-1][j];
+            }
         }
+        for(int i=0;i<n;i++){
+            dp[i][m-1] = matrix[i][m-1]-'0';
+            if(dp[i][m-1]>maxx){
+                maxx = dp[i][m-1];
+            }
+        }
+
+
         
-        for(int j=m-2;j>=0;j--){
-            for(int i=n-2;i>=0;i--){
+        for(int i=n-2;i>=0;i--){
+            for(int j=m-2;j>=0;j--){
                 if(matrix[i][j]=='0'){
                     dp[i][j]=0;
                 }
-                else if(matrix[i+1][j+1]=='0' || matrix[i+1][j]=='0' || matrix[i][j+1]=='0'){
-                    dp[i][j]=matrix[i][j]-'0';
+                else if(matrix[i][j]=='1' && matrix[i+1][j]=='1' && matrix[i+1][j+1]=='1' && matrix[i][j+1]=='1'){
+                    dp[i][j] = min({dp[i+1][j],dp[i+1][j+1],dp[i][j+1]})+1;
                 }
                 else{
-                    dp[i][j] = min({dp[i+1][j+1],dp[i+1][j],dp[i][j+1]})+1;
+                    dp[i][j] = matrix[i][j]-'0';
                 }
-                maxx = max(maxx,dp[i][j]);
+                if(dp[i][j]>maxx){
+                    maxx = dp[i][j];
+                }
             }
-        } 
-        
-         return pow(maxx,2);
+        }
+         int ans = pow(maxx,2);
+         return ans;
     }
 };
-
-
