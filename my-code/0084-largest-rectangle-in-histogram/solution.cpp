1b21fdc1747c453086213e1f374c5565c6258e0f
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-04-15 11:04:20 +0000

    Sync LeetCode submission Runtime - 107 ms (5.08%), Memory - 84 MB (52.48%)

--- my-code/0084-largest-rectangle-in-histogram/solution.cpp
+++ my-code/0084-largest-rectangle-in-histogram/solution.cpp
@@ -1,50 +1,55 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
+        //Important for stack concept:
         int n = heights.size();
-        stack<int> st;
-        int rb[n];
-        int lb[n];
+        vector<int> right(n);
+        vector<int> left(n);
 
-        st.push(n-1);   //push indexing:
-        rb[n-1]=n;
+     
+        stack<int> st;  //contain index of heights
+        right[n-1] = n;
+        st.push(n-1);
         for(int i=n-2;i>=0;i--){
-            while(!st.empty()>0 && heights[i] <= heights[st.top()]){
-                st.pop();
+                
+                while(!st.empty() && heights[i]<=heights[st.top()]){
+                    st.pop();
+                }
+                if(st.empty()){
+                    right[i] = n;
+                }
+                else{
+                    right[i] = st.top();
+                }
+                st.push(i);
+        }
+
+
+
+         stack<int> st1;
+         st1.push(0);
+         left[0]=-1;
+
+         for(int i=1;i<n;i++){
+            while(!st1.empty() && heights[i]<=heights[st1.top()]){
+                      st1.pop();
             }
-            if(st.empty()){
-                rb[i]=n;
+            if(st1.empty()){
+                left[i]= -1;
             }
             else{
-                rb[i]=st.top();
+                left[i] = st1.top();
             }
-            st.push(i);
-        }
-
-         stack<int> st2;
-         st2.push(0);
-         lb[0]=-1;
-         for(int i=1;i<n;i++){
-             while(!st2.empty() && heights[i]<=heights[st2.top()] ){
-                 st2.pop();
-             }
-             if(st2.empty()){
-                 lb[i]=-1;
-             }
-             else{
-                 lb[i]=st2.top();
-             }
-             st2.push(i);
+            st1.push(i);
          }
 
-  
-        int maxarea=0;
-        for(int i=0;i<n;i++){
-            int width = rb[i]-lb[i]-1;
-            int area = width*heights[i];
-            maxarea=max(maxarea,area);
-        }
-        return maxarea;
 
+         int maxx=0;
+         for(int i=0;i<n;i++){
+            int width = right[i]-left[i]-1;
+            int area = width*heights[i];
+            if(area>maxx) maxx = area;
+         }
+         return maxx;
     }
 };
