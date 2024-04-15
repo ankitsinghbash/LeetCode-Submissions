853b420cb51c9821efacbce5fa430452a77e1d37
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-04-15 14:58:25 +0000

    Sync LeetCode submission Runtime - 15 ms (7.13%), Memory - 10.2 MB (99.99%)

--- my-code/0402-remove-k-digits/solution.cpp
+++ my-code/0402-remove-k-digits/solution.cpp
@@ -1,29 +1,49 @@
 class Solution {
- public:
-  string removeKdigits(string num, int k) {
-    if (num.length() == k)
-      return "0";
+public:
+    string removeKdigits(string num, int k) {
+           if(num.size()==k){
+            return "0";
+           }
 
-    string ans;
-    vector<char> stack;
+           stack<char> st;
+           //st.push(num[0]);
+           for(int i=0;i<num.size();i++){
+              while(!st.empty() && k>0 && num[i]<st.top()){
+                st.pop();
+                k--;
+              }
+              st.push(num[i]);
+           }
 
-    for (int i = 0; i < num.length(); ++i) {
-      while (k > 0 && !stack.empty() && stack.back() > num[i]) {
-        stack.pop_back();
-        --k;
-      }
-      stack.push_back(num[i]);
+
+           while(k>0 && !st.empty()){
+            st.pop();
+            k--;
+           }
+
+           string temp;
+           while(!st.empty()){
+               temp.push_back(st.top());
+               st.pop();
+           }
+           reverse(temp.begin(),temp.end());
+
+           int m = temp.size();
+           
+          
+           int i=0;
+           while(temp[i]=='0'){
+            i++;
+           }
+
+          string result = "";
+           for(int k=i;k<temp.size();k++){
+              result.push_back(temp[k]);
+           }
+
+            if (result.size() == 0)   //case of "10" k=1;
+                result.push_back('0');
+           return result;
     }
-
-    while (k-- > 0)
-      stack.pop_back();
-
-    for (const char c : stack) {
-      if (c == '0' && ans.empty())
-        continue;
-      ans += c;
-    }
-
-    return ans.empty() ? "0" : ans;
-  }
 };
+
