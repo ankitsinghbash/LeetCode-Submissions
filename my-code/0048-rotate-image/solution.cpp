3fe8f9f7fe32a49541746bbbe8519a0dff4d1bf9
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-04-19 10:39:06 +0000

    Sync LeetCode submission Runtime - 2 ms (1.98%), Memory - 8.8 MB (100.00%)

--- my-code/0048-rotate-image/solution.cpp
+++ my-code/0048-rotate-image/solution.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        vector<vector<int>> store;
-        for(int j=0;j<matrix[0].size();j++){
-             vector<int> sm;
-             for(int i=matrix.size()-1;i>=0;i--){
-                  sm.push_back(matrix[i][j]);
-                  cout<<matrix[i][j]<<" ";
+           int n = matrix.size();
+           int m = matrix[0].size();
+           vector<vector<int>> new_matrix(m,vector<int>(n));
+           for(int i=0;i<n;i++){
+                  for(int j=0;j<m;j++){
+                          new_matrix[j][n-i-1] = matrix[i][j];
+                  }
+           }
+
+
+           for(int i=0;i<matrix.size();i++){
+             for(int j=0;j<matrix[0].size();j++){
+                matrix[i][j] = new_matrix[i][j];
              }
-            store.push_back(sm);
-        }
-        
-        
-        for(int i=0;i<store.size();i++){
-            for(int j=0;j<store[0].size();j++){
-                matrix[i][j]=store[i][j];
-            }
-            
-        }
+           }
     }
 };
