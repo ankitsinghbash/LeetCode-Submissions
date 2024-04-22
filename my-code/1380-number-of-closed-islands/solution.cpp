54566188efa75c91e86b65fcf4df9425d4b960e0
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-04-22 02:18:56 +0000

    Sync LeetCode submission Runtime - 19 ms (5.63%), Memory - 11.9 MB (100.00%)

--- my-code/1380-number-of-closed-islands/solution.cpp
+++ my-code/1380-number-of-closed-islands/solution.cpp
@@ -1,40 +1,65 @@
 class Solution {
 public:
-  
-  bool dfs(int i,int j,vector<vector<int>> &grid){
+   vector<int> dir_x = {-1,1,0,0};    
+   vector<int> dir_y = {0,0,-1,1};
+      
+    //    i-1,j
+    //   i+1,j
      
-     if(i>=grid.size()||i<0 || j>=grid[0].size() || j<0){
-         return false;   //out of bound return false: corner wale 0 nahi taken karage:
-     }
+    //   j-1,i
+    //   j+1,i
 
-     if(grid[i][j]==1){
-          return true;   //base case:
-     }
+    bool dfs(int i,int j,vector<vector<int>> &grid){
+            if(i<0 || j< 0 || i>=grid.size() || j>=grid[0].size()) return false;
 
-     grid[i][j]=1;   //this will marked as visited:
-     int right_call = dfs(i,j+1,grid);
-     int left_call = dfs(i,j-1,grid);
-     int top_call = dfs(i-1,j,grid);
-     int down_call  = dfs(i+1,j,grid);
-   
-     return right_call && left_call && top_call && down_call;  
-  } 
+           
 
- 
+             if(grid[i][j]==1 || grid[i][j]==2) return true;
+
+            
+            
+                 grid[i][j]=2; //when i found 0;
+                bool down = dfs(i+1,j,grid);
+                bool right = dfs(i,j+1,grid);
+                bool up = dfs(i-1,j,grid);
+                bool left = dfs(i,j-1,grid);
+                return down && right && up && left;
+             
+
+    }
+
+
+
+
 
     int closedIsland(vector<vector<int>>& grid) {
-        int n=grid.size();
+        //algo:
+        /*
+            apply dfs on 0:
+            return when in four direction has 1 and 2, return true:
+            else return false;
+
+        */
+ 
+        int n = grid.size();
         int m = grid[0].size();
-        
-        int cnt = 0;
+        int cnt=0;
         for(int i=0;i<n;i++){
-         for(int j=0;j<m;j++){
-             if(grid[i][j]==0){
-                if(dfs(i,j,grid)==true){
-                 cnt++;
-                }
-             }
-         }
+            for(int j=0;j<m;j++){
+                   if(grid[i][j]==0){
+                       if(dfs(i,j,grid)==true){
+                         cnt++;
+                       }
+                   }
+            }
+        }
+
+
+         for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+            cout<<grid[i][j]<<",";
+            }
+            cout<<endl;
         }
         return cnt;
     }
