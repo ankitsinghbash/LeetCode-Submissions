c9abb399792315be3fb25d4d3b63dedaf595c90b
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-04-26 03:51:14 +0000

    Sync LeetCode submission Runtime - 232 ms (41.19%), Memory - 18.3 MB (100.00%)

--- my-code/1224-minimum-falling-path-sum-ii/solution.cpp
+++ my-code/1224-minimum-falling-path-sum-ii/solution.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
-    
-    int solve(int i,int j,vector<vector<int>> &matrix,vector<vector<int>> &dp){
-        if(i<0 || j<0 || i>=matrix.size() || j>=matrix[0].size()) return INT_MAX;
-
-
-        if(i==matrix.size()-1){
-            return matrix[i][j];
-        }
-
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-
-         int mini = INT_MAX;
-         for(int k=0;k<matrix[0].size();k++){
-                  if(j!=k){
-                      int taken = matrix[i][j]+solve(i+1,k,matrix,dp);
-                      mini = min(mini,taken);
-                  }
-         }
-        
-        return dp[i][j] = mini;
-
-    }
-
     int minFallingPathSum(vector<vector<int>>& grid) {
-        //Do same thing but with the help of recursion:
-        int mini = INT_MAX;
-        int n = grid.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        for(int j=0;j<n;j++){
-                   mini  = min(mini,solve(0,j,grid,dp));
-        }
-        return mini;
+            int n = grid.size();
+            vector<vector<int>> dp(n,vector<int>(n));
+
+
+            for(int j=0;j<n;j++){
+                dp[0][j] = grid[0][j];
+            }
+
+
+            for(int i=1;i<n;i++){
+                for(int j=0;j<n;j++){
+                       
+                       int curr=INT_MAX;
+                       for(int k=0;k<n;k++){ //this also show coloum:
+                           if(k!=j){   //why k!=j;
+                            curr = min(curr,grid[i][j]+dp[i-1][k]);
+                           }
+                       }
+
+                       dp[i][j] = curr;
+                }
+            }
+
+
+            int mini = INT_MAX;
+            for(int j=0;j<n;j++){
+                 int value = dp[n-1][j];
+                 if(value<mini){
+                    mini = value;
+                 }
+            }
+            return mini;
+
     }
 };
