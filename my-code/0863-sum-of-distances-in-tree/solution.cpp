32a5110f264476bab66cd95904cbe24cd650fe39
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-04-28 15:01:08 +0000

    Sync LeetCode submission Runtime - 196 ms (8.44%), Memory - 96.2 MB (95.35%)

--- my-code/0863-sum-of-distances-in-tree/solution.cpp
+++ my-code/0863-sum-of-distances-in-tree/solution.cpp
@@ -1,94 +1,37 @@
-
 class Solution {
 public:
-//Hard hai:lol
-    vector<vector<int>> graph;
-    vector<int> counter, ans;
-        
-    void dfs(int i, int prev) {
-        for(auto u : graph[i]) {
-            if(u!=prev){   //condition of no repeating this same element because it is graph:
-            dfs(u, i);
-            counter[i] += counter[u];
-            ans[i] += ans[u] + counter[u];
-            }
-        }
-        counter[i] += 1;
-    }
-
-    void dfs2(int i, int n, int prev) {
-        for(auto u : graph[i]) {
-            if(u != prev){
-            ans[u] = ans[i] - counter[u] + n - counter[u];
-            dfs2(u, n, i);
-            }
+    vector<vector<int>> adj;
+    vector<int> sum, cnt;
+    void build_adj(int n, vector<vector<int>>& edges){
+        adj.resize(n);
+        for(auto& e: edges){
+            int v=e[0], w=e[1];
+            adj[v].push_back(w);
+            adj[w].push_back(v);       
         }
     }
-    
+    void dfs0(int i, int parent=-1){
+        for(int j: adj[i]){
+            if (j==parent) continue;
+            dfs0(j, i);
+            cnt[i]+=cnt[j];
+            sum[i]+=sum[j]+cnt[j];//subtree for root=i
+        }
+    }
+    void reroot(int i, int parent, int n){
+        for (int j: adj[i]){
+            if (j==parent) continue;
+            sum[j]=sum[i]+n-2*cnt[j];
+            reroot(j, i, n);
+        }
+    }
     vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
-         graph.resize(n);
-         ans.resize(n);
-         counter.resize(n);
-
-        for(int i=0;i<n -1;i++) {
-            int a = edges[i][0];
-            int b = edges[i][1];
-            graph[a].push_back(b);
-            graph[b].push_back(a);
-        }
-        
-        dfs(0,-1);
-        dfs2(0, n,-1);
-        return ans;
+    //    if (n==1) return {0};
+        build_adj(n, edges);
+        sum.assign(n, 0);
+        cnt.assign(n, 1);
+        dfs0(0);
+        reroot(0, -1, n);
+        return sum;
     }
 };
-
-
-
-
-
-
-
-
-
-
-/*
-class Solution {
-public:
-    vector<vector<int>> v;
-    vector<int> counter, res;
-        
-    void dfs(int i, int p = -1) {
-        for(auto u : v[i]) {
-            if(u == p) continue;
-            dfs(u, i);
-            counter[i] += counter[u];
-            res[i] += res[u] + counter[u];
-        }
-        counter[i] += 1;
-    }
-
-    void dfs2(int i, int n, int p = -1) {
-        for(auto u : v[i]) {
-            if(u == p) continue;
-            res[u] = res[i] - counter[u] + n - counter[u];
-            dfs2(u, n, i);
-        }
-    }
-    
-    vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
-        v.resize(n);
-        for(int i = 0; i < n - 1; i++) {
-            int a = edges[i][0];
-            int b = edges[i][1];
-            v[a].push_back(b);
-            v[b].push_back(a);
-        }
-        res.resize(n);
-        counter.resize(n);
-        dfs(0);
-        dfs2(0, n);
-        return res;
-    }
-};
-*/
