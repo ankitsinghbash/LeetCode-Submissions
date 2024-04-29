a67550ed8cfbc2bea44685a8715627e19d528cf2
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-04-29 06:56:09 +0000

    Sync LeetCode submission Runtime - 112 ms (28.97%), Memory - 91.8 MB (100.00%)

--- my-code/3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/solution.cpp
+++ my-code/3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/solution.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-          int start=0;
-          int len = nums.size();
-          start = start^k;
-          for(int i=0;i<len;i++){
-                start = start^nums[i];
+          int first = nums[0];
+          for(int i=1;i<nums.size();i++){
+                  first = first^nums[i];
           }
+
+          string binary_first = bitset<32>(first).to_string();
+          string binary_second = bitset<32>(k).to_string();
          
-          int val = start;
-          int totalway=0;
-          while(start!=0){
-             int cnt = (start&1);
-             totalway = totalway+(cnt);
-              start= start>>1;
+          int cnt=0;
+          for(int i=0;i<32;i++){
+               if(binary_first[i]!=binary_second[i]){
+                cnt++;
+               }
           }
-         return totalway;
-          
+          return cnt;
     }
 };
