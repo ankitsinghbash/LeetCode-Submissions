5df6f89f6112ad84f2a9d8ecfb2323b9806abb7c
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-05-06 17:26:10 +0000

    Sync LeetCode submission Runtime - 274 ms (5.04%), Memory - 159.8 MB (99.96%)

--- my-code/2573-remove-nodes-from-linked-list/solution.cpp
+++ my-code/2573-remove-nodes-from-linked-list/solution.cpp
@@ -1,29 +1,45 @@
 class Solution {
 public:
-    ListNode* reverse(ListNode *&head, ListNode *prev) {
-        if (head == NULL) return prev;
-        
-        ListNode *nextNode = head->next;
-        head->next = prev;
-        return reverse(nextNode, head);
+
+    ListNode *reverse(ListNode *head,ListNode *prev){
+             
+            //iterative:
+
+             if(head==NULL) return prev;
+
+             ListNode *temp = head->next;
+             head->next=prev;
+             return reverse(temp,head);
+
+
     }
 
     ListNode* removeNodes(ListNode* head) {
-        ListNode *reversedHead = reverse(head, NULL);
-        ListNode *prev = reversedHead;
-        ListNode *temp = reversedHead->next;
-        
-        while (temp != NULL) {
-            if (temp->val < prev->val) {
-                temp=temp->next;
-            } else {
-               prev->next = temp;
-               prev = temp;
-               temp = temp->next;
-            }
-        }
-        prev->next = NULL;
-        return reverse(reversedHead, NULL);
+
+          ListNode *head1 = reverse(head,NULL);
+          
+          
+         
+         //traversal in head1 means in the reverse of LList:
+          ListNode *prev = head1;
+          ListNode *temp = head1->next;
+          int curr_maxx = head1->val;
+          while(temp!=NULL){
+                int curr = temp->val;
+                if(curr<curr_maxx){
+                        temp = temp->next;
+                }
+                else{
+                     prev->next = temp;
+                     prev = temp;
+                     temp = temp->next;
+                    
+                }
+                curr_maxx = max(curr_maxx,curr);
+          }
+          
+           prev->next=NULL;
+          ListNode *headans = reverse(head1,NULL);
+          return headans;
     }
 };
-
