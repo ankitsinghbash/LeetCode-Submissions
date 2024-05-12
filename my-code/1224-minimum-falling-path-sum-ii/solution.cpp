2ccadda4ad2df256674be65957e6311b11beaedb
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-05-12 22:09:24 +0000

    Sync LeetCode submission Runtime - 233 ms (41.03%), Memory - 18.3 MB (100.00%)

--- my-code/1224-minimum-falling-path-sum-ii/solution.cpp
+++ my-code/1224-minimum-falling-path-sum-ii/solution.cpp
@@ -1,38 +1,41 @@
 class Solution {
-public:
+public:    
     int minFallingPathSum(vector<vector<int>>& grid) {
-            int n = grid.size();
-            vector<vector<int>> dp(n,vector<int>(n));
+        //method1:recursion:
 
+        //method2:tablution:
 
+       
+         
+         int n = grid.size();
+         vector<vector<int>> dp(n,vector<int>(n));
+         for(int j=0;j<n;j++){
+             dp[0][j] = grid[0][j];
+         }
+
+         for(int i=1;i<n;i++){    //current one Level:
             for(int j=0;j<n;j++){
-                dp[0][j] = grid[0][j];
-            }
-
-
-            for(int i=1;i<n;i++){
-                for(int j=0;j<n;j++){
-                       
-                       int curr=INT_MAX;
-                       for(int k=0;k<n;k++){ //this also show coloum:
-                           if(k!=j){   //why k!=j;
-                            curr = min(curr,grid[i][j]+dp[i-1][k]);
-                           }
-                       }
-
-                       dp[i][j] = curr;
+                //logic part:
+                int curr = INT_MAX;
+                for(int k=0;k<n;k++){   //above col for  i: 1 level less then current one:
+                      if(j!=k){
+                           curr = min(curr,grid[i][j]+dp[i-1][k]);
+                      }
                 }
+                dp[i][j] = curr;
+   
             }
+         }
 
 
-            int mini = INT_MAX;
-            for(int j=0;j<n;j++){
-                 int value = dp[n-1][j];
-                 if(value<mini){
-                    mini = value;
-                 }
-            }
-            return mini;
+         int mini=INT_MAX;
+         for(int j=0;j<n;j++){
+             int value = dp[n-1][j];
+             mini = min(mini,value);
+         }
+
+         return mini;
+
 
     }
 };
