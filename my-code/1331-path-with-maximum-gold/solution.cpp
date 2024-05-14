0b88d6e1fd64a06f7ed9b2bbc433d1cbbc4eeb3c
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-05-14 01:32:05 +0000

    Sync LeetCode submission Runtime - 378 ms (5.06%), Memory - 11.4 MB (12.36%)

--- my-code/1331-path-with-maximum-gold/solution.cpp
+++ my-code/1331-path-with-maximum-gold/solution.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
+    int dfs(int i,int j,vector<vector<bool>> &visited,vector<vector<int>> &grid){
+            if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size() || visited[i][j]==true || grid[i][j]==0){
+                return 0;
+            }
 
-    int recursion(int i,int j,vector<vector<int>> &grid)
-    {
-        if(i<0||j<0||i==grid.size()||j==grid[0].size()||grid[i][j]==0)
-            return 0;
+            
+            visited[i][j]=true;
+            int cnt=0;
+            cnt+=grid[i][j]+max({dfs(i+1,j,visited,grid),dfs(i-1,j,visited,grid),dfs(i,j+1,visited,grid),dfs(i,j-1,visited,grid)});
+            visited[i][j]=false;
 
-        int temp=grid[i][j];
-        grid[i][j]=0;  //ki visited mat karo aab:
-        int ans=temp;   //take a varible to add the all answer:
-        int ans1=0;
-        ans1=max(ans1,recursion(i+1,j,grid));
-        ans1=max(ans1,recursion(i,j+1,grid));
-        ans1=max(ans1,recursion(i-1,j,grid));
-        ans1=max(ans1,recursion(i,j-1,grid));
-        ans+=ans1;
-        grid[i][j]=temp;  //make grid again visited:
-        return ans;
+            return cnt;
+
     }
-
     int getMaximumGold(vector<vector<int>>& grid) {
-        int ans=0;
-        for(int i=0;i<grid.size();i++)
-        {
-            for(int j=0;j<grid[0].size();j++)
-            {
-                ans = max(ans,recursion(i,j,grid));
+        //every time apply dfs:
+        int n = grid.size();
+        int m = grid[0].size();
+        int maxx = 0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                  if(grid[i][j]!=0){
+                      vector<vector<bool>> visited(n,vector<bool>(m));
+                       int value = dfs(i,j,visited,grid);
+                       maxx = max(maxx,value);
+                  }
             }
         }
-        return ans;
+        return maxx;
     }
 };
