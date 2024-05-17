835d647d897e3b59b390727e162087d934f30e1b
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-05-17 19:22:08 +0000

    Sync LeetCode submission Runtime - 17 ms (2.98%), Memory - 21.3 MB (100.00%)

--- my-code/1450-delete-leaves-with-a-given-value/solution.cpp
+++ my-code/1450-delete-leaves-with-a-given-value/solution.cpp
@@ -1,24 +1,27 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-   TreeNode *Inorder(TreeNode *root,int target){
-       if(root==NULL){
-           return NULL;
-       }
+    TreeNode *solve(TreeNode *root,int target){
+          if(root==NULL) return NULL;
+          root->left = solve(root->left,target);
+          root->right = solve(root->right,target);
+           if(root->left==NULL && root->right==NULL && target==root->val){
+            return NULL;
+          }
+          return root;
 
-       root->left = Inorder(root->left,target);
-       root->right = Inorder(root->right,target);
-       if(root->left==NULL && root->right==NULL && root->val==target){
-           root=nullptr;
-       }
-
-       return root;
-
-
-   }
-
-
+    }
     TreeNode* removeLeafNodes(TreeNode* root, int target) {
-        return Inorder(root,target);
+            return solve(root,target);
     }
 };
