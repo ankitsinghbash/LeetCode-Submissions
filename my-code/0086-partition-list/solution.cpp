33ce56c878b3f75d53352588b66b6995f7d39859
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-05-18 10:15:41 +0000

    Sync LeetCode submission Runtime - 3 ms (3.54%), Memory - 13.5 MB (100.00%)

--- my-code/0086-partition-list/solution.cpp
+++ my-code/0086-partition-list/solution.cpp
@@ -1,78 +1,71 @@
 class Solution {
 public:
-void firsthead(ListNode *&head1,int data){
-      
-      if(head1==NULL){
-          ListNode *new_node = new ListNode(data);
-          head1 = new_node;
-          return;
-      }
+   void firstLL(ListNode *&head1,int x){
+       
+             ListNode *new_node = new ListNode(x);
+             if(head1==NULL){
+                  head1 = new_node;
+             }
+             else{
+                  ListNode *temp = head1;
+                  while(temp->next!=NULL){
+                     temp = temp->next;
+                  }
+                  temp->next = new_node;
+             }
 
-      ListNode *temp = head1;
-      while(temp->next!=NULL){
-          temp = temp->next;
-      }
-      ListNode *new_node = new ListNode(data);
-      temp->next = new_node;
-}
-
-
-void secondhead(ListNode *&head2,int data){
-      
-      if(head2==NULL){
-          ListNode *new_node = new ListNode(data);
-          head2 = new_node;
-          return;
-      }
-
-      ListNode *temp = head2;
-      while(temp->next!=NULL){
-          temp = temp->next;
-      }
-      ListNode *new_node = new ListNode(data);
-      temp->next = new_node;
-}
-
-ListNode *solve(ListNode *&head1,ListNode *&head2){
-    if(head1==NULL && head2==NULL){
-        return NULL;
-    }
-
-    if(head1==NULL){
-        return head2;
-    }
-    if(head2==NULL){
-        return head1;
-    }
-
-    ListNode *temp = head1;
-    while(temp->next!=NULL){
-         temp=temp->next;
-    }
-    temp->next=head2;
-    return head1;
-}
+   }
 
 
-    ListNode* partition(ListNode* head, int x) {
-        ListNode *head1=NULL;
-        ListNode *head2=NULL;
-  
-        ListNode *temp = head;
-        while(temp!=NULL){
-            if(temp->val<x){
-                firsthead(head1,temp->val);
-            }
-            else if(temp->val>=x){
-                secondhead(head2,temp->val);
-            }
-            temp=temp->next;
+   void secondLL(ListNode *&head2,int x){
+       
+             ListNode *new_node = new ListNode(x);
+             if(head2==NULL){
+                  head2 = new_node;
+             }
+             else{
+                  ListNode *temp = head2;
+                  while(temp->next!=NULL){
+                     temp = temp->next;
+                  }
+                  temp->next = new_node;
+             }
+
+   }
+   ListNode *merge_LL(ListNode *head1,ListNode *head2){
+     
+        if(head1==NULL){
+            return head2;
         }
-      
 
-       //add both first and second head:
-       ListNode *answer = solve(head1,head2);
-       return answer;
+
+         ListNode *temp = head1;
+         while(temp->next!=NULL){
+            temp=temp->next;
+         }
+
+        temp->next=head2;
+       return head1;
+      
+   }
+  
+    ListNode* partition(ListNode* head, int x) {
+           
+           ListNode *temp = head;
+           ListNode *head1 = NULL;
+           ListNode *head2 = NULL;
+           while(temp!=NULL){
+                if(temp->val<x){
+                      firstLL(head1,temp->val);
+                }
+                else{
+                       secondLL(head2,temp->val);
+                }
+                temp = temp->next;
+           }
+
+           ListNode *myans = merge_LL(head1,head2);
+           return myans;
 
     }
 };
