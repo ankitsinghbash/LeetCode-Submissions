de2490b870100ddf1952c7625fc7c7c7326761ac
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-05-21 20:15:19 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.4 MB (100.00%)

--- my-code/0078-subsets/solution.cpp
+++ my-code/0078-subsets/solution.cpp
@@ -1,50 +1,27 @@
 class Solution {
 public:
-//imp i ko increament karne ki koi jarurat nahi beacuase recursion me khud hi hota hai:
-   vector<vector<int>> ans;
+     vector<vector<int>> store;
+    void solve(int i,vector<int> &nums,vector<int> &vec){
+             if(i==nums.size()){
+                store.push_back(vec);
+                return;
+             }
+        
+              vec.push_back(nums[i]);
+              solve(i+1,nums,vec);
+              vec.pop_back();
+           
+              solve(i+1,nums,vec);
 
-   void helper(int i,vector<int> &subset,vector<int> nums)
-   {
-       if(nums.size()==i)
-       {
-           ans.push_back(subset);
-           return;
-       }
-     
-         //helper(i+1,subset,nums);  //not include next:first time it got empty:
-         subset.push_back(nums[i]);
-         helper(i+1,subset,nums);    //include next:
-         subset.pop_back();
 
-         helper(i+1,subset,nums);
+    }
 
-   }
+
+
 
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int> empty;
-        helper(0,empty,nums);
-        return ans;
+        vector<int> vec;
+         solve(0,nums,vec);
+         return store;
     }
 };
-
-
-//second method:
-/*
-class Solution {
-public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> hg;
-        vector<int> gh;
-        backtrack(nums,hg,gh,0);  
-        return hg;
-    }
-    void backtrack(vector<int>& nums,vector<vector<int>>& hg,vector<int>& gh,int start){
-	    hg.push_back(gh);
-        for(int i=start;i<nums.size();i++){
-            gh.push_back(nums[i]);
-            backtrack(nums,hg,gh,i+1);
-            gh.pop_back();
-        }
-    }
-};
-*/
