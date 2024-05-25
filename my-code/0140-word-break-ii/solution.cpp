e08fb84e57f7cbcf3d0a12aea1c9e5b3eaa4ebc2
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-05-25 16:52:24 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.1 MB (100.00%)

--- my-code/0140-word-break-ii/solution.cpp
+++ my-code/0140-word-break-ii/solution.cpp
@@ -1,27 +1,35 @@
 class Solution {
 public:
-     vector <string> v; // global vector
-    void compute (int i,string s, vector <string> wd,string path){
-         
-           if (i>= s.length()){ // base case
-               
-               path.pop_back(); // backtracking 
-               v.push_back(path);
-           }
-            string temp;
-           for (int j=i; j< s.length(); j++){
-                 temp+= s[j]; // check every character 
-               if (find(wd.begin(), wd.end(), temp)!= wd.end()){ // using find fn. to check word present or not in dict
-                   
-                  compute(j+1,s, wd,path+temp+' '); //important to add ' ' because in pop condition we pop last element:
-                  
-              }
-           }
-      }
-    vector<string> wordBreak(string s, vector<string>& w){
-       string path;
-       int i=0;
-       compute (i,s,w,path);
-          return v;
+    vector<string> store;
+    void solve(int i,string s,unordered_set<string> &st,string ans){
+       
+         if(i>=s.size()){
+            ans.pop_back();
+            store.push_back(ans);
+            return;
+         }
+
+         for(int idx=i;idx<s.size();idx++){
+            string get_str = s.substr(i,idx-i+1);
+            if(st.find(get_str)!=st.end()){
+                  solve(idx+1,s,st,ans + get_str + ' '); //similar to Backtrack not execpt bracktrack://becuase recursion go to our latest last call:
+            }
+         }
+
+
+
+    }
+
+
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        string ans;
+        unordered_set<string> st;
+        for(int i=0;i<wordDict.size();i++){
+            st.insert(wordDict[i]);
+        }
+        solve(0,s,st,ans);
+        return store;
+
+
     }
 };
