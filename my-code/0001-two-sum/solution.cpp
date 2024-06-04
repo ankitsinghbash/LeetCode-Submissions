d1857df6b504c739c5fabadd03bd51ea0b380ea4
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-06-04 21:53:15 +0000

    Sync LeetCode submission Runtime - 89 ms (7.25%), Memory - 12.5 MB (99.99%)

--- my-code/0001-two-sum/solution.cpp
+++ my-code/0001-two-sum/solution.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> mp;
-        int n  = nums.size();
+        //O(N*log(N)):
+        //10^4*10^2=10^6; that is less:
+
+         int n = nums.size();
+         
+         //[4]
+        //now i try to discuss the brute force approach:
+        vector<int> ans;
         for(int i=0;i<n;i++){
-            mp[nums[i]] = i;
-        }
-        
-        for(int i=0;i<n;i++){
-            int val = target-nums[i];
-            auto it = mp.find(val);
-            if(it!=mp.end() && it->second!=i){
-                return {i,it->second};
+            int rest_value  = target-nums[i];
+            for(int j=i+1;j<n;j++){
+                if(nums[j]==rest_value){
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    break;
+                }
             }
+
         }
-        return {};
+        return ans;
     }
 };
