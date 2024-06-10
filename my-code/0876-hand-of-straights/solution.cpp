8eb0922b196cbb7368f7e9ea90a22c127e69faf6
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-06-10 23:56:26 +0000

    Sync LeetCode submission Runtime - 55 ms (8.52%), Memory - 30.7 MB (91.49%)

--- my-code/0876-hand-of-straights/solution.cpp
+++ my-code/0876-hand-of-straights/solution.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
+        //
+        if(hand.size()%groupSize!=0){
+            return false;
+        }
         map<int,int> mp;
-		for(int i=0;i<hand.size();i++)
-		{
-			mp[hand[i]]++;
-		}
+        for(int i=0;i<hand.size();i++){
+            mp[hand[i]]++;
+        }
+        //now iterate in map:
+        
 
-		for(auto it=mp.begin();it!=mp.end();)
-		{
-			if(it->second>0)
-			{
-				for(int i=0;i<groupSize;i++)
-				{
-					if(mp[it->first+i]>0)
-					{
-						mp[it->first+i]--;
-					}
-					else
-						return false;
-				}
-			}
-			else
-				it++;
-		}
-		return true;
+        auto it = mp.begin();
+         while(it!=mp.end()){
+               if(it->second>0){
+                    for(int i=0;i<groupSize;i++){
+                        if(mp[it->first+i]>0){
+                              mp[it->first+i]--;
+                        }
+                        else{
+                            return false;
+                        }
+                    }
+               }
+               else{
+                it++;
+               }
+         }  
+         return true;
+
+
     }
 };
