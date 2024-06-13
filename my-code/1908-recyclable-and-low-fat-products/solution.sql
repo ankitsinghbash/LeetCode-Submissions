769cbdea9337b8983ee2d246ba8a5d6ebc85941f
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-06-13 13:59:48 +0000

    Sync LeetCode submission Runtime - 686 ms (11.87%), Memory - 0.0B (100.00%)

--- my-code/1908-recyclable-and-low-fat-products/solution.sql
+++ my-code/1908-recyclable-and-low-fat-products/solution.sql
@@ -1,2 +1,8 @@
 # Write your MySQL query statement below
-select product_id from products where low_fats = 'Y' and recyclable = 'Y';
+SELECT 
+    product_id
+from
+    Products
+WHERE
+    low_fats='Y' AND recyclable='Y'
+
