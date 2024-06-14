e5f58d51e32834c6a9f423be49541ccabb794d99
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-06-14 11:34:12 +0000

    Sync LeetCode submission Runtime - 973 ms (5.11%), Memory - 10.7 MB (100.00%)

--- my-code/0494-target-sum/solution.cpp
+++ my-code/0494-target-sum/solution.cpp
@@ -1,46 +1,26 @@
 class Solution {
 public:
-    
-int subsetsumcount(vector<int> a, int n, int sum)
-{
-    // Initializing the matrix
-    int dp[n + 1][sum + 1];
-  // Initializing the first value of matrix
-    dp[0][0] = 1;
-    for (int i = 1; i <= sum; i++)
-        dp[0][i] = 0;
-     
-   
-    for (int i = 1; i <n+1; i++)
-    {
-        for (int j = 0; j <sum+1; j++)
-        {
-          // if the value is greater than the sum
-            if (j>=a[i-1]){
-                dp[i][j] = dp[i - 1][j] + dp[i - 1][j - a[i - 1]];
-            }
-            else
-            {
-                dp[i][j] = dp[i - 1][j];
-            }
-        }
+    int solve(int i,int current,int target,vector<int> &nums){
+          if(i>=nums.size()){
+             if(current==target){
+                return 1;
+             }
+             return 0;
+          }
+
+
+
+          int plus = solve(i+1,current+nums[i],target,nums);
+          int minum = solve(i+1,current-nums[i],target,nums);
+
+
+
+
+          return plus+minum;
+
+
     }
-    return dp[n][sum];
-}
-
-
     int findTargetSumWays(vector<int>& nums, int target) {
-         int sum=0;
-        for(int i=0;i<nums.size();i++)
-        {
-         sum+=nums[i];
-        }
-        //base case: //problem same to count the subset that give the given target but check base case:
-        if (abs(target) > sum || (sum + target) % 2 != 0){ 
-            return 0;
-        }
-        int n=nums.size();
-	    int reqSum=(target+sum)/2;
-	    return subsetsumcount(nums,n,reqSum);
+        return solve(0,0,target,nums);
     }
 };
