843a77407f82d260c541130261444b7cda850fc4
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-06-15 14:32:00 +0000

    Sync LeetCode submission Runtime - 139 ms (12.18%), Memory - 85.5 MB (100.00%)

--- my-code/0502-ipo/solution.cpp
+++ my-code/0502-ipo/solution.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
-        int n = profits.size();
-        vector<pair<int, int>> projects;
-        for (int i = 0; i < n; i++) {
-            projects.emplace_back(capital[i], profits[i]);
-        }
-        sort(projects.begin(), projects.end());
-        priority_queue<int> priority;
-        int j = 0;
-        for (int i = 0; i < k; i++) {
-            while (j < n && projects[j].first <= w) {
-                priority.push(projects[j++].second);
-            }
-            if (priority.empty()) {
+         vector<pair<int,int>> projects;
+         int n = profits.size();
+         for(int i=0;i<n;i++){
+             projects.push_back({capital[i],profits[i]});
+         }
+         sort(projects.begin(),projects.end());
+         priority_queue<int> pq;
+         int j=0;
+         while(k--){
+             while(j<n && projects[j].first<=w){
+                pq.push(projects[j].second);
+                j++;
+             }          
+
+             if(pq.empty()){
                 break;
-            }
-            w += priority.top();
-            priority.pop();
-        }
-        return w;
+             } 
+
+             w+=pq.top();
+             pq.pop();
+         }
+         return w;
     }
 };
