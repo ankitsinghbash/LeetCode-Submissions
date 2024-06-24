b1bdc5779bb289aba41728a26e2d4a2f35efcf9a
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-06-24 13:49:53 +0000

    Sync LeetCode submission Runtime - 1240 ms (5.06%), Memory - 266.3 MB (5.18%)

--- my-code/2438-find-closest-node-to-given-two-nodes/solution.cpp
+++ my-code/2438-find-closest-node-to-given-two-nodes/solution.cpp
@@ -1,47 +1,56 @@
 class Solution {
 public:
-     
-    void dfs(vector<int> &edges,int node,vector<bool> &visited,vector<int> &dist){
-        visited[node]=true;
-        int v = edges[node];
+    void dfs(unordered_map<int,vector<int>> &mp,int current,vector<int> &dist,vector<bool> &visited){
+           if(visited[current]==true) return;
 
-        if(v!=-1 && !visited[v]){
-            visited[v]=true;
-            dist[v]=1+dist[node];
-            dfs(edges,v,visited,dist);
-        }
+
+           visited[current]=true;
+           for(auto &u : mp[current]){
+               if(u!=-1 && visited[u]==false){
+                     dist[u] = dist[current]+1;
+                     dfs(mp,u,dist,visited);
+               }
+           }
 
     }
 
     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
-        //node 1 means 0
-        //node 2 means 1;
-
+        unordered_map<int,vector<int>> mp;
         int n = edges.size();
-        vector<int> dist1(n,INT_MAX); //INT_MAX because we required min value other wise put 0;
-        vector<int> dist2(n,INT_MAX);
+        for(int i=0;i<n;i++){
+            int v = edges[i];
+            mp[i].push_back(v);
+        }
 
+
+        vector<int> dist1(n,-1);
+        vector<int> dist2(n,-1);
         vector<bool> visited1(n,false);
         vector<bool> visited2(n,false);
-
         dist1[node1]=0;
         dist2[node2]=0;
-
-        dfs(edges,node1,visited1,dist1);
-        dfs(edges,node2,visited2,dist2);
-   
        
+        dfs(mp,node1,dist1,visited1);
+        dfs(mp,node2,dist2,visited2);
 
-        //now take the min value of each i of vector dist1,dist2 and find value in edges and return it's index;
-        int index = -1;
-        int mini = INT_MAX;
+
+      
+        
+        int mini  = INT_MAX;
+        int ans=-1;
         for(int i=0;i<n;i++){
-           int maxD = max(dist1[i],dist2[i]);
-           if(mini>maxD){
-               mini = maxD;
-               index = i;
-           }
+            if(dist1[i]==-1 || dist2[i]==-1){
+                continue;
+            }
+            int value = max(dist1[i],dist2[i]);
+            if(value<mini){
+                mini = value;
+                ans = i;
+            }
+
         }
-        return index;
+        return ans;
+
+         
     }
 };
