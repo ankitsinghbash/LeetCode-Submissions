05c280ee9ee47225dcb67bcfa0ac66628de1df7d
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-06-25 08:09:41 +0000

    Sync LeetCode submission Runtime - 734 ms (5.10%), Memory - 138.5 MB (43.52%)

--- my-code/2439-longest-cycle-in-a-graph/solution.cpp
+++ my-code/2439-longest-cycle-in-a-graph/solution.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
-   int result = -1;
-   void dfs(int u,vector<int> &edges,vector<bool> &visited,vector<bool> &inRecursion,vector<int> &cnt){
-   
-      visited[u]=true;
-      inRecursion[u]=true;
-      int v = edges[u];
-      if(v!=-1 && visited[v]==false){   //when no cycle:
-          cnt[v]=cnt[u]+1;
-          dfs(v,edges,visited,inRecursion,cnt);
-      }
-      else if(v!=-1 && inRecursion[v]==true){
-          result = max(result,cnt[u]-cnt[v]+1);
-      }
+    int ans = -1;
+    void dfs(vector<int> &edges,int u,vector<bool> &visited,vector<bool> &inRecurssion,vector<int> &store){
+          visited[u]=true;
+          inRecurssion[u]=true;
+          int v = edges[u];
 
-      inRecursion[u]=false;
-   }
-
-
-
-    
-    int longestCycle(vector<int>& edges){
-      
-      int V = edges.size();
-      vector<bool> visited(V,false);
-      vector<bool> inRecursion(V,false);
-      vector<int> cnt(V,1);
-
-      for(int i=0;i<edges.size();i++){
-          if(visited[i]==false){
-              dfs(i,edges,visited,inRecursion,cnt);
+          if(v!=-1 && visited[v]==false){
+              store[v]=store[u]+1;
+              dfs(edges,v,visited,inRecurssion,store);
           }
-      }
+          else if(v!=-1 && visited[v]==true && inRecurssion[v]==true){
+               ans=max(ans,store[u]-store[v]+1);
+          }
+          inRecurssion[u] = false;
 
-        return result;
+    }
+
+    int longestCycle(vector<int>& edges) {
+        int V = edges.size();
+        vector<bool> visited(V,false);
+        vector<bool> inRecurssion(V,false);
+        vector<int> store(V,1);
+        for(int i=0;i<V;i++){
+            if(visited[i]==false){
+                dfs(edges,i,visited,inRecurssion,store);
+            }
+        }
+        return ans;
+
     }
 };
