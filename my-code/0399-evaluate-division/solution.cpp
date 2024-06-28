01595ea58d750aec1932fb2b82f99e13ccdb6c24
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-06-28 09:49:10 +0000

    Sync LeetCode submission Runtime - 2 ms (22.08%), Memory - 10.9 MB (100.00%)

--- my-code/0399-evaluate-division/solution.cpp
+++ my-code/0399-evaluate-division/solution.cpp
@@ -1,63 +1,58 @@
 class Solution {
 public:
-  
-  void dfs(unordered_map<string,vector<pair<string,double>>> &adj,string N,string D,double &cost,unordered_set<string> &st,double product){
-       if(st.find(N)!=st.end()) return;    
+    double dfs(string start,string final,unordered_map<string,vector<pair<string,double>>> &mp,unordered_set<string> &st){
+            
+
+             if(start==final) return  1.0;  
+
+
+            
  
+             st.insert(start);   //true:
+             for(auto &x : mp[start]){
+                  string v = x.first;
+                  double cost = x.second;
 
-        st.insert(N);
-        if(N==D){
-            cost = product;
-            return;
-        }
- 
-   
-        for(auto &v : adj[N]){
-                if(st.find(N)!=st.end()){
-                string f = v.first;
-                double s = v.second;
-                dfs(adj,f,D,cost,st,product*s);
-                }
-        }
+                  if(st.find(v)==st.end()){  //means unvisited:
+                       double result = dfs(v,final,mp,st);
+                       if(result!=-1.0){
+                           return result*cost;
+                       } 
+                  }
 
-  }
-
-
-
+             }
+             return -1.0;
+    }
 
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
-        unordered_map<string,vector<pair<string,double>>> adj;
+        unordered_map<string,vector<pair<string,double>>> mp;
         for(int i=0;i<equations.size();i++){
-            string u = equations[i][0];
-            string v = equations[i][1];
+             double current_cost = values[i];
+             string u = equations[i][0];
+             string v = equations[i][1];
 
-            double w = values[i];
-            adj[u].push_back({v,w});
-            adj[v].push_back({u,1/w*1.0});
-        }
+             mp[u].push_back({v,current_cost});
+             double alt_current_cost = 1.0/current_cost;
+             mp[v].push_back({u,alt_current_cost});
+        } 
 
-        vector<double> ans;
-        for(int i=0;i<queries.size();i++){
-            string N = queries[i][0];
-            string D = queries[i][1];
-   
-            auto it1=adj.find(N);
-            auto it2=adj.find(D);
-            double val = -1.0;
-            double product =  1.0;
-            if(it1!=adj.end()){     //This part has various way to write but write in an efficient way:
-                 unordered_set<string> st;     //for below input a-->c kabhi nahi paucha add -1.0 in answer:
-                 dfs(adj,N,D,val,st,product);
-            }
-            ans.push_back(val);
-        }
-        return ans;
+
+        
+         vector<double> ans;
+         for(int i=0;i<queries.size();i++){
+             string start = queries[i][0];
+             string end = queries[i][1];
+             unordered_set<string> st;  //for checking the visited or not:
+             if(mp.find(start)==mp.end() || mp.find(end)==mp.end()){
+              ans.push_back(-1.0);
+             }
+             else{
+               double get_cost = dfs(start,end,mp,st);
+               ans.push_back(get_cost);
+             }
+         }
+         return ans;
+
 
     }
 };
-
-/*Dry run for this input:
-[["a","b"],["c","d"]]
-[1.0,1.0]
-[["a","c"],["b","d"],["b","a"],["d","c"]]
-*/
