a5dd2337ac233dca4e22fc4e9c41f35973671ff5
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-06-30 20:09:47 +0000

    Sync LeetCode submission Runtime - 398 ms (5.71%), Memory - 142.6 MB (98.94%)

--- my-code/1701-remove-max-number-of-edges-to-keep-graph-fully-traversable/solution.cpp
+++ my-code/1701-remove-max-number-of-edges-to-keep-graph-fully-traversable/solution.cpp
@@ -1,29 +1,30 @@
 class DSU{
-public:
-    vector<int> parent;
-    vector<int> rank;
-    int components;
-
-    DSU(int n){
-        parent.resize(n+1);
-        for(int i = 1; i<=n; i++){  //Remember here parent 1 se start ho raha hai n taak hoga that why we use (n+1) as a parent and rank size:
-            parent[i] = i;
-        }
-        rank.resize(n+1);
-        components=n;
-    }
-
-
-     //STANDARD APPLICATIN OF DISJOINT SET FIND;
-    int find (int x) {
-        if (x == parent[x]) 
-            return x;
-        
-        return parent[x] = find(parent[x]);
-    }
+       vector<int> parent;
+       vector<int> rank;
+       int component;
     
-    //STANDARD APPLICATION OF DISJOINT SET UNION;
-    void Union (int x, int y) {
+        public:
+        DSU(int  n){
+           component = n;
+           parent.resize(n+1);
+           rank.resize(n+1);
+           for(int i=1;i<=n;i++){
+             parent[i] = i;
+          }
+       }
+
+
+       int find(int x){
+            if(parent[x]==x){
+                return x;
+            }
+
+            return parent[x] = find(parent[x]);
+       }
+    
+
+    
+       void Union (int x, int y) {
         int x_parent = find(x);
         int y_parent = find(y);
         
@@ -37,74 +38,83 @@ public:
         } else {
             parent[x_parent] = y_parent;
         }
-        components--;
+        component--;
     }
   
-    bool isSingleComponents(){
-        return components==1;
+      bool iscomponent(){
+        return component==1;
+    }
+    
+
+};
+
+
+struct cmp{
+    bool operator()(vector<int> &a ,vector<int> &b){
+        return a[0]>b[0];
     }
 };
 
 class Solution {
 public:
-    static bool cmp(vector<int> &v1,vector<int> &v2){
-        return v1[0]>v2[0];
-    }    
     int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {
-        DSU Alice(n);
-        DSU Bob(n);
-       
-        // auto cmp = [](vector<int> &vec1,vector<int> &vec2){ //lamda sorting:
-        //     return vec1[0]>vec2[0];
-        // };
+         int total_edge= 0;
 
-        sort(edges.begin(),edges.end(),cmp);  //so that type 3 wale phale fix ho jaay:
+         DSU Alice(n);
+         DSU Bob(n);
 
-        int edgeCount = 0;
-        for(auto &vec : edges){
-            int type = vec[0];
-            int u = vec[1];
-            int v = vec[2];
+         sort(edges.begin(),edges.end(),cmp());  ///sort in that way so we get type 3 first: and apply dfs on type 3:
+         for(int i=0;i<edges.size();i++){
+             int type = edges[i][0];
+             int  u = edges[i][1];
+             int v = edges[i][2];
 
-            if(type==3){
-                //Alice
-                bool addedge_or_not = false;
+              
+             if(type == 3){
+                 bool isadded = false;
+                 if(Alice.find(v)!=Alice.find(u)){
+                      Alice.Union(u,v);
+                      isadded = true;
+                 }
 
-                if(Alice.find(u)!=Alice.find(v)){
-                    Alice.Union(u,v);
-                    addedge_or_not = true;
-                }
-  
-                if(Bob.find(u)!=Bob.find(v)){
+                 if(Bob.find(v)!=Bob.find(u)){
                     Bob.Union(u,v);
-                    addedge_or_not = true;
-                }
+                    isadded=true;
+                 }
 
-                if(addedge_or_not==true){
-                    edgeCount++;
-                }
+                 if(isadded==true){
+                     total_edge++;
+                 }
+             } 
 
-            }
-            else if(type==2){
-                     //BOb;
-                    if(Bob.find(u)!=Bob.find(v)){
-                    Bob.Union(u,v);
-                    edgeCount++;
+             if(type == 1){  //alice:
+                  bool isadded = false;
+                  if(Alice.find(u)!=Alice.find(v)){
+                       Alice.Union(u,v);
+                       isadded = true;
                   }
-            }
-            else{
-                //Alice:
-                if(Alice.find(u)!=Alice.find(v)){
-                    Alice.Union(u,v);
-                    edgeCount++;
-                }
-            }
-        }
-     
-      if(Alice.isSingleComponents() == true && Bob.isSingleComponents()==true){
-          return edges.size() - edgeCount;
-      }
-      return -1;
-    
+                  if(isadded==true){
+                      total_edge++;
+                  }
+             }
+
+             if(type==2){
+                 bool isadded = false;
+                 if(Bob.find(u)!=Bob.find(v)){
+                    Bob.Union(u,v);
+                    isadded = true;
+                 }
+                 if(isadded == true){
+                     total_edge++;
+                 }
+             }
+         }
+
+
+         if(Alice.iscomponent()==true && Bob.iscomponent()==true){
+              return edges.size()-total_edge;
+         }
+         return -1;
+         
     }
 };
