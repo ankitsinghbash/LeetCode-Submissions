bb18c2c3c1d29d813baf14b92c219a3258c4a767
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-07-01 09:09:03 +0000

    Sync LeetCode submission Runtime - 6 ms (0.31%), Memory - 10.5 MB (100.00%)

--- my-code/1293-three-consecutive-odds/solution.cpp
+++ my-code/1293-three-consecutive-odds/solution.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
-    bool threeConsecutiveOdds(vector<int>& arr) {
-        
-        if(arr.size()<3) return false;
-
-        int left=0;
-        int right=2;  //window size:
-        int codd=0;
-        for(int i=left;i<=right;i++)
-        {
-            if(arr[i]%2!=0) codd++;
+     bool solve(vector<int> &arr){
+        if(arr.size()<3){
+            return false;
         }
+     
       
-       if(codd==3)return true;
-
-        while(right!=arr.size()-1)
-        {
-           
-            right++;
-            if(arr[right]%2!=0) codd++;
-            left++;
-            if(arr[left-1]%2!=0) codd--;
-             if(codd==3) return true;
+        for(int i=0;i<=arr.size()-3;i++){
+              if(arr[i]%2!=0 && arr[i+1]%2!=0 && arr[i+2]%2!=0){
+                return true;
+              }
         }
         return false;
-        
 
+    }
+
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        return solve(arr);
     }
 };
