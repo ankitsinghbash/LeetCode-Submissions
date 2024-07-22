3810df03858430f09d58d735b4e6f08c4ee6e9a2
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-07-22 12:57:24 +0000

    Sync LeetCode submission Runtime - 161 ms (9.46%), Memory - 137.4 MB (95.71%)

--- my-code/0239-sliding-window-maximum/solution.cpp
+++ my-code/0239-sliding-window-maximum/solution.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-          int n = nums.size();
-           vector<int> ans;
-           deque<int> dq;   //deque adjust like front give maximum and back give minimum:
-           for(int i=0;i<n;i++){
-                    while(!dq.empty() && dq.front()<i-k+1){
-                        dq.pop_front();
-                    }
+        vector<int> ans;
+        deque<int> dq;
+        for(int i=0;i<nums.size();i++){
+                   
+               while(!dq.empty() && dq.front()<=i-k){
+                     dq.pop_front();
+               }
 
-                    while(!dq.empty() && nums[dq.back()]<=nums[i]){
-                        dq.pop_back();
-                    }
-                    dq.push_back(i);   //0
-                    if(i>=k-1){
-                         ans.push_back(nums[dq.front()]);
-                    }
-           }
-           return ans;
-
+            while(!dq.empty() && nums[dq.back()]<=nums[i]){
+                dq.pop_back();
+            }
+           dq.push_back(i);
+            if(i>=k-1){
+                ans.push_back(nums[dq.front()]);
+            }         
+        }
+        return ans;
     }
 };
-
-
-
