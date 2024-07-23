fdd47f82a3bbf6f64d971cd81046c72f4b65580c
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-07-23 07:35:31 +0000

    Sync LeetCode submission Runtime - 5 ms (19.28%), Memory - 14.9 MB (84.20%)

--- my-code/1741-sort-array-by-increasing-frequency/solution.cpp
+++ my-code/1741-sort-array-by-increasing-frequency/solution.cpp
@@ -1,45 +1,39 @@
 class Solution {
 public:
-    vector<int> ans;
-    static bool cmp(pair<int,int> a,pair<int,int> b){
+   struct cmp{
+    bool operator()(const pair<int,int> &a,const pair<int,int> &b){
 
-        if(a.second!=b.second){
-            return a.second<b.second;
-        }
-        else{
-            if(a.second==b.second){
-                return a.first>b.first;
-            }
-        }
-      return false;
+          if(a.second==b.second){
+            //same frequency:
+            return a.first>b.first;
+          }
+          return a.second<b.second;
 
-    }
-
-
+    
+   }
+   };
    
-
     vector<int> frequencySort(vector<int>& nums) {
+        int n=nums.size();
         unordered_map<int,int> mp;
-        for(int i=0;i<nums.size();i++){
+        for(int i=0;i<n;i++){
             mp[nums[i]]++;
         }
 
-       vector<pair<int,int>> store;
-       for(auto it=mp.begin();it!=mp.end();it++)
-       {
-           store.push_back({it->first,it->second});
-       }
+        vector<pair<int,int>> vec(mp.begin(),mp.end());
 
-       sort(store.begin(),store.end(),cmp);
-       for(int i=0;i<store.size();i++){
-           pair<int,int> P = store[i];
-           int value = P.first;
-           int time = P.second;
-           for(int i=0;i<time;i++){
-                ans.push_back(value);
-           }
-       }
-       return ans;
+        sort(vec.begin(),vec.end(),cmp());
+
+        
+         vector<int> ans;
+        for(int i=0;i<vec.size();i++){
+            pair<int,int> P = vec[i];
+            int time = P.second;
+            for(int j=1;j<=time;j++){
+                ans.push_back(P.first);
+            }
+        }
+        return ans;
 
     }
 };
