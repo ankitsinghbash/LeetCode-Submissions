08b565bb7ed725b607124b4cc1baef11c365d463
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-07-26 07:17:39 +0000

    Sync LeetCode submission Runtime - 78 ms (63.56%), Memory - 70.4 MB (99.88%)

--- my-code/0948-sort-an-array/solution.cpp
+++ my-code/0948-sort-an-array/solution.cpp
@@ -1,26 +1,97 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        //count sort:
-        unordered_map<int,int> mp;
-        for(auto &x: nums){
-            mp[x]++;
-        }
-        int sm = *min_element(nums.begin(),nums.end());
-        int lg = *max_element(nums.begin(),nums.end());
+              //1.insertion sort:  //array[i+1] always greater then arr[i] else swap:
+              //2.bubble sort:  swap adjcent node and put most greater to the end of the array:
+              //3.selection sort: find the smallest array value and swap with 
+              //4.merge sort:
+              //5.quick sort:
+        
+        //Insertion Sorting:  pass 16
+        //    int n = arr.size();
+        //    for(int i=0;i<n;i++){
+        //            int j = i-1;
+        //            int temp = arr[i];
+        //            while(j>=0  && arr[j]>temp){
+        //                 arr[j+1] = arr[j];
+        //                 j--;
+        //            }
+
+        //            arr[j+1] = temp;
+        //    }
+        //    return arr;
 
 
-        vector<int> ans;
-        for(int i=sm;i<=lg;i++){
-            auto it=mp.find(i);
-            if(it!=mp.end()){
-                //present:
-                int cnt=it->second;
-                while(cnt--){
-                  ans.push_back(i);
-                }
-            }
-        }
-        return ans;
+
+         //Bubble sorting: pass 12
+        //  int n  = arr.size();
+        //  for(int i=0;i<n-1;i++){
+        //       bool flag=true;
+        //        for(int j=0;j<n-i-1;j++){
+        //             if(arr[j]>arr[j+1]){
+        //                 swap(arr[j],arr[j+1]);
+        //                 flag=false;
+        //             }
+        //        }
+
+        //        if(flag==true){
+        //         break;
+        //        }
+        //  }
+        //  return arr;
+
+
+        //Selection Sort: //pass 12
+        // int n = arr.size();
+        // for(int i=0;i<n;i++){
+        //       int j = i;
+        //       int mini = INT_MAX;
+        //       int idx;
+        //       while(j<n){
+        //            if(arr[j]<mini){
+        //                mini = arr[j];
+        //                idx = j;
+        //            }
+        //            j++;
+        //       }
+        //       swap(arr[i],arr[idx]);
+        // }
+        // return arr;
+
+  
+
+        //merge sort:
+
+        // int low = 0;
+        // int high = arr.size()-1;
+        // while(low<=high){
+        //       int mid = low+(high-low)/2;
+              
+        // }
+
+        sort(nums.begin(),nums.end());
+
+        return nums;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //Quick sort:
+          
+
+
     }
 };
