2138e0b3a8ab1473249ee0edbab3a6cf70322488
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-08-01 15:34:52 +0000

    Sync LeetCode submission Runtime - 8 ms (2.02%), Memory - 17.4 MB (12.59%)

--- my-code/2727-number-of-senior-citizens/solution.cpp
+++ my-code/2727-number-of-senior-citizens/solution.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int countSeniors(vector<string>& details) {
-        int cnt = 0;
-        for(auto x : details){
-            string str = x;
-            string s = str.substr(11,2);
-            int val = stoi(s);
-            if(val>60){
-                cnt++;
-            }
+         int cnt=0;
+           
+        for(int i=0;i<details.size();i++){
+             int ten = details[i][11]-'0';
+             int one = details[i][12]-'0';
+             int value = ten*10+one;
+             if(value>60) cnt++;
+
         }
-        return cnt;
+
+
+
+         return cnt;
     }
 };
