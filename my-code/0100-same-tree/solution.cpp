3053090aaf5ab441b028dd156e3835a2ec29a9af
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-08-06 15:27:31 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.1 MB (99.95%)

--- my-code/0100-same-tree/solution.cpp
+++ my-code/0100-same-tree/solution.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
-    bool solve(TreeNode *p, TreeNode *q){
+    bool solve(TreeNode *p,TreeNode *q){
         if(p==NULL || q==NULL){
-            return p==q;
+            return p==q;  //structure 
         }
-
-        if(p->val!=q->val){
+        
+        
+        if(p->val!=q->val){   //node value same or not:
             return false;
         }
-
-        bool flag1 = solve(p->left,q->left);
-        bool flag2 = solve(p->right,q->right);
-
-
-        return flag1&&flag2;
-
+        
+        
+        
+        bool left = solve(p->left,q->left);
+        bool right = solve(p->right,q->right);
+        
+        
+        return left&&right;
+        
     }
+    
     bool isSameTree(TreeNode* p, TreeNode* q) {
         return solve(p,q);
     }
