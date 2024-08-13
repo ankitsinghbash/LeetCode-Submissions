8600bae710aa3e3334e42dd854d18227d533fed0
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-08-13 07:19:04 +0000

    Sync LeetCode submission Runtime - 22 ms (5.39%), Memory - 25 MB (100.00%)

--- my-code/0789-kth-largest-element-in-a-stream/solution.cpp
+++ my-code/0789-kth-largest-element-in-a-stream/solution.cpp
@@ -1,52 +1,55 @@
-/*class KthLargest {
+class KthLargest {
 public:
+    vector<int> arr;
+    int kth;
+    priority_queue<int,vector<int>,greater<int>> pq;
     KthLargest(int k, vector<int>& nums) {
+        
+        for(int i=0;i<nums.size();i++){
+            arr.push_back(nums[i]);
+        }
+        
+        kth = k;
+        
+        
+        for(int i=0;i<nums.size();i++){
+            pq.push(nums[i]);
+            if(pq.size()>kth){
+                pq.pop();
+            }
+        }
         
     }
     
     int add(int val) {
+       
+         pq.push(val);
+         if(pq.size()>kth){
+             pq.pop();
+             
+         }
+         int ans = pq.top();
+       
+        return ans;
         
+       
+       
+       
+         
+    
+      
+      
+        
+      
+         
+      
     }
 };
 
-
-
+/**
  * Your KthLargest object will be instantiated and called as such:
  * KthLargest* obj = new KthLargest(k, nums);
  * int param_1 = obj->add(val);
  */
-    //  the TC of the code is O(n*log(k)) for initialization and O(log(k)) for each add() operation.
-       // The space complexity is O(k).
-
-       class KthLargest {
-       public:
-            
-                priority_queue<int,vector<int>,greater<int>> pq;
-                    int sz;
-                        KthLargest(int k, vector<int>& nums) {
-
-                                 sz = k;
-
-                                         for(int i=0;i<nums.size();i++){
-                                                     pq.push(nums[i]);
-                                                             }
-
-                                                                    
-                                                                            
-                                                                                }
-                                                                                    
-                                                                                        int add(int val) {
-                                                                                                
-                                                                                                       pq.push(val);
-                                                                                                               while(pq.size()>sz){
-                                                                                                                           pq.pop();
-                                                                                                                                   }
-                                                                                                                                           return pq.top();
-                                                                                                                                               }
-                                                                                                                                               };
-
-                                                                                                                                               /**
-                                                                                                                                                * Your KthLargest object will be instantiated and called as such:
-                                                                                                                                                 * KthLargest* obj = new KthLargest(k, nums);
-                                                                                                                                                  * int param_1 = obj->add(val);
-                                                                                                                                                   */
+//2,3,4,4,5,5,8,9,10
+//means duplicate is not allow:
