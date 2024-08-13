49e299f8b23db9402805cd0702a033b45ccce1f6
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-08-13 17:41:28 +0000

    Sync LeetCode submission Runtime - 3 ms (38.17%), Memory - 12.9 MB (100.00%)

--- my-code/0040-combination-sum-ii/solution.cpp
+++ my-code/0040-combination-sum-ii/solution.cpp
@@ -1,43 +1,26 @@
 class Solution {
 public:
-   vector<vector<int>> ans;
-
-   void helper(int i,vector<int> path,vector<int> candidates,int target)
-   {
-       if(target==0)
-       {
-           ans.push_back(path);
-           return;
-       }
-
-       if(i>=candidates.size()||target<0)
-       {
-           return;
-       }
-
-        int prev  =-1;
-        for(int k = i; k < candidates.size(); k++){
-            int curr = candidates[k];
-            if(curr == prev)
-                continue;
-            prev = curr;
-            path.push_back(curr);                      //39 question have same approx with double calling:
-            helper(k+1, path,candidates, target-curr);  //here increment in k because we does not required the value prev value:
-            path.pop_back();           
+    vector<vector<int>> combinationSum2(vector<int> &candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>> result;
+        vector<int> combination;
+        combination2(result, combination, candidates, target, 0);
+        return result;
+    }
+    void combination2(vector<vector<int>> &res, vector<int> &combination,
+                      vector<int> &candidates, int target, int index) {
+        if (target == 0) {
+            res.push_back(combination);
+            return;
         }
-
-
-   }
-
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        ans.clear();
-        sort(candidates.begin(),candidates.end());
-        int i=0;
-        vector<int> path;
-        helper(i,path,candidates,target);
-        return ans;
+        for (int i = index; i < candidates.size() && target >= candidates[i];
+             ++i) {
+            if (i == index || candidates[i] != candidates[i - 1]) {
+                combination.push_back(candidates[i]);
+                combination2(res, combination, candidates,
+                             target - candidates[i], i + 1);
+                combination.pop_back();
+            }
+        }
     }
 };
-
-
- 
