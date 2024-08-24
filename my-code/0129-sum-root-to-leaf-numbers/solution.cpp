48708d79c4f8ec824461a9e7df861368c6fb372e
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-08-24 13:46:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.9 MB (11.99%)

--- my-code/0129-sum-root-to-leaf-numbers/solution.cpp
+++ my-code/0129-sum-root-to-leaf-numbers/solution.cpp
@@ -1,36 +1,27 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    int total=0;
+    int ans=0;
     void solve(TreeNode *root,string str){
-          if(root==NULL) return;
-      
-         
- 
-          if(root->left==NULL && root->right==NULL){
-            str+=to_string(root->val);
-            total+=stoi(str);
-            str.pop_back();
-          }
-            
-          str+=to_string(root->val);
-          solve(root->left,str);
-          solve(root->right,str);
-
+        
+         if(root==NULL) return;
+        
+        
+        str+=to_string(root->val);
+        if(root->left==NULL && root->right==NULL){
+             int value = stoi(str);
+              ans+=value;
+        }
+        
+        solve(root->left,str);
+        solve(root->right,str);
+        str.pop_back();
+        
+        
     }
+    
     int sumNumbers(TreeNode* root) {
-        string str;
-        solve(root,str);
-        return total;
+            string str;
+           solve(root,str);
+           return ans;
     }
 };
