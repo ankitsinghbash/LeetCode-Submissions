18ad2ebe540f85803095389b40269503583f8ad7
Ankit Singh <ankitsingh4734414code@gmail.com>
2024-08-24 13:58:36 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.1 MB (88.12%)

--- my-code/0257-binary-tree-paths/solution.cpp
+++ my-code/0257-binary-tree-paths/solution.cpp
@@ -1,44 +1,36 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-   vector<string> ans;
-   void solve(TreeNode *root,string str){
-    if(root==NULL){
-        return;
-    }
-     
-     if(root->left==NULL && root->right==NULL){
-         str+=to_string(root->val);
-     }
-     else{
+    vector<string> store;
+    void solve(TreeNode *root,string str){
+        if(root==NULL) return;
+        
+        
+       
+        
+        if(root->left==NULL && root->right==NULL){
+              str+=to_string(root->val);
+              store.push_back(str);
+              return;
+        }
+        
+        
          str+=to_string(root->val)+"->";
-     }
-
-     if(root->left==NULL && root->right==NULL){
-         ans.push_back(str);
-         str.pop_back();
-     }
-     
-     solve(root->left,str);
-     solve(root->right,str);
-     str.pop_back();
-   }
-
-
-
-
+        solve(root->left,str);
+        solve(root->right,str);
+        
+        str.pop_back();
+        
+        
+        
+        
+        
+    }
+    
+    
+    
     vector<string> binaryTreePaths(TreeNode* root) {
-        solve(root,"");
-        return ans;
+        string str;
+        solve(root,str);
+        return store;
     }
 };
